40ac349698c5ed9e2e0b29bb356c49557d6ecd3c
maroy <maroy@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2004-07-23 15:58:58 +0000

    added libxml++ 1.0.4

---
+++ livesupport/tools/libxml++/libxml++-1.0.4/bin/install.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+#-------------------------------------------------------------------------------
+#   Copyright (c) 2004 Media Development Loan Fund
+#
+#   This file is part of the LiveSupport project.
+#   http://livesupport.campware.org/
+#   To report bugs, send an e-mail to bugs@campware.org
+#
+#   LiveSupport is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   LiveSupport is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with LiveSupport; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#
+#   Author   : $Author: maroy $
+#   Version  : $Revision: 1.1 $
+#   Location : $Source: /home/paul/cvs2svn-livesupport/newcvsrepo/livesupport/tools/libxml++/libxml++-1.0.4/bin/Attic/install.sh,v $
+#-------------------------------------------------------------------------------                                                                                
+#-------------------------------------------------------------------------------
+# Run this script to install cppunit into the development system usr
+# environment.
+# For more information on libxml++, see http://libxmlplusplus.sourceforge.net/
+#-------------------------------------------------------------------------------
+product=libxml++-1.0.4
+
+reldir=`dirname $0`/..
+basedir=`cd $reldir; pwd; cd -`
+installdir=`cd $basedir/../../../usr; pwd; cd -`
+tmpdir=$basedir/tmp
+tar=$basedir/src/$product.tar.bz2
+
+echo "installing $product from $basedir to $installdir"
+
+
+mkdir -p $tmpdir
+cd $tmpdir
+
+tar xfj $tar
+cd $product
+./configure --prefix=$installdir
+make install
+
+cd $basedir
+rm -rf tmp
+
