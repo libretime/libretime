38c61b0dd534c190c623610fdfcebe253da8d05f
maroy <maroy@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2004-07-23 15:33:30 +0000

    added development environment documentation

---
+++ livesupport/doc/developmentEnvironment/templates/Bar.cxx
@@ -0,0 +1,70 @@
+/*------------------------------------------------------------------------------
+
+    Copyright (c) 2004 Media Development Loan Fund
+ 
+    This file is part of the LiveSupport project.
+    http://livesupport.campware.org/
+    To report bugs, send an e-mail to bugs@campware.org
+ 
+    LiveSupport is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+  
+    LiveSupport is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ 
+    You should have received a copy of the GNU General Public License
+    along with LiveSupport; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ 
+ 
+    Author   : $Author$
+    Version  : $Revision$
+    Location : $Source$
+
+------------------------------------------------------------------------------*/
+
+/* =============================================== include files & namespaces */
+
+#ifdef HAVE_CONFIG_H
+#include "configure.h"
+#endif
+
+#include <LiveSupport/Foo/Bar.h>
+
+
+using namespace LiveSupport::Core;
+using namespace LiveSupport::Bar;
+
+/* ===================================================  local data structures */
+
+
+/* ================================================  local constants & macros */
+
+/*------------------------------------------------------------------------------
+ *  Our famous foo string.
+ *----------------------------------------------------------------------------*/
+const std::string Bar::fooStr = "foo";
+
+
+/* ===============================================  local function prototypes */
+
+
+/* =============================================================  module code */
+
+/*------------------------------------------------------------------------------
+ *  Return the famous bar string.
+ *----------------------------------------------------------------------------*/
+const std::string
+Bar :: sayBar(void)                             throw (std::exception)
+{
+    if (barInt) {
+        throw std::exception();
+    }
+
+    return barStr;
+}
+
---
+++ livesupport/doc/developmentEnvironment/templates/Bar.h
@@ -0,0 +1,111 @@
+/*------------------------------------------------------------------------------
+
+    Copyright (c) 2004 Media Development Loan Fund
+ 
+    This file is part of the LiveSupport project.
+    http://livesupport.campware.org/
+    To report bugs, send an e-mail to bugs@campware.org
+ 
+    LiveSupport is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+  
+    LiveSupport is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ 
+    You should have received a copy of the GNU General Public License
+    along with LiveSupport; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ 
+ 
+    Author   : $Author$
+    Version  : $Revision$
+    Location : $Source$
+
+------------------------------------------------------------------------------*/
+#ifndef LiveSupport_Foo_Bar_h
+#define LiveSupport_Foo_Bar_H
+
+#ifndef __cplusplus
+#error This is a C++ include file
+#endif
+
+
+/* =============================================== include files & namespaces */
+
+#ifdef HAVE_CONFIG_H
+#include "configure.h"
+#endif
+
+#include <string>
+
+
+namespace LiveSupport {
+namespace Foo {
+
+using namespace LiveSupport::Core;
+
+/* ================================================================ constants */
+
+
+/* =================================================================== macros */
+
+
+/* =============================================================== data types */
+
+/**
+ *  Bar class.
+ *  This does nothing.
+ *
+ *  @author  $Author$
+ *  @version $Revision$
+ */
+class Bar
+{
+    private:
+        /**
+         *  A static member variable.
+         */
+        static const std::string        barStr;
+
+        /**
+         *  A member variable.
+         */
+        int                             barInt;
+
+    public:
+        /**
+         *  Default constructor.
+         */
+        Bar (void)                                      throw ()
+        {
+        }
+
+        /**
+         *  Say something.
+         *
+         *  @param parameter a parameter we don't care about.
+         *  @return the bar string.
+         *  @exception std::exception on some problems.
+         */
+        const std::string
+        sayBar (void)                           throw (std::exception)
+        {
+            return barStr;
+        }
+};
+
+
+/* ================================================= external data structures */
+
+
+/* ====================================================== function prototypes */
+
+} // namespace Foo
+} // namespace LiveSupport
+
+
+#endif // LiveSupport_Foo_Bar_H
