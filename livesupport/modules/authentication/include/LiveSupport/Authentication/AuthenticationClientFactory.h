03e541ce798b1c64ae2fe86f503c83358e85531e
fgerlits <fgerlits@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2004-11-20 10:24:23 +0000

    added authentication module

---
+++ livesupport/modules/authentication/include/LiveSupport/Authentication/AuthenticationClientFactory.h
@@ -0,0 +1,188 @@
+/*------------------------------------------------------------------------------
+
+    Copyright (c) 2004 Media Development Loan Fund
+ 
+    This file is part of the LiveSupport project.
+    http://livesupport.campware.org/
+    To report bugs, send an e-mail to bugs@campware.org
+ 
+    LiveSupport is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+  
+    LiveSupport is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ 
+    You should have received a copy of the GNU General Public License
+    along with LiveSupport; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ 
+ 
+    Author   : $Author: fgerlits $
+    Version  : $Revision: 1.1 $
+    Location : $Source: /home/paul/cvs2svn-livesupport/newcvsrepo/livesupport/modules/authentication/include/LiveSupport/Authentication/AuthenticationClientFactory.h,v $
+
+------------------------------------------------------------------------------*/
+#ifndef LiveSupport_Authentication_AuthenticationClientFactory_h
+#define LiveSupport_Authentication_AuthenticationClientFactory_h
+
+#ifndef __cplusplus
+#error This is a C++ include file
+#endif
+
+
+/* ============================================================ include files */
+
+#ifdef HAVE_CONFIG_H
+#include "configure.h"
+#endif
+
+#include <stdexcept>
+
+#include "LiveSupport/Core/Configurable.h"
+#include "LiveSupport/Authentication/AuthenticationClientInterface.h"
+
+
+namespace LiveSupport {
+namespace Authentication {
+
+using namespace LiveSupport;
+using namespace LiveSupport::Core;
+
+/* ================================================================ constants */
+
+
+/* =================================================================== macros */
+
+
+/* =============================================================== data types */
+
+/**
+ *  The factory to create appropriate AuthenticationClient objects.
+ *
+ *  This object has to be configured with an XML configuration element
+ *  called authenticationClientFactory. This element contains a child element
+ *  specifying and configuring the kind of AuthenticationClient that the
+ *  factory builds. Currently only the WebAuthenticationClient is supported.
+ *
+ *  A authenticationClientFactory configuration element may look like the following:
+ *
+ *  <pre><code>
+ *  &lt;authenticationClientFactory&gt;
+ *      &lt;webAuthentication&gt;
+ *          ...
+ *      &lt;/webAuthentication&gt;
+ *  &lt;/authenticationClientFactory&gt;
+ *  </code></pre>
+ *
+ *  For detais of the testAuthentication element, see the documentation for the
+ *  WebAuthenticationClient class.
+ *
+ *  The DTD for the above element is:
+ *
+ *  <pre><code>
+ *  &lt;!ELEMENT authenticationClientFactory (webAuthentication) &gt;
+ *  </code></pre>
+ *
+ *  @author  $Author: fgerlits $
+ *  @version $Revision: 1.1 $
+ *  @see TestAuthenticationClient
+ */
+class AuthenticationClientFactory :
+                        virtual public Configurable
+{
+    private:
+        /**
+         *  The name of the configuration XML elmenent used by this object.
+         */
+        static const std::string                        configElementNameStr;
+
+        /**
+         *  The singleton instance of this object.
+         */
+        static Ptr<AuthenticationClientFactory>::Ref    singleton;
+
+        /**
+         *  The authentication client created by this factory.
+         */
+        Ptr<AuthenticationClientInterface>::Ref         authenticationClient;
+
+        /**
+         *  The default constructor.
+         */
+        AuthenticationClientFactory(void)       throw()
+        {
+        }
+
+
+    public:
+        /**
+         *  A virtual destructor, as this class has virtual functions.
+         */
+        virtual
+        ~AuthenticationClientFactory(void)      throw ()
+        {
+        }
+
+        /**
+         *  Return the name of the XML element this object expects
+         *  to be sent to a call to configure().
+         *  
+         *  @return the name of the expected XML configuration element.
+         */
+        static const std::string
+        getConfigElementName(void)              throw ()
+        {
+            return configElementNameStr;
+        }
+
+        /**
+         *  Returns the singleton instance of this object.
+         *
+         *  @return the singleton instance of this object.
+         */
+        static Ptr<AuthenticationClientFactory>::Ref
+        getInstance()                           throw ();
+
+        /**
+         *  Configure the object based on the XML element supplied.
+         *
+         *  @param element the XML element to configure the object from.
+         *  @exception std::invalid_argument if the supplied XML element
+         *             contains bad configuraiton information
+         *  @exception std::logic_error if the object has already
+         *             been configured, and can not be reconfigured.
+         */
+        virtual void
+        configure(const xmlpp::Element    & element)
+                                                throw (std::invalid_argument,
+                                                       std::logic_error);
+
+        /**
+         *  Return an authentication client.
+         *
+         *  @return the appropriate authentication client, according to the
+         *          configuration of this factory.
+         */
+        Ptr<AuthenticationClientInterface>::Ref
+        getAuthenticationClient(void)           throw ()
+        {
+            return authenticationClient;
+        }
+};
+
+
+/* ================================================= external data structures */
+
+
+/* ====================================================== function prototypes */
+
+
+} // namespace Authentication
+} // namespace LiveSupport
+
+#endif // LiveSupport_Authentication_AuthenticationClientFactory_h
+
---
+++ livesupport/modules/authentication/src/AuthenticationClientFactory.cxx
@@ -0,0 +1,111 @@
+/*------------------------------------------------------------------------------
+
+    Copyright (c) 2004 Media Development Loan Fund
+ 
+    This file is part of the LiveSupport project.
+    http://livesupport.campware.org/
+    To report bugs, send an e-mail to bugs@campware.org
+ 
+    LiveSupport is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+  
+    LiveSupport is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ 
+    You should have received a copy of the GNU General Public License
+    along with LiveSupport; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ 
+ 
+    Author   : $Author: fgerlits $
+    Version  : $Revision: 1.1 $
+    Location : $Source: /home/paul/cvs2svn-livesupport/newcvsrepo/livesupport/modules/authentication/src/AuthenticationClientFactory.cxx,v $
+
+------------------------------------------------------------------------------*/
+
+/* ============================================================ include files */
+
+#ifdef HAVE_CONFIG_H
+#include "configure.h"
+#endif
+
+#include "LiveSupport/Authentication/AuthenticationClientFactory.h"
+#include "LiveSupport/Authentication/WebAuthenticationClient.h"
+
+using namespace LiveSupport::Core;
+using namespace LiveSupport::Authentication;
+
+
+/* ===================================================  local data structures */
+
+
+/* ================================================  local constants & macros */
+
+/*------------------------------------------------------------------------------
+ *  The name of the config element for this class
+ *----------------------------------------------------------------------------*/
+const std::string AuthenticationClientFactory::configElementNameStr =
+                                               "authenticationClientFactory";
+
+/*------------------------------------------------------------------------------
+ *  The singleton instance of AuthenticationClientFactory
+ *----------------------------------------------------------------------------*/
+Ptr<AuthenticationClientFactory>::Ref AuthenticationClientFactory::singleton;
+
+
+/* ===============================================  local function prototypes */
+
+
+/* =============================================================  module code */
+
+/*------------------------------------------------------------------------------
+ *  Return the singleton instance to AuthenticationClientFactory
+ *----------------------------------------------------------------------------*/
+Ptr<AuthenticationClientFactory>::Ref
+AuthenticationClientFactory :: getInstance(void)                   throw ()
+{
+    if (!singleton.get()) {
+        singleton.reset(new AuthenticationClientFactory());
+    }
+
+    return singleton;
+}
+
+
+/*------------------------------------------------------------------------------
+ *  Configure the test authentication client.
+ *----------------------------------------------------------------------------*/
+void
+AuthenticationClientFactory :: configure(const xmlpp::Element & element)
+                                                throw (std::invalid_argument,
+                                                       std::logic_error)
+{
+    if (element.get_name() != configElementNameStr) {
+        std::string eMsg = "Bad configuration element ";
+        eMsg += element.get_name();
+        throw std::invalid_argument(eMsg);
+    }
+
+    authenticationClient.reset();
+
+    // try to look for an WebAuthenticationClient configuration element
+    xmlpp::Node::NodeList   nodes =
+        element.get_children(WebAuthenticationClient::getConfigElementName());
+    if (nodes.size() >= 1) {
+        const xmlpp::Element  * configElement =
+                         dynamic_cast<const xmlpp::Element*> (*(nodes.begin()));
+        Ptr<WebAuthenticationClient>::Ref   wac(new WebAuthenticationClient());
+        wac->configure(*configElement);
+        authenticationClient = wac;
+    }
+
+    if (!authenticationClient) {
+        throw std::invalid_argument("no authentication client factories "
+                                                                "to configure");
+    }
+}
+
