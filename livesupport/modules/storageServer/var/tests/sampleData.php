ce64c6ead3da56ed68df03cd7216f4f729a0bd6b
tomas <tomas@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2004-12-12 20:32:51 +0000

    cosmetic chngs

--- livesupport/modules/storageServer/var/tests/sampleData.php
+++ livesupport/modules/storageServer/var/tests/sampleData.php
@@ -2,7 +2,6 @@
         $sampleData = array(
             array('../tests/ex1.mp3', '../tests/mdata1.xml'),
             array('../tests/ex2.ogg', '../tests/mdata2.xml'),
-            array('../tests/ex2.wav', '../tests/mdata3.xml'),
-//            array('', ''),
+            array('../tests/ex2.wav', '../tests/mdata3.xml')
         );
 ?>
