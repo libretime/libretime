408f79abfafdaaef31460ff23eed72d284ced523
fgerlits <fgerlits@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2004-12-30 11:35:59 +0000

    added curlpp (instead of curl)

---
+++ livesupport/tools/curlpp/curlpp-0.3.1/bin/install.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+#-------------------------------------------------------------------------------
+#   Copyright (c) 2004 Media Development Loan Fund
+#
+#   This file is part of the LiveSupport project.
+#   http://livesupport.campware.org/
+#   To report bugs, send an e-mail to bugs@campware.org
+#
+#   LiveSupport is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   LiveSupport is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with LiveSupport; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#
+#   Author   : $Author: fgerlits $
+#   Version  : $Revision: 1.1 $
+#   Location : $Source: /home/paul/cvs2svn-livesupport/newcvsrepo/livesupport/tools/curlpp/curlpp-0.3.1/bin/Attic/install.sh,v $
+#-------------------------------------------------------------------------------                                                                                
+#-------------------------------------------------------------------------------
+# Run this script to install cppunit into the development system usr
+# environment.
+# For more information on libxml++, see http://libxmlplusplus.sourceforge.net/
+#
+# WARNING: This install script does NOT install related dependencies, like Glib.
+# To have these dependencies installed, please install GTK-- first, which
+# in fact installs all needed dependencies
+#-------------------------------------------------------------------------------
+product=curlpp-0.3.1
+
+reldir=`dirname $0`/..
+basedir=`cd $reldir; pwd; cd -`
+usrdir=`cd $basedir/../../../usr; pwd; cd -`
+tmpdir=$basedir/tmp
+tar=$basedir/src/$product.tar.gz
+installdir=$usrdir
+
+export CPPFLAGS="-I$usrdir/include"
+export LDFLAGS="-L$usrdir/lib"
+export PKG_CONFIG_PATH="$usrdir/lib/pkgconfig"
+export LD_LIBRARY_PATH="$usrdir/lib"
+
+
+echo "installing $product from $basedir to $installdir"
+
+
+mkdir -p $tmpdir
+cd $tmpdir
+
+tar xfz $tar
+cd $product
+./configure --prefix=$installdir
+make install
+
+cd $basedir
+rm -rf tmp
+
