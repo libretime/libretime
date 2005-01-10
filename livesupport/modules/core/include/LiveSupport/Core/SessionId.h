bd36de8bd492eff329cb4b1db049b15da05a90a1
maroy <maroy@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2005-01-10 09:24:27 +0000

    added missing include for <string>

--- livesupport/modules/core/include/LiveSupport/Core/SessionId.h
+++ livesupport/modules/core/include/LiveSupport/Core/SessionId.h
@@ -21,8 +21,8 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  
  
-    Author   : $Author: fgerlits $
-    Version  : $Revision: 1.2 $
+    Author   : $Author: maroy $
+    Version  : $Revision: 1.3 $
     Location : $Source: /home/paul/cvs2svn-livesupport/newcvsrepo/livesupport/modules/core/include/LiveSupport/Core/SessionId.h,v $
 
 ------------------------------------------------------------------------------*/
@@ -40,6 +40,9 @@
 #include "configure.h"
 #endif
 
+#include <string>
+
+
 namespace LiveSupport {
 namespace Core {
 
@@ -54,8 +57,8 @@ namespace Core {
 /**
  *  A class representing session identifiers.
  *
- *  @author  $Author: fgerlits $
- *  @version $Revision: 1.2 $
+ *  @author  $Author: maroy $
+ *  @version $Revision: 1.3 $
  */
 class SessionId
 {
