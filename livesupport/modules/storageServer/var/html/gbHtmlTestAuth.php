89e9be4041be1aa54e817eb43461349fc9276cc7
tomas <tomas@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2005-04-28 00:52:16 +0000

    obsolete example directory

--- livesupport/modules/storageServer/var/html/gbHtmlTestAuth.php
+++
@@ -1,9 +0,0 @@
-<?php
-// $Id: gbHtmlTestAuth.php,v 1.2 2004/09/21 00:27:41 tomas Exp $
-$login = $gb->getSessLogin($_REQUEST[$config['authCookieName']]);
-if(!isset($login)||$login==''){
-    $_SESSION['alertMsg'] = "Login required";
-    header("Location: gbHtmlLogin.php");
-    exit;
-}
-?>
