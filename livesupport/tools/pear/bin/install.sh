5001ed991ad3b2b2db03aeb2cd35e1de7d5203ef
tomas <tomas@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2005-05-01 17:17:49 +0000

    Solved #880

--- livesupport/tools/pear/bin/install.sh
+++ livesupport/tools/pear/bin/install.sh
@@ -21,8 +21,8 @@
 #   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 #
-#   Author   : $Author: maroy $
-#   Version  : $Revision: 1.2 $
+#   Author   : $Author: tomas $
+#   Version  : $Revision: 1.3 $
 #   Location : $Source: /home/paul/cvs2svn-livesupport/newcvsrepo/livesupport/tools/pear/bin/install.sh,v $
 #-------------------------------------------------------------------------------                                                                                
 #-------------------------------------------------------------------------------
@@ -32,47 +32,56 @@
 #-------------------------------------------------------------------------------
 description="PEAR packages needed by LiveSupport"
 
-#PEAR-1.4.0a11.tgz
+#PEAR-1.4.0a11
 packages="
-Archive_Tar-1.3.1.tgz
-Console_Getopt-1.2.tgz
-XML_RPC-1.3.0RC1.tgz
-PEAR-1.3.5.tgz
-Calendar-0.5.2.tgz
-DB-1.7.6.tgz
-File-1.2.0.tgz
-File_Find-0.3.1.tgz
-HTML_Common-1.2.1.tgz
-HTML_QuickForm-3.2.4pl1.tgz
-XML_Util-1.1.1.tgz
-XML_Parser-1.2.6.tgz
-XML_Beautifier-1.1.tgz
-XML_Serializer-0.15.0.tgz
+Archive_Tar-1.3.1
+Console_Getopt-1.2
+XML_RPC-1.3.0RC1
+PEAR-1.3.5
+Calendar-0.5.2
+DB-1.7.6
+File-1.2.0
+File_Find-0.3.1
+HTML_Common-1.2.1
+HTML_QuickForm-3.2.4pl1
+XML_Util-1.1.1
+XML_Parser-1.2.6
+XML_Beautifier-1.1
+XML_Serializer-0.15.0
 "
 
 reldir=`dirname $0`/..
 basedir=`cd $reldir; pwd;`
-rootdir=`cd $basedir/../..; pwd;`
-tmpdir=$basedir/tmp
 etcdir=$basedir/etc
-usrdir=$rootdir/usr
+srcdir=$basedir/src
 
-echo "installing $description (with rootdir: $rootdir)"
+dest_usrdir=`cd $basedir/../../usr; pwd;`
+destdir=$dest_usrdir/lib/php
+mkdir -p $destdir
+mkdir -p $destdir/etc
 
-cd $basedir/src
+echo "configuring $description (with destdir: $destdir)"
+
+configtemplate=$etcdir/pear.conf.template
+configfile=$destdir/etc/pear.conf
+pearcmd="pear -c $configtemplate"
+cp $configtemplate $configfile
+
+$pearcmd config-set php_dir $destdir/php || exit 1
+$pearcmd config-set bin_dir $destdir/bin || exit 1
+$pearcmd config-set doc_dir $destdir/php/docs || exit 1
+$pearcmd config-set data_dir $destdir/php/data || exit 1
+$pearcmd config-set cache_dir $destdir/php/cache || exit 1
+$pearcmd config-set test_dir $destdir/php/tests || exit 1
+#$pearcmd config-show; exit
+
+echo "installing $description (with destdir: $destdir)"
+cd $srcdir
 for i in $packages
 do echo $i
-    pear install -R $rootdir $i || \
+    $pearcmd install $i.tgz || \
         { echo "*** ERROR installing $i"; exit 1; }
 done
-
-# ugly work-around, as some system install the modules under
-# usr/share/pear, but we're expecting it to be at usr/lib/php
-
-if [ -d $usrdir/share/pear ]; then
-    rm -rf $usrdir/lib/php
-    mv -f $usrdir/share/pear $usrdir/lib/php
-fi
 
 echo "PEAR packages install finished OK"
 exit 0
