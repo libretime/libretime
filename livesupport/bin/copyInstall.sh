4b24043c16c6f9079d14261da648e1d3a0b6f4fd
mash <mash@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2005-05-27 08:26:39 +0000

    move icon to products/gLiveSupport

--- livesupport/bin/copyInstall.sh
+++ livesupport/bin/copyInstall.sh
@@ -21,8 +21,8 @@
 #   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 #
-#   Author   : $Author: tomas $
-#   Version  : $Revision: 1.7 $
+#   Author   : $Author: mash $
+#   Version  : $Revision: 1.8 $
 #   Location : $Source: /home/paul/cvs2svn-livesupport/newcvsrepo/livesupport/bin/Attic/copyInstall.sh,v $
 #-------------------------------------------------------------------------------                                                                                
 #-------------------------------------------------------------------------------
@@ -171,6 +171,7 @@ cp -pPR $products_dir/scheduler/bin/scheduler.sh $install_bin
 cp -pPR $products_dir/gLiveSupport/tmp/gLiveSupport $install_bin
 cp -pPR $products_dir/gLiveSupport/bin/gLiveSupport.sh $install_bin
 cp -pPR $products_dir/gLiveSupport/var/widgets $install_var
+cp -pPR $products_dir/gLiveSupport/var/livesupport.png $install_var
 cp -pPR $products_dir/gLiveSupport/tmp/gLiveSupport*.res $install_var
 
 
