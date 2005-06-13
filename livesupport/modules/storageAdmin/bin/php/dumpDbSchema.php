8ab2d92134ac9d21aeedc90626a8232b89527a04
tomas <tomas@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2005-06-13 10:17:49 +0000

    moved to storageAdmin/var

--- livesupport/modules/storageAdmin/bin/php/dumpDbSchema.php
+++
@@ -1,9 +0,0 @@
-<?
-require_once 'conf.php';
-require_once "$storageServerPath/var/conf.php";
- header("Conten-type: text/plain");
- $dbname = $config['dsn']['database'];
- $dbuser = $config['dsn']['username'];
- $res = `pg_dump -s $dbname -U $dbuser`;
- echo "$res\n";
-?>
