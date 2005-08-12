d2eb874fddc928fa28940d46dc8659a5b40dea7d
fgerlits <fgerlits@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2005-08-12 17:18:35 +0000

    modified livesupport-station installation to provide our own pg_hba.conf

--- livesupport/bin/postInstallStation.sh
+++ livesupport/bin/postInstallStation.sh
@@ -22,7 +22,7 @@
 #
 #
 #   Author   : $Author: fgerlits $
-#   Version  : $Revision: 1.4 $
+#   Version  : $Revision: 1.5 $
 #   Location : $Source: /home/paul/cvs2svn-livesupport/newcvsrepo/livesupport/bin/postInstallStation.sh,v $
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
@@ -284,6 +284,21 @@ if [ $? != 0 ]; then
     exit 1;
 fi
 rm -f $group_tmp_file;
+
+
+#-------------------------------------------------------------------------------
+#  Install the new pg_hba.conf file
+#-------------------------------------------------------------------------------
+echo "Modifying postgresql access permissions...";
+
+pg_config_dir=/etc/postgresql
+pg_config_file=pg_hba.conf
+pg_config_file_saved=pg_hba.conf.before-livesupport
+
+if [ -f $pg_config_dir/$pg_config_file ] ; then
+    mf -f $pg_config_dir/$pg_config_file $pg_config_dir/$pg_config_file_saved ;
+fi
+cp $install_etc/$pg_config_file $pg_config_dir/$pg_config_file
 
 
 #-------------------------------------------------------------------------------
