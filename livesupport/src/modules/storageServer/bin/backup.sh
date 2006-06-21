939172830f807e3ab11636cca2f3df5d2ac3f703
csikosjanos <csikosjanos@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2006-06-21 16:27:39 +0000

    adding faultString for backup/restore checking 2

--- livesupport/src/modules/storageServer/bin/backup.sh
+++ livesupport/src/modules/storageServer/bin/backup.sh
@@ -5,14 +5,14 @@
 # param $3: statusfile
 echo "==>"
 date +\ %Y%m%d\ %H:%M:%S
-echo "backup2.sh: create tarball $1 to $2"
-echo "backup2.sh: status: #$3#"
+echo "backup.sh: create tarball $1 to $2"
+echo "backup.sh: status: #$3#"
 echo "<=="
 echo -n "working" > $3;
-touch $2 || { echo -n "fault" > $3; exit 1; }
+touch $2 || { echo -n "fault|error with .tar file" > $3; exit 1; }
 #sleep 120
 cd $1
-tar cf $2 * || { echo -n "fault" > $3; exit 1; }
+tar cf $2 * || { echo -n "fault|error in tar procedure" > $3; exit 1; }
 chmod 666 $2
 
 echo -n "success" > $3
