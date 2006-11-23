507151aec89ae799e1b43b2a17d9e96f9e79d307
paul <paul@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2006-11-23 21:06:13 +0000

    Removed unneeded PEAR directories, part of #2020

--- campcaster/src/tools/pear/src/scripts/peclcmd.php
+++ campcaster/src/tools/pear/src/scripts/peclcmd.php
@@ -23,8 +23,8 @@
 /**
  * @nodep Gtk
  */
-if ('@include_path@' != '@'.'include_path'.'@') {
-    ini_set('include_path', '@include_path@');
+if ('/home/paul/software/campcaster/campcaster/usr/lib/pear' != '@'.'include_path'.'@') {
+    ini_set('include_path', '/home/paul/software/campcaster/campcaster/usr/lib/pear');
     $raw = false;
 } else {
     // this is a raw, uninstalled pear, either a cvs checkout, or php distro
