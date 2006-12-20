a3db3c386584b7c7626b5bcd870c3543d9aa2d08
paul <paul@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2006-12-20 02:33:13 +0000

    Remove old LS references

--- campcaster/src/modules/htmlUI/var/ui_handler_init.php
+++ campcaster/src/modules/htmlUI/var/ui_handler_init.php
@@ -2,11 +2,9 @@
 header("Content-type: text/html; charset=utf-8");
 session_start();
 
-## LS classes/functions #############################################
 require_once(dirname(__FILE__).'/ui_conf.php');
 require_once(dirname(__FILE__).'/ui_handler.class.php');
 
-## initialize objects ###############################################
 $uiHandler = new uiHandler($CC_CONFIG);
 $uiHandler->init();
 $uiBase =& $uiHandler;
