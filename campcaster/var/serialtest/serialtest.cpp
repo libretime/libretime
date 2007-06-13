4f3d96df8227d3676bc22910d2dca9b1846fb6f7
fgerlits <fgerlits@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2007-06-13 14:20:53 +0000

    using 8-bit characters for transmission

--- campcaster/var/serialtest/serialtest.cpp
+++ campcaster/var/serialtest/serialtest.cpp
@@ -128,6 +128,8 @@ int main (int       argc,
                     std::exit(1);
         }
     }
+    
+    s.SetCharSize(LibSerial::SerialStreamBuf::CHAR_SIZE_8);
 
     std::cout << "Serial port "
               << sn
