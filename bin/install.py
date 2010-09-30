acda23db88fb1b2c8f8f9b5b4fd2c516ee523e6a
paul.baranowski <paul.baranowski@sourcefabric.org>
2010-09-30 15:52:08 -0400

    CC-1695  	 Remove Campcaster Studio and make install easier Moved Smarty

---
+++ bin/install.py
@@ -0,0 +1,22 @@
+// install dir
+// DB server
+// Database name
+// DB user
+// DB password
+// Apache group
+// postgres dir
+// postgres init script
+// Web root
+
+Save this stuff to config file
+Run install.php
+Create/adjust directories
+
+Move config values to INI file
+
+// Setup dir structure (can it be setup that way in the first place?)
+// Check that the exe's exist
+// check apache group exists
+// install pg_hba.conf
+// create database
+// 
