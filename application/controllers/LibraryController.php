7aac80ed58f7f72f2c317d9864a169c29a29c2d0
naomiaro <naomiaro@gmail.com>
2011-01-06 01:12:24 -0500

    renaming for airtime

--- application/controllers/LibraryController.php
+++ application/controllers/LibraryController.php
@@ -204,7 +204,7 @@ class LibraryController extends Zend_Controller_Action
 
     public function quickSearchAction()
     {
-		$this->view->headScript()->appendFile('/js/campcaster/library/quicksearch.js','text/javascript');
+		$this->view->headScript()->appendFile('/js/airtime/library/quicksearch.js','text/javascript');
 
         $this->_helper->viewRenderer->setResponseSegment('quick_search'); 
 
