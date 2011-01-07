13c05d35a92e1ca2d14b3633b0e10b7cf1abd0c5
mkonecny <martin.konecny@sourcefabric.org>
2011-01-07 11:26:27 -0500

    -removed css footer for now...

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -53,8 +53,8 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 		$view = $this->getResource('view');
 		$view->headScript()->appendFile('/js/libs/jquery-1.4.4.min.js','text/javascript');
 		$view->headScript()->appendFile('/js/libs/jquery-ui-1.8.7.custom.min.js','text/javascript');
-		$view->headScript()->appendFile('/js/playlist/playlist.js','text/javascript');
-		$view->headScript()->appendFile('/js/progressbar/jquery.progressbar.min.js','text/javascript');
+		//$view->headScript()->appendFile('/js/playlist/playlist.js','text/javascript');
+		//$view->headScript()->appendFile('/js/progressbar/jquery.progressbar.min.js','text/javascript');
 	}
 
 }
