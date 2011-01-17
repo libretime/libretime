209e8b599839033ce975ee6cd4b82f5808dc7c18
mkonecny <martin.konecny@sourcefabric.org>
2011-01-17 10:27:55 -0500

    -pypo update

--- pypo/scripts/notify.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-############################################
-# just a wrapper to call the notifyer      #
-# needed here to keep dirs/configs clean   #
-# and maybe to set user-rights             #
-############################################
-cd ../ && ./pypo-notify.py $1 $2 $3 $4 $5 $6 $7 $8 &
--- pypo/util/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from json import *
-from status import *
-from cue_file import *
