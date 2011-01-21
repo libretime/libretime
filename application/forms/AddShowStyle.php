edc7c03824f6b803b09eab4486e1af61ff8d8e44
Naomi <naomi@naomi-DX4840.(none)>
2011-01-21 14:58:23 -0500

    color isn't required

--- application/forms/AddShowStyle.php
+++ application/forms/AddShowStyle.php
@@ -8,17 +8,13 @@ class Application_Form_AddShowStyle extends Zend_Form_SubForm
        // Add show background-color input
         $this->addElement('text', 'add_show_background_color', array(
             'label'      => 'Background Colour:',
-            'required'   => true,
-            'filters'    => array('StringTrim'),
-            'validators' => array('NotEmpty')
+            'filters'    => array('StringTrim')
         ));
 
 	// Add show color input
         $this->addElement('text', 'add_show_color', array(
             'label'      => 'Text Colour',
-            'required'   => true,
-            'filters'    => array('StringTrim'),
-            'validators' => array('NotEmpty')
+            'filters'    => array('StringTrim')
         ));
     }
 
