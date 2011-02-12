9a7dda49fe8b4796172b0b5bc50f296fe8f7713d
naomiaro <naomiaro@gmail.com>
2011-02-12 14:44:55 -0500

    CC-1917 file meta data screen

--- application/forms/EditAudioMD.php
+++ application/forms/EditAudioMD.php
@@ -9,9 +9,10 @@ class Application_Form_EditAudioMD extends Zend_Form
         $this->setMethod('post');
 
 		// Add title field
-        $this->addElement('text', 'track_title', array(
+        $this->addElement('text', 'dc:title', array(
             'label'      => 'Title:',
             'required'   => true,
+            'class'      => 'input_text',
             'filters'    => array('StringTrim'),
             'validators' => array(
                 'NotEmpty',
@@ -19,70 +20,74 @@ class Application_Form_EditAudioMD extends Zend_Form
         ));
 
 		// Add artist field
-        $this->addElement('text', 'artist_name', array(
+        $this->addElement('text', 'dc:creator', array(
             'label'      => 'Artist:',
             'required'   => true,
+            'class'      => 'input_text',
             'filters'    => array('StringTrim'),
             'validators' => array(
                 'NotEmpty',
             )
         ));
 
-		// Add bitrate field
-       // $this->addElement('text', 'bit_rate', array(
-       //     'label'      => 'Bitrate:',
-		//	'attribs'    => array('disabled' => 'disabled')
-        //));
-
 		// Add album field
-        $this->addElement('text', 'album_title', array(
+        $this->addElement('text', 'dc:source', array(
             'label'      => 'Album:',
+            'class'      => 'input_text',
             'filters'    => array('StringTrim')
         ));
 
 		// Add genre field
         $this->addElement('text', 'genre', array(
             'label'      => 'Genre:',
+            'class'      => 'input_text',
             'filters'    => array('StringTrim')
         ));
 
 		// Add year field
-        $this->addElement('text', 'year', array(
+        $this->addElement('text', 'ls:year', array(
             'label'      => 'Year:',
+            'class'      => 'input_text',
             'filters'    => array('StringTrim'),
             'validators' => array(
 				array('date', false, array('YYYY-MM-DD')),
+                array('date', false, array('YYYY-MM')),
         		array('date', false, array('YYYY'))
     		) 
         ));
 
 		// Add label field
-        $this->addElement('text', 'label', array(
+        $this->addElement('text', 'dc:publisher', array(
             'label'      => 'Label:',
+            'class'      => 'input_text',
             'filters'    => array('StringTrim')
         ));
 
 		// Add composer field
-        $this->addElement('text', 'composer', array(
+        $this->addElement('text', 'ls:composer', array(
             'label'      => 'Composer:',
+            'class'      => 'input_text',
             'filters'    => array('StringTrim')
         ));
 
 		// Add mood field
-        $this->addElement('text', 'mood', array(
+        $this->addElement('text', 'ls:mood', array(
             'label'      => 'Mood:',
+            'class'      => 'input_text',
             'filters'    => array('StringTrim')
         ));
 
 		// Add language field
-        $this->addElement('text', 'language', array(
+        $this->addElement('text', 'dc:language', array(
             'label'      => 'Language:',
+            'class'      => 'input_text',
             'filters'    => array('StringTrim')
         ));
 
 		// Add the submit button
         $this->addElement('submit', 'submit', array(
             'ignore'   => true,
+            'class'    => 'ui-button ui-state-default',
             'label'    => 'Submit',
         ));
     }
