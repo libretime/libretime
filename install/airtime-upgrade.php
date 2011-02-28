2818cc95b7692904e27daac20aac74bca8a07287
martin <martin@martin-ThinkPad-T410.(none)>
2011-02-28 13:27:12 -0500

    -added liquibase support

---
+++ install/airtime-upgrade.php
@@ -0,0 +1,10 @@
+<?php
+
+$command = __DIR__."/../library/liquibase/liquibase --driver=org.postgresql.Driver "
+    ."--classpath=".__DIR__."/../library/liquibase/lib/postgresql-9.0-801.jdbc4.jar "
+    ."--changeLogFile=".__DIR__."/upgrade/db.changelog.xml "
+    ."--url=\"jdbc:postgresql://localhost:5432/airtime\" "
+    ."--username=airtime "
+    ."--password=airtime "
+    ."migrate";
+system($command);
