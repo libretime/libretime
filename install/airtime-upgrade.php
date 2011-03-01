781ceeaa7f1d44fa4293eda451e62c81b6b5f59b
martin <martin@martin-ThinkPad-T410.(none)>
2011-02-28 23:51:42 -0500

    -web streaming enabled in liquidsoap script. -liquisoap scripts updated

--- install/airtime-upgrade.php
+++ install/airtime-upgrade.php
@@ -2,7 +2,7 @@
 
 $command = __DIR__."/../library/liquibase/liquibase --driver=org.postgresql.Driver "
     ."--classpath=".__DIR__."/../library/liquibase/lib/postgresql-9.0-801.jdbc4.jar "
-    ."--changeLogFile=".__DIR__."/upgrade/db.changelog.xml "
+    ."--changeLogFile=".__DIR__."/upgrade/db.changelog-master.xml "
     ."--url=\"jdbc:postgresql://localhost:5432/airtime\" "
     ."--username=airtime "
     ."--password=airtime "
