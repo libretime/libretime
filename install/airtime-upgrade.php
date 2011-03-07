a51c02b28137867882b0d64db1666c7a0abdc0ae
martin <martin@martin-ThinkPad-T410.(none)>
2011-03-07 17:20:33 -0500

    -removed liquibase stuff. Preparing for doctrine...

--- install/airtime-upgrade.php
+++ install/airtime-upgrade.php
@@ -1,10 +1,3 @@
 <?php
 
-$command = __DIR__."/../library/liquibase/liquibase --driver=org.postgresql.Driver "
-    ."--classpath=".__DIR__."/../library/liquibase/lib/postgresql-9.0-801.jdbc4.jar "
-    ."--changeLogFile=".__DIR__."/upgrade/db.changelog-master.xml "
-    ."--url=\"jdbc:postgresql://localhost:5432/airtime\" "
-    ."--username=airtime "
-    ."--password=airtime "
-    ."migrate";
-system($command);
+//doctrine stuff here soon....
