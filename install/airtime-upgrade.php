3253d521d1707a9aff218be2cc7382313ee03b70
mkonecny <martin.konecny@sourcefabric.org>
2011-03-08 00:52:36 -0500

    -hardcoded paths for doctrine migrations removed.

--- install/airtime-upgrade.php
+++ install/airtime-upgrade.php
@@ -2,6 +2,6 @@
 
 $dir = __DIR__;
 
-$command = "php $dir/../library/doctrine/migrations/doctrine-migrations.phar --configuration=$dir/DoctrineMigrations/migrations.xml --db-configuration=$dir/../library/doctrine/migrations/migrations-db.php migrations:migrate 0";
+$command = "php $dir/../library/doctrine/migrations/doctrine-migrations.phar --configuration=$dir/DoctrineMigrations/migrations.xml --db-configuration=$dir/../library/doctrine/migrations/migrations-db.php migrations:status";
 system($command);
 
