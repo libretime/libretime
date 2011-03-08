2a8e8715d238294ac7469a60372bd8655287f7b1
martin <martin@martin-ThinkPad-T410.(none)>
2011-03-08 17:02:26 -0500

    -migrations uses object mapping to update database

--- install/DoctrineMigrations/Version20110308003959.php
+++ install/DoctrineMigrations/Version20110308003959.php
@@ -9,13 +9,13 @@ class Version20110308003959 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $table = $this->getTable("cc_show_instances");
-        $table->addColumn("record", "boolean");
+        $table = $schema->getTable("cc_show_instances");
+        $table->addColumn("record", "boolean", array( 'notnull' => 0, 'default' => 0));
     }
 
     public function down(Schema $schema)
     {
-        $table = $this->getTable("cc_show_instances");
+        $table = $schema->getTable("cc_show_instances");
         $table->dropColumn("record");
     }
 }
--- install/airtime-upgrade.php
+++ install/airtime-upgrade.php
@@ -2,6 +2,6 @@
 
 $dir = __DIR__;
 
-$command = "php $dir/../library/doctrine/migrations/doctrine-migrations.phar --configuration=$dir/DoctrineMigrations/migrations.xml --db-configuration=$dir/../library/doctrine/migrations/migrations-db.php --no-interaction migrations:diff";
+$command = "php $dir/../library/doctrine/migrations/doctrine-migrations.phar --configuration=$dir/DoctrineMigrations/migrations.xml --db-configuration=$dir/../library/doctrine/migrations/migrations-db.php --no-interaction migrations:migrate";
 system($command);
 
