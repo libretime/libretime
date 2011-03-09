0d0b8e380b59be1cb836068463d7711e2f77e85f
Paul Baranowski <paul.baranowski@sourcefabric.org>
2011-03-09 18:21:29 -0500

    Minor tweaks to some frontend text.

--- application/forms/AddUser.php
+++ application/forms/AddUser.php
@@ -81,7 +81,7 @@ class Application_Form_AddUser extends Zend_Form
         $submit = new Zend_Form_Element_Submit('submit');
         $submit->setAttrib('class', 'ui-button ui-state-default right-floated');
         $submit->setIgnore(true);
-        $submit->setLabel('submit');
+        $submit->setLabel('Submit');
         $this->addElement($submit);
     }
     
