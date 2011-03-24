bf925901c19dc9fbc28f09714b7491d76259f604
martin <martin@martin-ThinkPad-T410.(none)>
2011-03-24 00:52:35 -0400

    CC-2016: Rearrange python scripts for reusability -Fixed pypo-notify.py not being to find new location of api_clients module

--- python_apps/pypo/install/pypo-daemontools-liquidsoap.sh
+++ python_apps/pypo/install/pypo-daemontools-liquidsoap.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 ls_user="pypo"
 export HOME="/home/pypo/"
+api_client_path="/opt/pypo/"
 ls_path="/opt/pypo/bin/liquidsoap/liquidsoap"
 ls_param="/opt/pypo/bin/scripts/ls_script.liq"
 echo "*** Daemontools: starting liquidsoap"
 exec 2>&1
-echo "exec sudo -u ${ls_user} ${ls_path} ${ls_param} "
-cd /opt/pypo/bin/scripts && sudo -u ${ls_user} ${ls_path} ${ls_param} 
+
+PYTHONPATH=${api_client_path}:$PYTHONPATH
+export PYTHONPATH
+
+cd /opt/pypo/bin/scripts
+exec setuidgid ${ls_user} \
+               ${ls_path} ${ls_param}
 # EOF
