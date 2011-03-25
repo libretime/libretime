091938d34ea79b82ce8f82acfb969c462ab9c8cf
naomiaro <naomiaro@gmail.com>
2011-03-25 00:19:44 -0400

    CC-2097 : Show-recorder: Use RabbitMQ, api_client, and the structure of pypofetch
    fixing daemon tool to find logging.cfg

--- python_apps/show-recorder/install/recorder-daemontools.sh
+++ python_apps/show-recorder/install/recorder-daemontools.sh
@@ -7,10 +7,11 @@ recorder_path="/opt/recorder/bin/"
 recorder_script="testrecordscript.py"
 
 api_client_path="/opt/pypo/"
+cd ${recorder_path}
 
 echo "*** Daemontools: starting daemon"
 exec 2>&1
 # Note the -u when calling python! we need it to get unbuffered binary stdout and stderr
 
-sudo PYTHONPATH=${api_client_path} -u ${recorder_user} python -u ${recorder_path}${recorder_script} -f
+sudo PYTHONPATH=${api_client_path} -u ${recorder_user} python -u ${recorder_path}${recorder_script}
 # EOF
