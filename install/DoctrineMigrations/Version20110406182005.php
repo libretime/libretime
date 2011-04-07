df70185ae67c15a089231c3291c0da1475891fe4
Naomi <naomi@naomi-DX4840.(none)>
2011-04-07 14:15:19 -0400

    CC-2146 : Speed up calendar when there are a large number of shows
    new doctrine migrations for adding aggregate column, fixing some things from 1.7 upgrade.

---
+++ install/DoctrineMigrations/Version20110406182005.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+class Version20110406182005 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        //start cc_show_instances modifications
+        $cc_show_instances = $schema->getTable('cc_show_instances');
+
+        $cc_show_instances->addColumn('time_filled', 'time',  array('notnull' => false));
+        //end cc_show_instances modifications
+
+        //start cc_show_instances modifications
+        $cc_show_rebroadcast = $schema->getTable('cc_show_rebroadcast');
+        $cc_show_rebroadcast->dropColumn('start_time');
+        $cc_show_rebroadcast->addColumn('start_time', 'time',  array('notnull' => true));
+        //end cc_show_instances modifications
+
+    }
+
+    public function down(Schema $schema)
+    {
+        //start cc_show_instances modifications
+        $cc_show_instances = $schema->getTable('cc_show_instances');
+
+        $cc_show_instances->dropColumn('time_filled');
+        //end cc_show_instances modifications
+
+        //start cc_show_instances modifications
+        $cc_show_rebroadcast = $schema->getTable('cc_show_rebroadcast');
+        $cc_show_rebroadcast->dropColumn('start_time');
+        $cc_show_rebroadcast->addColumn('start_time', 'datetime',  array('notnull' => 1));
+        //end cc_show_instances modifications
+    }
+}
