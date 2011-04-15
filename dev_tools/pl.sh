6db16bc6d11f34626babca0f5a77041028d0db2a
martin <martin@martin-ThinkPad-T410.(none)>
2011-04-15 12:27:25 -0400

    -update paths logs for utils in dev_tools/

--- dev_tools/pl.sh
+++ dev_tools/pl.sh
@@ -2,4 +2,4 @@
 echo
 echo "This will tail the pypo-liquidsoap log file."
 echo "Type in password for pypo user (default password is 'pypo'):"
-su -l pypo -c "tail -F /etc/service/pypo-liquidsoap/log/main/current"
+su -l pypo -c "tail -F /var/log/airtime/pypo-liquidsoap/main/current"
--- dev_tools/pr.sh
+++ dev_tools/pr.sh
@@ -4,4 +4,4 @@ echo
 echo "This will tail the recorder log file."
 echo "Type in password for pypo user (default password is 'pypo'):"
 
-su -l pypo -c "tail -F /etc/service/recorder/log/main/current"
+su -l pypo -c "tail -F /var/log/airtime/recorder/log/main/current"
