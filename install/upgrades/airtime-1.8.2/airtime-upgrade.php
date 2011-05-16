5e2e3dbf05329ceea6aa191eac57701b13382d3e
Naomi <naomi@naomi-DX4840.(none)>
2011-05-16 17:29:18 -0400

    CC-2256 Airtime-upgrade.php does not upgrade the web app

--- install/upgrades/airtime-1.8.2/airtime-upgrade.php
+++ install/upgrades/airtime-1.8.2/airtime-upgrade.php
@@ -7,6 +7,8 @@
  */
 
 set_include_path(__DIR__.'/../../../airtime_mvc/library' . PATH_SEPARATOR . get_include_path());
+require_once __DIR__.'/../../../airtime_mvc/application/configs/conf.php';
+require_once(dirname(__FILE__).'/../../include/AirtimeInstall.php');
 require_once(dirname(__FILE__).'/../../include/AirtimeIni.php');
 
 const CONF_FILE_AIRTIME = "/etc/airtime/airtime.conf";
@@ -31,9 +33,6 @@ AirtimeIni::CreateIniFiles();
 echo "* Initializing INI files".PHP_EOL;
 AirtimeIni::MergeConfigFiles($configFiles, $suffix);
 
-
-require_once __DIR__.'/../../../airtime_mvc/application/configs/conf.php';
-require_once(dirname(__FILE__).'/../../include/AirtimeInstall.php');
 global $CC_CONFIG;
 $CC_CONFIG = Config::loadConfig($CC_CONFIG);
 
