658570c670f67419afd8272977a1efe69b867354
james <james@sourcefabric-DX4840.(none)>
2011-05-30 16:04:52 -0400

    CC-2277:Rename airtime-pypo to airtime-playout
    done

--- install/upgrades/airtime-1.9/airtime-upgrade.php
+++ install/upgrades/airtime-1.9/airtime-upgrade.php
@@ -11,7 +11,10 @@ require_once __DIR__.'/../../../airtime_mvc/application/configs/conf.php';
 require_once(dirname(__FILE__).'/../../include/AirtimeInstall.php');
 require_once(dirname(__FILE__).'/../../include/AirtimeIni.php');
 
-unlink('/usr/bin/airtime-pypo-start');
-unlink('/usr/bin/airtime-pypo-stop');
+// clean up old files
+@unlink('/usr/bin/airtime-pypo-start');
+@unlink('/usr/bin/airtime-pypo-stop');
+@unlink(dirname(__FILE__).'/../../../python_apps/pypo/airtime-pypo-start');
+@unlink(dirname(__FILE__).'/../../../python_apps/pypo/airtime-pypo-stop');
 
 AirtimeInstall::CreateZendPhpLogFile();
