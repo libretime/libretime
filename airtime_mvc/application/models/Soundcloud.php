27075ad369bc1b3ce9ff27469a8d0fff99e17000
Naomi Aro <naomiaro@gmail.com>
2011-06-03 18:12:10 -0400

    CC-2356 Default genre when uploading to Soundcloud doesn't work
    function call changed incorrectly.

--- airtime_mvc/application/models/Soundcloud.php
+++ airtime_mvc/application/models/Soundcloud.php
@@ -28,7 +28,7 @@ class ATSoundcloud {
         return $token;
     }
 
-    public function uploadTrack($filepath, $filename, $description, $tags=array(), $release=null, $genre=null) 
+    public function uploadTrack($filepath, $filename, $description, $tags=array(), $release=null, $genre=null)
     {
         if($this->getToken())
         {
@@ -47,7 +47,7 @@ class ATSoundcloud {
                 'track[tag_list]' => $tags,
                 'track[description]' => $description,
                 'track[downloadable]' => true,
-                
+
             );
 
             if(isset($release)) {
@@ -61,12 +61,12 @@ class ATSoundcloud {
                 $track_data['track[release_month]'] = $release[1];
                 $track_data['track[release_day]'] = $release[2];
             }
-       
+
             if (isset($genre) && $genre != "") {
                 $track_data['track[genre]'] = $genre;
             }
             else {
-                $default_genre = Application_Model_Preference::GetSoundCloudTrackType();
+                $default_genre = Application_Model_Preference::GetSoundCloudGenre();
                 if ($genre != "") {
                     $track_data['track[genre]'] = $default_genre;
                 }
@@ -88,7 +88,7 @@ class ATSoundcloud {
             );
 
             return $response["id"];
-        }  
+        }
     }
 
 }
