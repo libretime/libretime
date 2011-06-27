6a7f1d35880f206fa3ae04f3895f8c111df53e9b
james <james@sourcefabric-DX4840.(none)>
2011-06-27 14:37:31 -0400

    CC-2412:Error causes box to float
    - fixed

--- airtime_mvc/application/forms/AddShowWhen.php
+++ airtime_mvc/application/forms/AddShowWhen.php
@@ -5,64 +5,71 @@ class Application_Form_AddShowWhen extends Zend_Form_SubForm
 
     public function init()
     {
-
-        //$this->setDisableLoadDefaultDecorators(true);
-        //$this->removeDecorator('DtDdWrapper');
-
-		// Add start date element
-        $this->addElement('text', 'add_show_start_date', array(
-            'label'      => 'Date/Time Start:',
-            'class'      => 'input_text',
-            'required'   => true,
-            'value'     => date("Y-m-d"),
-            'filters'    => array('StringTrim'),
-            'validators' => array(
-				'NotEmpty',
-        		array('date', false, array('YYYY-MM-DD'))
-    		)
-        ));
+        // Add start date element
+        $startDate = new Zend_Form_Element_Text('add_show_start_date');
+        $startDate->class = 'input_text';
+        $startDate->setRequired(true)
+                    ->setLabel('Date/Time Start:')
+                    ->setValue(date("Y-m-d"))
+                    ->setFilters(array('StringTrim'))
+                    ->setValidators(array(
+                        'NotEmpty',
+                        array('date', false, array('YYYY-MM-DD'))))
+                    ->setDecorators(array(
+                        array(array('open'=>'HtmlTag'), array('tag' => 'dd', 'openOnly'=>true)),
+                        'ViewHelper',
+                        'Description',
+                        array('Label', array('tag' =>'dt'))));
+        $this->addElement($startDate);
         
-		// Add start time element
-        $startTime = $this->addElement('text', 'add_show_start_time', array(
-        	'decorators' => array('ViewHelper', array('HtmlTag', array('tag'=>'dd'))),
-            'class'      => 'input_text',
-            'required'   => true,
-        	'value'		 => '00:00',
-            'filters'    => array('StringTrim'),
-			'validators' => array(
-				'NotEmpty',
-        		array('date', false, array('HH:mm')),
-                array('regex', false, array('/^[0-9:]+$/', 'messages' => 'Invalid character entered'))
-    		)
-        ));
+        // Add start time element
+        $startTime = new Zend_Form_Element_Text('add_show_start_time');
+        $startTime->class = 'input_text';
+        $startTime->setRequired(true)
+                    ->setValue('00:00')
+                    ->setFilters(array('StringTrim'))
+                    ->setValidators(array(
+                        'NotEmpty',
+                        array('date', false, array('HH:mm')),
+                        array('regex', false, array('/^[0-9:]+$/', 'messages' => 'Invalid character entered'))))
+                    ->setDecorators(array(
+                        'ViewHelper',
+                        'Errors',
+                        array(array('close'=>'HtmlTag'), array('tag' => 'dd', 'closeOnly'=>true))));
+        $this->addElement($startTime);
 
         // Add end date element
-        $this->addElement('text', 'add_show_end_date_no_repeat', array(
-            'label'      => 'Date/Time End:',
-            'class'      => 'input_text',
-            'required'   => true,
-            'value'     => date("Y-m-d"),
-            'filters'    => array('StringTrim'),
-            'validators' => array(
-				'NotEmpty',
-        		array('date', false, array('YYYY-MM-DD'))
-    		)
-        ));
+        $endDate = new Zend_Form_Element_Text('add_show_end_date_no_repeat');
+        $endDate->class = 'input_text';
+        $endDate->setRequired(true)
+                    ->setLabel('Date/Time End:')
+                    ->setValue(date("Y-m-d"))
+                    ->setFilters(array('StringTrim'))
+                    ->setValidators(array(
+                        'NotEmpty',
+                        array('date', false, array('YYYY-MM-DD'))))
+                    ->setDecorators(array(
+                        array(array('open'=>'HtmlTag'), array('tag' => 'dd', 'openOnly'=>true)),
+                        'ViewHelper',
+                        'Description',
+                        array('Label', array('tag' =>'dt'))));
+        $this->addElement($endDate);
         
         // Add end time element
-        $this->addElement('text', 'add_show_end_time', array(
-        	'decorators' => array('ViewHelper', array('HtmlTag', array('tag'=>'dd'))),
-            'class'      => 'input_text',
-            'required'   => true,
-        	'value'		 => '01:00',
-            'filters'    => array('StringTrim'),
-            'validators' => array(
-                'NotEmpty',
-                array('date', false, array('HH:mm')),
-                array('regex', false, array('/^[0-9:]+$/', 'messages' => 'Invalid character entered'))
-            )
-                
-        ));
+        $endTime = new Zend_Form_Element_Text('add_show_end_time');
+        $endTime->class = 'input_text';
+        $endTime->setRequired(true)
+                    ->setValue('01:00')
+                    ->setFilters(array('StringTrim'))
+                    ->setValidators(array(
+                        'NotEmpty',
+                        array('date', false, array('HH:mm')),
+                        array('regex', false, array('/^[0-9:]+$/', 'messages' => 'Invalid character entered'))))
+                    ->setDecorators(array(
+                        'ViewHelper',
+                        'Errors',
+                        array(array('close'=>'HtmlTag'), array('tag' => 'dd', 'closeOnly'=>true))));
+        $this->addElement($endTime);
         
         // Add duration element
         $this->addElement('text', 'add_show_duration', array(
@@ -93,7 +100,7 @@ class Application_Form_AddShowWhen extends Zend_Form_SubForm
 
 		if ((($formData['add_show_id'] != -1) && $startDateModified) || ($formData['add_show_id'] == -1)){
 	        if($start_epoch < $now_epoch) {
-	            $this->getElement('add_show_start_date')->setErrors(array('Cannot create show in the past'));
+	            $this->getElement('add_show_start_time')->setErrors(array('Cannot create show in the past'));
 	            $valid = false;
 	        }
 	    }
