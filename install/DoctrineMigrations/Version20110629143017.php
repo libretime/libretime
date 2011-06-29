c300b8f36dc46a37117ec6bbecfe60ce677cd644
Naomi Aro <naomiaro@gmail.com>
2011-06-29 14:32:17 +0200

    CC-2279 : Upgrade script for converting stor directory to new format
    correcting paths for doctrine migrations, generating my db file.

---
+++ install/DoctrineMigrations/Version20110629143017.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+class Version20110629143017 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+
+    }
+
+    public function down(Schema $schema)
+    {
+
+    }
+}
