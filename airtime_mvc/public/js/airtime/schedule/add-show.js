48537c3c10df77221fd24472ca9a83b6b65fee29
james <james@james-P35-DS3L.(none)>
2011-06-30 12:15:54 -0400

    CC-2484:Calendar sholdn't move down
    - the fix was fairly easy. so I did complete fix.

--- airtime_mvc/public/js/airtime/schedule/add-show.js
+++ airtime_mvc/public/js/airtime/schedule/add-show.js
@@ -423,6 +423,15 @@ $(document).ready(function() {
 	//setAddShowEvents();
 });
 
+$(window).resize(function(){
+	var windowWidth = $(this).width();
+    // margin on showform are 16 px on each side
+    var calendarWidth = 100-(($("#schedule-add-show").width() + (16 * 6))/windowWidth*100);
+    var widthPercent = parseInt(calendarWidth)+"%";
+    $("#schedule_calendar").css("width", widthPercent);
+    $("#schedule_calendar").fullCalendar('render');
+});
+
 $(window).load(function() {
 
 	setAddShowEvents();
--- airtime_mvc/public/js/airtime/schedule/full-calendar-functions.js
+++ airtime_mvc/public/js/airtime/schedule/full-calendar-functions.js
@@ -13,7 +13,11 @@ function openAddShowForm() {
      if($("#add-show-form").length == 1) {
         if( ($("#add-show-form").css('display')=='none')) {
             $("#add-show-form").show();
-            $("#schedule_calendar").css("width", "70%");
+            var windowWidth = $(window).width();
+            // margin on showform are 16 px on each side
+            var calendarWidth = 100-(($("#schedule-add-show").width() + (16 * 6))/windowWidth*100);
+            var widthPercent = parseInt(calendarWidth)+"%";
+            $("#schedule_calendar").css("width", widthPercent);
             $("#schedule_calendar").fullCalendar('render');
         }
         $("#schedule-show-what").show(0, function(){
