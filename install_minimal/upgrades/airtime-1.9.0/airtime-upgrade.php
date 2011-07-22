f7413cacf6f71075e0e1c39480a4eaba203fc29e
Naomi Aro <naomiaro@gmail.com>
2011-07-22 15:32:01 +0200

    CC-2562 : Allow users to convert linked files into watched directories

--- install_minimal/upgrades/airtime-1.9.0/airtime-upgrade.php
+++ install_minimal/upgrades/airtime-1.9.0/airtime-upgrade.php
@@ -8,6 +8,7 @@
 
 set_include_path(__DIR__.'/../../../airtime_mvc/library' . PATH_SEPARATOR . get_include_path());
 set_include_path(__DIR__.'/../../../airtime_mvc/library/pear' . PATH_SEPARATOR . get_include_path());
+set_include_path(__DIR__.'/../../../airtime_mvc/application/models' . PATH_SEPARATOR . get_include_path());
 require_once 'conf.php';
 require_once 'DB.php';
 
