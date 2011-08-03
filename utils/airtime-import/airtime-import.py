b5f232ccaf9a492fcbcef47f1a31930a4967543f
James <james@sourcefabric-DX4840.(none)>
2011-08-03 15:28:40 -0400

    CC-2633: airtime-import of relative path crashes
    - fixed
    - using copyfile() instead of copy()

--- utils/airtime-import/airtime-import.py
+++ utils/airtime-import/airtime-import.py
@@ -56,7 +56,7 @@ def copy_or_move_files_to(paths, dest, flag):
                     destfile = dest+os.path.basename(path)
                     if(flag == 'copy'):
                         print "Copying %(src)s to %(dest)s..." % {'src':path, 'dest':destfile}
-                        shutil.copy(path, destfile)
+                        shutil.copyfile(path, destfile)
                     elif(flag == 'move'):
                         print "Moving %(src)s to %(dest)s..." % {'src':path, 'dest':destfile}
                         shutil.move(path, destfile)
