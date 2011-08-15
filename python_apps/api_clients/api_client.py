e18c0903cb66903962b1606c4c7ac053be970cb5
James <james@sourcefabric-DX4840.(none)>
2011-08-15 11:11:55 -0400

    -version string changes

--- python_apps/api_clients/api_client.py
+++ python_apps/api_clients/api_client.py
@@ -21,7 +21,7 @@ from urlparse import urlparse
 import base64
 from configobj import ConfigObj
 
-AIRTIME_VERSION = "2.0.0"
+AIRTIME_VERSION = "2.0.0-beta1"
 
 def api_client_factory(config):
     logger = logging.getLogger()
