805e358d9cdf6dd03807c9a8d5abefb46eaef66e
martin <martin@martin-ThinkPad-T410.(none)>
2011-09-08 12:36:51 -0400

    CC-2788: Airtime uses both /bin/bash and /bin/sh in different scripts
    -fixed

--- dev_tools/pypotail.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo
-echo "This will tail the pypo log file."
-echo "Type in password for pypo user (default password is 'pypo'):"
-
-su -l pypo -c "tail -F /var/log/airtime/pypo/main/current"
