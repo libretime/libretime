7c94734277bfdff22bb64f540ec69cbc523851a2
James <james@sourcefabric-DX4840.(none)>
2011-09-16 14:01:23 -0400

    CC-2810: More chmnod fixes for scripts
    - removed shebang from .py files
    - removed cron folder and its content as we don't use them
    - .js files are from 3rd party(datatable) so we should leave them as they are

--- airtime_mvc/application/models/cron/croncall.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/php
-<?php
-chdir(dirname(__FILE__));
-$p =  unserialize($argv[1]);
-require_once (dirname(__FILE__).'/'.$p['class'].'.php');
-$cronjob = new $p['class']();
-$ret = $cronjob->execute($p['params']);
-exit(0);
