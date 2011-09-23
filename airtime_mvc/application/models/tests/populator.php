693c72c39692fe16b11b785784e375cd49088b90
martin <martin@martin-ThinkPad-T410.(none)>
2011-09-23 10:56:12 -0400

    -change Shows.php to Show.php

--- airtime_mvc/application/models/tests/populator.php
+++ airtime_mvc/application/models/tests/populator.php
@@ -1,7 +1,7 @@
 <?php
 set_include_path(__DIR__.'/..' . PATH_SEPARATOR . get_include_path());
 set_include_path(__DIR__.'/../../../library' . PATH_SEPARATOR . get_include_path());
-require_once __DIR__.'/../Shows.php';
+require_once __DIR__.'/../Show.php';
 require_once __DIR__.'/../StoredFile.php';
 require_once __DIR__.'/../Playlist.php';
 require_once __DIR__.'/../Schedule.php';
