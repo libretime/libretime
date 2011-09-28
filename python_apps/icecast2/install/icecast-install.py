79f8e36ee439740e7e8ffe9927a03729beacae8d
martin <martin@martin-ThinkPad-T410.(none)>
2011-09-28 17:46:37 -0400

    CC-2886: airtime-check-system should not fail Icecast check  if Icecast outputs are not configured in  /etc/airtime/liquidsoap.cfg

---
+++ python_apps/icecast2/install/icecast-install.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import shutil
+import os
+import sys
+
+if os.geteuid() != 0:
+    print "Please run this as root."
+    sys.exit(1)
+
+def get_current_script_dir():
+  current_script_dir = os.path.realpath(__file__)
+  index = current_script_dir.rindex('/')
+  return current_script_dir[0:index]
+
+try:
+    current_script_dir = get_current_script_dir()
+    shutil.copy(current_script_dir+"/../airtime-icecast-status.xsl", "/usr/share/icecast2/web")
+
+except Exception, e:
+    print "exception: %s" % e
+    sys.exit(1)
