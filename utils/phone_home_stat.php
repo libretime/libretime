38f2290214ed5e4638e75035ab00dcabdbda9736
Martin Konecny <martin@martin-ThinkPad-T410.(none)>
2011-10-13 23:35:18 +0200

    CC-2972: Find better way to determine domain which pypo should download from.
    -remove basePort and baseUrl from code + config file, since it isn't used anywhere.

--- utils/phone_home_stat.php
+++ utils/phone_home_stat.php
@@ -6,8 +6,8 @@ $CC_CONFIG['webServerUser'] = $values['general']['web_server_user'];
 $CC_CONFIG['phpDir'] = $values['general']['airtime_dir'];
 $CC_CONFIG['rabbitmq'] = $values['rabbitmq'];
 
-$CC_CONFIG['baseUrl'] = $values['general']['base_url'];
-$CC_CONFIG['basePort'] = $values['general']['base_port'];
+//$CC_CONFIG['baseUrl'] = $values['general']['base_url'];
+//$CC_CONFIG['basePort'] = $values['general']['base_port'];
 
 // Database config
 $CC_CONFIG['dsn']['username'] = $values['database']['dbuser'];
