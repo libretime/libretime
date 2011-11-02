3675e3c6e779cd65142594bd1b1f1546f6484884
Martin Konecny <martin@localhost.localdomain>
2011-11-02 15:34:47 -0400

    Airtime 1.9.5 upgrade script

--- install_minimal/include/airtime-upgrade.php
+++ install_minimal/include/airtime-upgrade.php
@@ -77,6 +77,9 @@ if (strcmp($version, "1.9.3") < 0){
 if (strcmp($version, "1.9.4") < 0){
     passthru("php --php-ini $SCRIPTPATH/../airtime-php.ini $SCRIPTPATH/../upgrades/airtime-1.9.4/airtime-upgrade.php");
 }
+if (strcmp($version, "1.9.5") < 0){
+    passthru("php --php-ini $SCRIPTPATH/../airtime-php.ini $SCRIPTPATH/../upgrades/airtime-1.9.5/airtime-upgrade.php");
+}
 
 //set the new version in the database.
 $sql = "DELETE FROM cc_pref WHERE keystr = 'system_version'";
