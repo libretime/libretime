798fd4827477a627c0ed239969da2526c8f51f8d
Martin Konecny <martin@localhost.localdomain>
2011-11-22 12:27:36 -0500

    CC-3081: Upgrade 1.9.5->2.0 fails (PHP fatal error on db)
    -fixed

--- install_minimal/DoctrineMigrations/Version20111114222927.php
+++ install_minimal/DoctrineMigrations/Version20111114222927.php
@@ -10,7 +10,7 @@ class Version20111114222927 extends AbstractMigration
     public function up(Schema $schema)
     {
         $cc_show_instances = $schema->getTable('cc_show_instances');
-        $cc_show_instances->addColumn('deleted_instance', 'boolean', array('notnull' => true, 'default'=> '0'));
+        $cc_show_instances->addColumn('modified_instance', 'boolean', array('notnull' => true, 'default'=> '0'));
     }
 
     public function down(Schema $schema)
