7d605303126f17bdac1099494c01e6ed983a76b6
Martin Konecny <martin@localhost.localdomain>
2011-12-15 11:22:22 -0500

    CC-3206: Add ability to do Airtime upgrade without restarting liquidsoap
    -done

--- python_apps/pypo/install/pypo-initialize.py
+++ python_apps/pypo/install/pypo-initialize.py
@@ -109,7 +109,7 @@ try:
         sts = os.waitpid(p.pid, 0)[1]
     else:
         print " * Keeping any previous Liquidsoap instances running"
-        p = Popen("/etc/init.d/airtime-playout pypo_stop", shell=True)
+        p = Popen("/etc/init.d/airtime-playout pypo-stop", shell=True)
         sts = os.waitpid(p.pid, 0)[1]
     p = Popen("/etc/init.d/airtime-playout start-no-monit", shell=True)
     sts = os.waitpid(p.pid, 0)[1]
