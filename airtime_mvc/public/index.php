6c0f035b0b936fb58b0dd5bda5e24995ab8414c0
Martin Konecny <martin@localhost.localdomain>
2012-02-08 11:42:21 -0500

    CC-3307: airtime-full-install fails on Ubuntu 11.10

--- airtime_mvc/public/index.php
+++ airtime_mvc/public/index.php
@@ -24,6 +24,11 @@ set_include_path(APPLICATION_PATH . '/models' . PATH_SEPARATOR . get_include_pat
 //Controller plugins.
 set_include_path(APPLICATION_PATH . '/controllers/plugins' . PATH_SEPARATOR . get_include_path());
 
+//Zend framework
+if (file_exists('/usr/share/php/libzend-framework-php')){
+    set_include_path('/usr/share/php/libzend-framework-php' . PATH_SEPARATOR . get_include_path());
+}
+
 /** Zend_Application */
 require_once 'Zend/Application.php';
 
--- install_minimal/include/AirtimeInstall.php
+++ install_minimal/include/AirtimeInstall.php
@@ -1,5 +1,9 @@
 <?php
 set_include_path(__DIR__.'/../../airtime_mvc/library' . PATH_SEPARATOR . get_include_path());
+//Zend framework
+if (file_exists('/usr/share/php/libzend-framework-php')){
+    set_include_path('/usr/share/php/libzend-framework-php' . PATH_SEPARATOR . get_include_path());
+}
 require_once('Zend/Loader/Autoloader.php');
 require_once('DB.php');
 
