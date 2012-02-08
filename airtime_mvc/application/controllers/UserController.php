33fcbddcb9b4426f5135458eb7ba01b605cab35d
James <james@sourcefabric-DX4840.(none)>
2012-02-08 16:21:57 -0500

    CC-3284: Don't make browser users clear their cache when Airtime is upgraded
    - use airtime version string instead of filemtime function
    
    Conflicts:
    
    	airtime_mvc/application/Bootstrap.php
    	airtime_mvc/application/controllers/LibraryController.php
    	airtime_mvc/application/controllers/ScheduleController.php

--- airtime_mvc/application/controllers/UserController.php
+++ airtime_mvc/application/controllers/UserController.php
@@ -23,11 +23,10 @@ class UserController extends Zend_Controller_Action
         
         $request = $this->getRequest();
         $baseUrl = $request->getBaseUrl();
-        $baseDir = dirname($_SERVER['SCRIPT_FILENAME']);
         
-        $this->view->headScript()->appendFile($baseUrl.'/js/datatables/js/jquery.dataTables.js?'.filemtime($baseDir.'/js/datatables/js/jquery.dataTables.js'),'text/javascript');
-        $this->view->headScript()->appendFile($baseUrl.'/js/datatables/plugin/dataTables.pluginAPI.js?'.filemtime($baseDir.'/js/datatables/plugin/dataTables.pluginAPI.js'),'text/javascript');
-        $this->view->headScript()->appendFile($baseUrl.'/js/airtime/user/user.js?'.filemtime($baseDir.'/js/airtime/user/user.js'),'text/javascript');    
+        $this->view->headScript()->appendFile($baseUrl.'/js/datatables/js/jquery.dataTables.js?'.$CC_CONFIG['airtime_version'],'text/javascript');
+        $this->view->headScript()->appendFile($baseUrl.'/js/datatables/plugin/dataTables.pluginAPI.js?'.$CC_CONFIG['airtime_version'],'text/javascript');
+        $this->view->headScript()->appendFile($baseUrl.'/js/airtime/user/user.js?'.$CC_CONFIG['airtime_version'],'text/javascript');    
 
         $form = new Application_Form_AddUser();
 
