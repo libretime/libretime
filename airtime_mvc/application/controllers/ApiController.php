c5c1dce4d6354f13563204ba7e656ee5cb491de2
Martin Konecny <martin.konecny@gmail.com>
2012-02-22 17:29:49 -0500

    CC-3336: Refactor schedule API used by pypo
    -initial commit

--- airtime_mvc/application/controllers/ApiController.php
+++ airtime_mvc/application/controllers/ApiController.php
@@ -276,12 +276,14 @@ class ApiController extends Zend_Controller_Action
 
         $api_key = $this->_getParam('api_key');
 
+        /*
         if(!in_array($api_key, $CC_CONFIG["apiKey"]))
         {
             header('HTTP/1.0 401 Unauthorized');
             print 'You are not allowed to access this resource. ';
             exit;
         }
+        * */
 
         PEAR::setErrorHandling(PEAR_ERROR_RETURN);
 
