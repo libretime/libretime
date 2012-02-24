f92c4258902016a97c6b592477edf2ef2a1c8173
James <james@sourcefabric-DX4840.(none)>
2012-02-24 14:10:27 -0500

    CC-3345: Enter unusable state when liquidsoap value has an '=' character in it.
    - fixed

--- python_apps/pypo/pypofetch.py
+++ python_apps/pypo/pypofetch.py
@@ -134,7 +134,7 @@ class PypoFetch(Thread):
             # if empty line
             if not line:
                 continue
-            key, value = line.split('=')
+            key, value = line.split(' = ')
             key = key.strip()
             value = value.strip()
             value = value.replace('"', '')
