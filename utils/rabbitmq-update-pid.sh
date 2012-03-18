766414651945dfec98e2a38cbd89bbc188342db1
Martin Konecny <martin.konecny@gmail.com>
2012-03-18 02:21:30 -0400

    -getting rabbitmq pid should now work for all future Ubuntu versions

--- utils/rabbitmq-update-pid.sh
+++ utils/rabbitmq-update-pid.sh
@@ -4,12 +4,13 @@
 #done in our rabbitmq init.d script, but placing it here so that monit recognizes 
 # it faster (in time for the upcoming airtime-check-system)
 codename=`lsb_release -cs`
-if [ "$codename" == "oneiric" ];
+if [ "$codename" = "lucid" -o "$codename" = "maverick" -o "$codename" = "natty" -o "$codename" = "squeeze" ]
 then
+    rabbitmqpid=`sed "s/.*,\(.*\)\}.*/\1/" /var/lib/rabbitmq/pids`
+else
+    #RabbitMQ in Ubuntu Oneiric and newer have a different way of storing the PID.
     rabbitmqstatus=`/etc/init.d/rabbitmq-server status | grep "\[{pid"`
     rabbitmqpid=`echo $rabbitmqstatus | sed "s/.*,\(.*\)\}.*/\1/"`
-else
-    rabbitmqpid=`sed "s/.*,\(.*\)\}.*/\1/" /var/lib/rabbitmq/pids`
 fi
 echo "RabbitMQ PID: $rabbitmqpid"
 echo "$rabbitmqpid" > /var/run/rabbitmq.pid
