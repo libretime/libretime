b935c82b8386a81ee035001066dfdc187691622e
James <james@sourcefabric-DX4840.(none)>
2012-03-27 17:14:34 -0400

    CC-3519: System-> Stream> Problem with Master DJ Port and DJ Port settings
    - fixed

--- airtime_mvc/application/forms/LiveStreamingPreferences.php
+++ airtime_mvc/application/forms/LiveStreamingPreferences.php
@@ -132,24 +132,28 @@ class Application_Form_LiveStreamingPreferences extends Zend_Form_SubForm
             $element->addError("You cannot use same port as Master DJ port.");
         }
         if($master_harbor_input_port != ""){
-            $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-            $res = socket_bind($sock, 0, $master_harbor_input_port);
-            if(!$res){
-                $element = $this->getElement("master_harbor_input_port");
-                $element->addError("Port '$master_harbor_input_port' is not available.");
-                $isValid = false;
+            if($master_harbor_input_port != Application_Model_StreamSetting::GetMasterLiveSteamPort()){
+                $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+                $res = socket_bind($sock, 0, $master_harbor_input_port);
+                if(!$res){
+                    $element = $this->getElement("master_harbor_input_port");
+                    $element->addError("Port '$master_harbor_input_port' is not available.");
+                    $isValid = false;
+                }
+                socket_close($sock);
             }
-            socket_close($sock);
         }
         if($dj_harbor_input_port != ""){
-            $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-            $res = socket_bind($sock, 0, $dj_harbor_input_port);
-            if(!$res){
-                $element = $this->getElement("dj_harbor_input_port");
-                $element->addError("Port '$dj_harbor_input_port' is not available.");
-                $isValid = false;
+            if($dj_harbor_input_port != Application_Model_StreamSetting::GetDJLiveSteamPort()){
+                $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+                $res = socket_bind($sock, 0, $dj_harbor_input_port);
+                if(!$res){
+                    $element = $this->getElement("dj_harbor_input_port");
+                    $element->addError("Port '$dj_harbor_input_port' is not available.");
+                    $isValid = false;
+                }
+                socket_close($sock);
             }
-            socket_close($sock);
         }
         return $isValid;
     }
