1de7ffcd3f20a460dafb954f2509010aa79d07dd
James <james@sourcefabric-DX4840.(none)>
2012-04-03 14:41:37 -0400

    CC-3422: Upgrade Script: add/enhance upgrade script for relay stream feature
    - done

--- install_minimal/upgrades/airtime-2.1.0/common/Version20120402103944.php
+++ install_minimal/upgrades/airtime-2.1.0/common/Version20120402103944.php
@@ -15,8 +15,6 @@ class Version20120402103944 extends AbstractMigration
         
         $cc_files = $schema->getTable('cc_files');
         $cc_files->addColumn('file_exists', 'boolean', array('default'=> 'true'));
-        
-        //$cc_files->removeForeignKey('cc_music_dirs_folder_fkey');
         $cc_files->addNamedForeignKeyConstraint('cc_music_dirs_folder_fkey', $cc_music_dirs, array('directory'), array('id'));
         $cc_files->addIndex(array('file_exists'), 'cc_files_file_exists_idx');
     }
---
+++ install_minimal/upgrades/airtime-2.1.0/common/Version20120403143635.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+class Version20120403143635 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $cc_show = $schema->getTable('cc_show');
+        $cc_show->addColumn('live_stream_using_airtime_auth', 'boolean', array('default'=> 'false'));
+        $cc_show->addColumn('live_stream_using_custom_auth', 'boolean', array('default'=> 'false'));
+        $cc_show->addColumn('live_stream_user', 'string', array('length' => 255));
+        $cc_show->addColumn('live_stream_pass', 'string', array('length' => 255));
+    }
+
+    public function down(Schema $schema)
+    {
+
+    }
+}
