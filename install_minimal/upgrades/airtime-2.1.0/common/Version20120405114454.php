84efc6afb8503f6ab60ecc89465651df9940f913
Naomi Aro <naomiaro@gmail.com>
2012-04-05 16:05:19 +0200

    CC-3110 : Password reset

--- install_minimal/upgrades/airtime-2.1.0/common/Version20120405114454.php
+++ install_minimal/upgrades/airtime-2.1.0/common/Version20120405114454.php
@@ -13,7 +13,7 @@ class Version20120405114454 extends AbstractMigration
         $cc_subjs_token = $schema->createTable('cc_subjs_token');
         
         $cc_subjs_token->addColumn('id', 'integer', array('unsigned' => true, 'autoincrement' => true));
-        $cc_subjs_token->addColumn('show_id', 'integer',  array('notnull' => 1));
+        $cc_subjs_token->addColumn('user_id', 'integer',  array('notnull' => 1));
         $cc_subjs_token->addColumn('action', 'string', array('length' => 255, 'notnull' => 1));
         $cc_subjs_token->addColumn('token', 'string', array('length' => 40, 'notnull' => 1));
         $cc_subjs_token->addColumn('created', 'datetime',  array('notnull' => 1));
