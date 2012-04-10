3b80c7c018b14413e63f2922f988e7b4a42269bd
Naomi Aro <naomiaro@gmail.com>
2012-04-10 17:02:10 +0200

    CC-3605 : Create 2.1 upgrades

--- install_minimal/upgrades/airtime-2.1.0/common/Version20120410143340.php
+++ install_minimal/upgrades/airtime-2.1.0/common/Version20120410143340.php
@@ -9,10 +9,11 @@ class Version20120410143340 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        //add temp columns for changing bitrate and sample rate to integers.
-        $this->_addSql("ALTER TABLE cc_playlist ADD creator_id integer");
+        //convert column creator to be creator_id on cc_playlist
         
-        $this->_addSql("UPDATE cc_playlist SET creator_id = ()");
+        $this->_addSql("ALTER TABLE cc_playlist ADD creator_id integer");
+        $this->_addSql("UPDATE cc_playlist SET creator_id = (SELECT id FROM cc_subjs WHERE creator = login)");
+        $this->_addSql("ALTER TABLE cc_playlist DROP COLUMN creator");
     }
 
     public function down(Schema $schema)
