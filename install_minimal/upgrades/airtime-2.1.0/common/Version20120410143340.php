1bc208d7245cc8b77f7cfd687f20693a35cede80
Naomi Aro <naomiaro@gmail.com>
2012-04-10 14:46:17 +0200

    CC-3605 : Create 2.1 upgrades

---
+++ install_minimal/upgrades/airtime-2.1.0/common/Version20120410143340.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+class Version20120410143340 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+
+    }
+
+    public function down(Schema $schema)
+    {
+
+    }
+}
