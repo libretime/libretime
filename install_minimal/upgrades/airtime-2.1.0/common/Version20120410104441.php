222aad08e7dc6b0e928d0e13444e15e749adab39
Naomi Aro <naomiaro@gmail.com>
2012-04-10 12:26:33 +0200

    CC-3605 : Create 2.1 upgrades

--- install_minimal/upgrades/airtime-2.1.0/common/Version20120410104441.php
+++ install_minimal/upgrades/airtime-2.1.0/common/Version20120410104441.php
@@ -7,24 +7,16 @@ use Doctrine\DBAL\Migrations\AbstractMigration,
 
 class Version20120410104441 extends AbstractMigration
 {
-    public function preUp(Schema $schema) 
+    public function up(Schema $schema)
     {
-        //start changes to cc_files 
         //add temp columns for changing bitrate and sample rate to integers.
         $cc_files = $schema->getTable('cc_files');
         $cc_files->addColumn('temp_br', 'integer',  array('notnull' => 0));
         $cc_files->addColumn('temp_sr', 'integer', array('notnull' => 0));
-        //end changes to cc_files
-    }
-        
-    public function up(Schema $schema)
-    {
-        $this->_addSql("UPDATE cc_files SET temp_br = bit_rate::integer");
-        $this->_addSql("UPDATE cc_files SET temp_sr = sample_rate::integer");
-    }
     
-    public function postUp(Schema $schema)
-    {
+        $this->addSql("UPDATE cc_files SET temp_br = bit_rate::integer");
+        $this->addSql("UPDATE cc_files SET temp_sr = sample_rate::integer");
+        
         $cc_files = $schema->getTable('cc_files');
         $cc_files->dropColumn('bit_rate');
         $cc_files->dropColumn('sample_rate');
@@ -32,7 +24,7 @@ class Version20120410104441 extends AbstractMigration
         $cc_files->renameColumn('temp_br', 'bit_rate');
         $cc_files->renameColumn('temp_sr', 'sample_rate');
     }
-
+    
     public function down(Schema $schema)
     {
 
