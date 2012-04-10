bf5eca605c4c563fbbb18ea360b259e1e7ba9d6b
Naomi Aro <naomiaro@gmail.com>
2012-04-10 11:23:34 +0200

    CC-3605 : Create 2.1 upgrades

---
+++ install_minimal/upgrades/airtime-2.1.0/common/Version20120410104441.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+class Version20120410104441 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        //start changes to cc_files
+        $cc_files = $schema->getTable('cc_files');
+        $cc_files->addColumn('temp_br', 'integer');
+        $cc_files->addColumn('temp_sr', 'integer');
+        
+        $this->_addSql("UPDATE cc_files SET temp_br = bit_rate::integer");
+        $this->_addSql("UPDATE cc_files SET temp_sr = sample_rate::integer");
+        
+        //end changes to cc_files
+    }
+
+    public function down(Schema $schema)
+    {
+
+    }
+}
