baaeae0890c30daa779525bd5df81916cab62bbe
Naomi Aro <naomiaro@gmail.com>
2012-04-10 13:53:30 +0200

    CC-3605 : Create 2.1 upgrades

--- install_minimal/upgrades/airtime-2.1.0/common/Version20120410104441.php
+++ install_minimal/upgrades/airtime-2.1.0/common/Version20120410104441.php
@@ -11,18 +11,8 @@ class Version20120410104441 extends AbstractMigration
     {
         //add temp columns for changing bitrate and sample rate to integers.
         $cc_files = $schema->getTable('cc_files');
-        $cc_files->addColumn('temp_br', 'integer',  array('notnull' => 0));
+        $cc_files->addColumn('temp_br', 'integer', array('notnull' => 0));
         $cc_files->addColumn('temp_sr', 'integer', array('notnull' => 0));
-    
-        $this->addSql("UPDATE cc_files SET temp_br = bit_rate::integer");
-        $this->addSql("UPDATE cc_files SET temp_sr = sample_rate::integer");
-        
-        $cc_files = $schema->getTable('cc_files');
-        $cc_files->dropColumn('bit_rate');
-        $cc_files->dropColumn('sample_rate');
-        
-        $cc_files->renameColumn('temp_br', 'bit_rate');
-        $cc_files->renameColumn('temp_sr', 'sample_rate');
     }
     
     public function down(Schema $schema)
---
+++ install_minimal/upgrades/airtime-2.1.0/common/Version20120410134819.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+class Version20120410134819 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->_addSql("UPDATE cc_files SET temp_br = bit_rate::integer");
+        $this->_addSql("UPDATE cc_files SET temp_sr = sample_rate::integer");
+        
+        $cc_files = $schema->getTable('cc_files');
+        $cc_files->dropColumn('bit_rate');
+        $cc_files->dropColumn('sample_rate');
+        
+        $cc_files->renameColumn('temp_br', 'bit_rate');
+        $cc_files->renameColumn('temp_sr', 'sample_rate');
+    }
+
+    public function down(Schema $schema)
+    {
+
+    }
+}
