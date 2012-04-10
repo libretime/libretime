10039208154742e17bbea6b21234a2a5cf23226a
Naomi Aro <naomiaro@gmail.com>
2012-04-10 12:03:39 +0200

    CC-3605 : Create 2.1 upgrades

--- install_minimal/upgrades/airtime-2.1.0/common/Version20120410104441.php
+++ install_minimal/upgrades/airtime-2.1.0/common/Version20120410104441.php
@@ -11,8 +11,8 @@ class Version20120410104441 extends AbstractMigration
     {
         //start changes to cc_files
         $cc_files = $schema->getTable('cc_files');
-        $cc_files->addColumn('temp_br', 'integer');
-        $cc_files->addColumn('temp_sr', 'integer');
+        $cc_files->addColumn('temp_br', 'integer',  array('notnull' => 0));
+        $cc_files->addColumn('temp_sr', 'integer', array('notnull' => 0));
         
         //end changes to cc_files
     }
