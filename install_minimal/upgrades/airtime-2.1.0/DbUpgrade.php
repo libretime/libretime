c2ce50bf46ca5e89822c8206e199fd0425c70683
Naomi Aro <naomiaro@gmail.com>
2012-04-11 16:27:46 +0200

    CC-3605 : Create 2.1 upgrades

--- install_minimal/upgrades/airtime-2.1.0/DbUpgrade.php
+++ install_minimal/upgrades/airtime-2.1.0/DbUpgrade.php
@@ -8,16 +8,36 @@ class AirtimeDatabaseUpgrade{
     public static function start(){
         echo "* Updating Database".PHP_EOL;
         self::task0();
+        self::task1();
     }
 
     private static function task0(){
-        UpgradeCommon::MigrateTablesToVersion(__DIR__, '20120410143340');
+        UpgradeCommon::MigrateTablesToVersion(__DIR__, '20120411102907');
        
         $sql = "INSERT INTO cc_pref(\"keystr\", \"valstr\") VALUES('scheduled_play_switch', 'on')";
         UpgradeCommon::nonSelectQueryDb($sql);
+    }
+    
+    /*
+     * set values for playout_status in cc_schedule
+     */
+    private static function task1() {
+        
+        // Define path to application directory
+        defined('APPLICATION_PATH')
+        || define('APPLICATION_PATH', realpath(__DIR__."/../../application"));
+        
+        // Ensure library is on include_path
+        set_include_path(implode(PATH_SEPARATOR, array(
+                get_include_path(),
+                realpath(APPLICATION_PATH . '/../library')
+        )));
+        
+        //Propel classes.
+        set_include_path(APPLICATION_PATH . '/models' . PATH_SEPARATOR . get_include_path());
         
         require_once 'propel/runtime/lib/Propel.php';
-        Propel::init(__DIR__."/../../configs/airtime-conf-production.php");
+        Propel::init(APPLICATION_PATH."/configs/airtime-conf-production.php");
         
         $con = Propel::getConnection(CcSchedulePeer::DATABASE_NAME);
         
