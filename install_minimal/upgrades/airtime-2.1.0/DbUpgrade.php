bcc30b9cb1cb9405b49c7b7b99f998472239ffb8
Naomi Aro <naomiaro@gmail.com>
2012-04-11 16:01:04 +0200

    CC-3605 : Create 2.1 upgrades

--- install_minimal/upgrades/airtime-2.1.0/DbUpgrade.php
+++ install_minimal/upgrades/airtime-2.1.0/DbUpgrade.php
@@ -12,8 +12,23 @@ class AirtimeDatabaseUpgrade{
 
     private static function task0(){
         UpgradeCommon::MigrateTablesToVersion(__DIR__, '20120410143340');
+       
         $sql = "INSERT INTO cc_pref(\"keystr\", \"valstr\") VALUES('scheduled_play_switch', 'on')";
         UpgradeCommon::nonSelectQueryDb($sql);
+        
+        require_once 'propel/runtime/lib/Propel.php';
+        Propel::init(__DIR__."/../../configs/airtime-conf-production.php");
+        
+        $con = Propel::getConnection(CcSchedulePeer::DATABASE_NAME);
+        
+        $showInstances = CcShowInstancesQuery::create()
+            ->setFormatter(ModelCriteria::FORMAT_ON_DEMAND)
+            ->filterByDbStarts(Criteria::GREATER_EQUAL)
+            ->find($con);
+        
+        foreach ($showInstances as $instance) {
+            $instance->updateScheduleStatus($con);
+        }
     }
 
 }
