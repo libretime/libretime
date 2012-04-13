ddf1a6d027529a319090c20a95cc19690eade550
denise <denise@denise-DX4860.(none)>
2012-04-13 13:02:54 -0400

    CC-3637: Now playing shows "Nothing Scheduled" when restreaming external source
    -fixed

--- airtime_mvc/public/js/airtime/dashboard/playlist.js
+++ airtime_mvc/public/js/airtime/dashboard/playlist.js
@@ -150,9 +150,9 @@ function updatePlaybar(){
     }else{
         if (master_dj_on_air) {
             $('#current').html("Current: <span style='color:red; font-weight:bold'>Master DJ On Air</span>");
-        } else if (scheduled_play_on_air) {
-			$('#current').html("Current: <span style='color:red; font-weight:bold'>Show DJ On Air</span>");
         } else if (live_dj_on_air) {
+            $('#current').html("Current: <span style='color:red; font-weight:bold'>Live DJ On Air</span>");
+        } else {
             $('#current').html("Current: <span style='color:red; font-weight:bold'>Nothing Scheduled</span>");
         }
     }
