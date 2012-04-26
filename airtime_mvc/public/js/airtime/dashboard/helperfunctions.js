9c9abfab8d93675d310d247e1519db6a515b82ec
Martin Konecny <martin.konecny@gmail.com>
2012-04-26 10:15:30 -0400

    CC-3704: No default checked day when creating repeat show
    better validation of user input

--- airtime_mvc/public/js/airtime/dashboard/helperfunctions.js
+++ airtime_mvc/public/js/airtime/dashboard/helperfunctions.js
@@ -24,6 +24,10 @@ function getDateFromString(time){
     var month = parseInt(date[1], 10) -1;
     var day = parseInt(date[2], 10);
     
+    if (isNaN(year) || isNaN(month) || isNaN(day)){
+        return null;
+    }
+    
     return new Date(year, month, day);
     
 }
