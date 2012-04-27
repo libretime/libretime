a289480e2789d23d9bdcdd599d5ea79eb4c195a5
James <james@sourcefabric-DX4840.(none)>
2012-04-27 15:30:51 -0400

    CC-3683: Upgraded database not equivalent to fresh install database part 1
    - fixed cc_show table upgrade

--- install_minimal/upgrades/airtime-2.1.0/common/Version20120403143635.php
+++ install_minimal/upgrades/airtime-2.1.0/common/Version20120403143635.php
@@ -10,10 +10,10 @@ class Version20120403143635 extends AbstractMigration
     public function up(Schema $schema)
     {
         $cc_show = $schema->getTable('cc_show');
-        $cc_show->addColumn('live_stream_using_airtime_auth', 'boolean', array('default'=> 'false'));
-        $cc_show->addColumn('live_stream_using_custom_auth', 'boolean', array('default'=> 'false'));
-        $cc_show->addColumn('live_stream_user', 'string', array('notnull' => 0, 'length' => 255));
-        $cc_show->addColumn('live_stream_pass', 'string', array('notnull' => 0, 'length' => 255));
+        $cc_show->addColumn('live_stream_using_airtime_auth', 'boolean', array('default'=> 0, 'notnull' => 0));
+        $cc_show->addColumn('live_stream_using_custom_auth', 'boolean', array('default'=> 0, 'notnull' => 0));
+        $cc_show->addColumn('live_stream_user', 'string', array('default'=> 0, 'notnull' => 0, 'length' => 255));
+        $cc_show->addColumn('live_stream_pass', 'string', array('default'=> 0, 'notnull' => 0, 'length' => 255));
     }
 
     public function down(Schema $schema)
