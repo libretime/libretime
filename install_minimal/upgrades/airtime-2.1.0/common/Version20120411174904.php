495ad633336f61b4c22286ece683ed627d1464c4
Martin Konecny <martin.konecny@gmail.com>
2012-05-04 17:37:30 -0400

    CC-3732: NationTalk Database upgrade fail, from 2.0.3 to 2.1.0
    -move NOT NULL constraint to after the column has been populated

--- install_minimal/upgrades/airtime-2.1.0/common/Version20120411174904.php
+++ install_minimal/upgrades/airtime-2.1.0/common/Version20120411174904.php
@@ -13,14 +13,14 @@ class Version20120411174904 extends AbstractMigration
     public function up(Schema $schema)
     {
         $this->_addSql("ALTER TABLE cc_show_instances ADD created timestamp");
-        $this->_addSql("ALTER TABLE cc_show_instances ALTER COLUMN created SET NOT NULL");
-        
         $this->_addSql("ALTER TABLE cc_show_instances ADD last_scheduled timestamp");
         
         //setting these to a default now for timeline refresh purposes.
         $now = gmdate("Y-m-d H:i:s");
         $this->_addSql("UPDATE cc_show_instances SET created = '$now'");
         $this->_addSql("UPDATE cc_show_instances SET last_scheduled = '$now'");
+        
+        $this->_addSql("ALTER TABLE cc_show_instances ALTER COLUMN created SET NOT NULL");
     }
 
     public function down(Schema $schema)
