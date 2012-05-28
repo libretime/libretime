3d23d5102b5fef8a49a745d06205097a63cbf9f4
Martin Konecny <martin.konecny@gmail.com>
2012-05-28 13:36:49 -0400

    make sure /usr/bin/airtime-liquidsoap symlink is gone

--- python_apps/pypo/install/pypo-uninitialize.py
+++ python_apps/pypo/install/pypo-uninitialize.py
@@ -9,7 +9,10 @@ if os.geteuid() != 0:
 try:      
     #stop pypo and liquidsoap processes
     print "Waiting for pypo processes to stop...",
-    subprocess.call("rm -f /usr/bin/airtime-liquidsoap")
+    try:
+        os.remove("/usr/bin/airtime-liquidsoap")
+    except Exception, e:
+        pass
     if (os.path.exists('/etc/init.d/airtime-playout')):
         subprocess.call("invoke-rc.d airtime-playout stop", shell=True)
         print "OK"
