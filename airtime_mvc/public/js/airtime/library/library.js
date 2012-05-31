c18704aa4d1a8bffb19ad37f466a992fc790e607
denise <denise@denise-DX4860.(none)>
2012-05-31 12:28:18 -0400

    CC-3900: Show Builder boxes remain checked even when the item is no longer visible
    -fixed playlist builder "Dragging <x> items" message so <x> is amount of visible items only

--- airtime_mvc/public/js/airtime/library/library.js
+++ airtime_mvc/public/js/airtime/library/library.js
@@ -34,10 +34,25 @@ var AIRTIME = (function(AIRTIME) {
     };
     
     mod.getChosenAudioFilesLength = function(){
-    	var files = Object.keys(chosenItems),
-    		i, length,
-    		count = 0,
-    		reAudio=/^au/ ;
+    	//var files = Object.keys(chosenItems),
+    	var files,
+            $trs,
+            cItem,
+            i, length,
+            count = 0,
+            reAudio=/^au/ ;
+    		
+        // Get visible items and check if any chosenItems are visible
+        $trs = $libTable.find("tbody input:checkbox").parents("tr");
+        $trs.each(function(i){
+            for (cItem in chosenItems) {
+                if (cItem === $(this).attr("id")) {
+                    visibleChosenItems[cItem] = $(this).data('aData');
+                }
+            } 
+        });
+        
+        files = Object.keys(visibleChosenItems);
     	
     	for (i = 0, length = files.length; i < length; i++) {
     		
