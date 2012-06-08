3b9f6f12de7670d0550811a100c17378c82ab879
James <james@sourcefabric-DX4840.(none)>
2012-06-08 10:33:06 -0400

    CC-3834: System -> Stream Settings: click "save" will cause some errors in zendphp.log
    - removed logging

--- airtime_mvc/application/models/StreamSetting.php
+++ airtime_mvc/application/models/StreamSetting.php
@@ -179,8 +179,6 @@ class Application_Model_StreamSetting {
 
                     $con->exec($sql);
                 }
-            } else {
-                Logging::log("Warning unexpected value: ".$key);
             }
         }
     }
