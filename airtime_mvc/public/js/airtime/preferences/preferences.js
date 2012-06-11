5b9add360caa0ae1cefd086a62d1799b86fd23b5
denise <denise@denise-DX4860.(none)>
2012-06-11 15:18:12 -0400

    CC-3718: Please enable user to input the mail server settings
    -fixed soundcloud settings from collapsing if there are form input errors

--- airtime_mvc/public/js/airtime/preferences/preferences.js
+++ airtime_mvc/public/js/airtime/preferences/preferences.js
@@ -5,6 +5,11 @@ function showErrorSections() {
         $(window).scrollTop($("#soundcloud-settings .errors").position().top);
     }
     
+    if($("#email-server-settings .errors").length > 0) {
+        $("#email-server-settings").show();
+        $(window).scrollTop($("#email-server-settings .errors").position().top);
+    }
+    
     if($("#livestream-settings .errors").length > 0) {
         $("#livestream-settings").show();
         $(window).scrollTop($("#livestream-settings .errors").position().top);
