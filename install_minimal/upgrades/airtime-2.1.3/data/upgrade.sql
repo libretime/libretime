b7ca858506b9925dc7813bd0aa8268ac432d2a8d
James <james@sourcefabric-DX4840.(none)>
2012-07-09 16:50:51 -0400

    CC-3921: Add mobile phone number field to user record
    - added a line to upgrade.sql which takes care of db upgrade

--- install_minimal/upgrades/airtime-2.1.3/data/upgrade.sql
+++ install_minimal/upgrades/airtime-2.1.3/data/upgrade.sql
@@ -1,4 +1,5 @@
 DELETE FROM cc_pref WHERE keystr = 'system_version';
 INSERT INTO cc_pref (keystr, valstr) VALUES ('system_version', '2.1.3');
 
-UPDATE cc_show_instances SET time_filled='00:00:00' WHERE time_filled IS NULL;
+UPDATE cc_show_instances SET time_filled='00:00:00' WHERE time_filled IS NULL;\
+ALTER TABLE cc_subjs ADD COLUMN cell_phone VARCHAR(255);
