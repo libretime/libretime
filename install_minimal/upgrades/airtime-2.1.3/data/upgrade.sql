35655f94626fc3f90dadc13fdb7ab97a432935ef
Martin Konecny <martin.konecny@gmail.com>
2012-07-10 19:05:27 -0400

    -no longer require "cell_phone" column for 2.1.3 upgrade

--- install_minimal/upgrades/airtime-2.1.3/data/upgrade.sql
+++ install_minimal/upgrades/airtime-2.1.3/data/upgrade.sql
@@ -2,4 +2,3 @@ DELETE FROM cc_pref WHERE keystr = 'system_version';
 INSERT INTO cc_pref (keystr, valstr) VALUES ('system_version', '2.1.3');
 
 UPDATE cc_show_instances SET time_filled='00:00:00' WHERE time_filled IS NULL;
-ALTER TABLE cc_subjs ADD COLUMN cell_phone VARCHAR(255);
