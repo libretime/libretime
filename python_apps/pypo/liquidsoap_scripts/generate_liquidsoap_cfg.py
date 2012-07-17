2ad7e78e10026a426a854bc754f324dbcd727718
Martin Konecny <martin.konecny@gmail.com>
2012-07-17 16:00:13 -0400

    CC-1665: Scheduled stream rebroadcasting and recording
    -initial prototype

--- python_apps/pypo/liquidsoap_scripts/generate_liquidsoap_cfg.py
+++ python_apps/pypo/liquidsoap_scripts/generate_liquidsoap_cfg.py
@@ -8,18 +8,19 @@ def generate_liquidsoap_config(ss):
     fh.write("################################################\n")
     fh.write("# THIS FILE IS AUTO GENERATED. DO NOT CHANGE!! #\n")
     fh.write("################################################\n")
-    for d in data:
 
+    for d in data:
         key = d['keyname']
 
         str_buffer = d[u'keyname'] + " = "
-        if(d[u'type'] == 'string'):
+        if d[u'type'] == 'string':
             val = '"%s"' % d['value']
         else:
             val = d[u'value']
             val = val if len(val) > 0 else "0"
         str_buffer = "%s = %s\n" % (key, val)
         fh.write(str_buffer.encode('utf-8'))
+
     fh.write('log_file = "/var/log/airtime/pypo-liquidsoap/<script>.log"\n')
     fh.close()
 
