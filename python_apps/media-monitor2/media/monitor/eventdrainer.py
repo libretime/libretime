332e0f50805c240b3a3e5b2a4bdcc1c507d4395b
Rudi Grinberg <rudi.grinberg@sourcefabric.org>
2012-07-27 15:38:09 -0400

    cc-4105-2: added a separate thread to flush messages from kombu

---
+++ python_apps/media-monitor2/media/monitor/eventdrainer.py
@@ -0,0 +1,15 @@
+import socket
+from media.monitor.log import Loggable
+from media.monitor.toucher import RepeatTimer
+
+class EventDrainer(Loggable):
+    def __init__(self, connection, interval=1):
+        def cb():
+            try: connection.drain_events(timeout=0.1)
+            except socket.timeout: pass
+            except Exception as e:
+                self.logger.error("Error flushing events")
+                self.logger.error( str(e) )
+        t = RepeatTimer(interval, cb)
+        t.daemon = True
+        t.start()
