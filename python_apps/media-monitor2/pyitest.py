c8df791d1326553eeb8b416cd2b9c7f37e4bb1aa
Rudi Grinberg <rudi.grinberg@sourcefabric.org>
2012-08-01 17:54:32 -0400

    cc-4105: added misc. testing scripts

---
+++ python_apps/media-monitor2/pyitest.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import sys
+import os
+import getopt
+import pyinotify
+import pprint
+
+# a little shit script to test out pyinotify events
+
+class AT(pyinotify.ProcessEvent):
+    def process_default(self, event):
+        pprint.pprint(event)
+
+def main():
+    optlist, arguments  = getopt.getopt(sys.argv[1:], '', ["dir="])
+    ldir = ""
+    for k,v in optlist:
+        if k == '--dir':
+            ldir = v
+            break
+    if not os.path.exists(ldir):
+        print("can't pyinotify dir: '%s'. it don't exist" % ldir)
+        sys.exit(0)
+    wm = pyinotify.WatchManager()
+    notifier = pyinotify.Notifier(wm)
+    wm.add_watch(ldir, pyinotify.ALL_EVENTS, auto_add=True, rec=True, proc_fun=AT())
+    notifier.loop()
+
+if __name__ == '__main__': main()
