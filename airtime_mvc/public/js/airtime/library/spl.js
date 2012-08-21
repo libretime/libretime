cba717dc4b9bd015daaea228f2e6ea4d33a54220
Martin Konecny <martin.konecny@gmail.com>
2012-08-21 10:33:36 -0400

    CC-1665: Scheduled stream rebroadcasting and recording
    -Fixed regressions introduced by changing HTML element id

--- airtime_mvc/public/js/airtime/library/spl.js
+++ airtime_mvc/public/js/airtime/library/spl.js
@@ -568,7 +568,7 @@ var AIRTIME = (function(AIRTIME){
             //stream url
             //default_length  
             //playlist name
-            var id = $pl.find("#ws_id").attr("value"); 
+            var id = $pl.find("#obj_id").attr("value"); 
             var description = $pl.find("#description").val();
             var streamurl = $pl.find("#streamurl-element input").val();
             var length = $pl.find("#streamlength-element input").val();
@@ -596,7 +596,7 @@ var AIRTIME = (function(AIRTIME){
                         $status.show();
                         setTimeout(function(){$status.fadeOut("slow", function(){$status.empty()})}, 5000);
 
-                        var $ws_id = $("#ws_id");
+                        var $ws_id = $("#obj_id");
                         $ws_id.attr("value", json.streamId);
 
                         var $ws_id = $("#ws_delete");
