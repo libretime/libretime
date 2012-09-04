1c425eb17d537666de65510e80dca77bf68cadde
Rudi Grinberg <rudi.grinberg@sourcefabric.org>
2012-09-04 08:55:22 -0400

    cc-4339: removed unnecessary file

--- python_apps/media-monitor2/mm1.99.sh
+++
@@ -1,4 +0,0 @@
-#export PYTHONPATH="/home/rudi/Airtime/python_apps/:/home/rudi/Airtime/python_apps/media-monitor2/"
-PYTHONPATH='/home/rudi/Airtime/python_apps/:/home/rudi/Airtime/python_apps/media-monitor2/'
-export PYTHONPATH
-python ./mm2.py --config="/etc/airtime/media-monitor.cfg" --apiclient="/etc/airtime/api_client.cfg" --log="/home/rudi/Airtime/python_apps/media-monitor/logging.cfg"
