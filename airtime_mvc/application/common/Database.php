26c2c2c0d152a06287f5058e2538b0c173f5709e
denise <denise@denise-DX4860sourcefabric.org>
2012-09-05 17:52:53 -0400

    CC-4345: Prepared statements - part 1
    -added extra parameter to prepareAndExecute()

--- airtime_mvc/application/common/Database.php
+++ airtime_mvc/application/common/Database.php
@@ -1,9 +1,8 @@
 <?php
 class Application_Common_Database
 {
+    public static function prepareAndExecute($sql, $paramValueMap, $type='all', $fetchType=PDO::FETCH_ASSOC){
 
-    public static function prepareAndExecute($sql, $paramValueMap, $type='all')
-    {
         $con = Propel::getConnection();
         $stmt = $con->prepare($sql);
         foreach ($paramValueMap as $param => $v) {
@@ -12,11 +11,11 @@ class Application_Common_Database
         $rows = array();
         if ($stmt->execute()) {
             if ($type == 'single') {
-                $rows = $stmt->fetch(PDO::FETCH_ASSOC);
-            } elseif ($type == 'column'){
+                $rows = $stmt->fetch($fetchType);
+            } else if ($type == 'column'){
                 $rows = $stmt->fetchColumn();
             } else {
-                $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                $rows = $stmt->fetchAll($fetchType);
             }
         } else {
             $msg = implode(',', $stmt->errorInfo());
