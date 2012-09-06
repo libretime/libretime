f30507fce63ea65cbb44c8015363269358f07350
Rudi Grinberg <rudi.grinberg@sourcefabric.org>
2012-09-06 16:17:14 -0400

    cc-4347: pdo

--- airtime_mvc/application/models/Show.php
+++ airtime_mvc/application/models/Show.php
@@ -803,12 +803,23 @@ SQL;
             ':add_show_id' => $p_data['add_show_id']
         ));
 
-        $sql = "UPDATE cc_show_instances "
-                ."SET ends = starts + INTERVAL '$p_data[add_show_duration]' "
-                ."WHERE show_id = $p_data[add_show_id] "
-                ."AND ends > TIMESTAMP '$timestamp'";
-        $con->exec($sql);
-
+        // TODO : get rid of this
+        //$sql = "UPDATE cc_show_instances "
+                //."SET ends = starts + INTERVAL '$p_data[add_show_duration]' "
+                //."WHERE show_id = $p_data[add_show_id] "
+                //."AND ends > TIMESTAMP '$timestamp'";
+        $sql = <<<SQL
+UPDATE cc_show_instances 
+SET ends = starts + INTERVAL :add_show_duration
+WHERE show_id = :show_id
+AND ends > TIMESTAMP :timestamp
+SQL;
+        
+        Application_Common_Database::prepareAndExecute( $sql,
+            array( 
+                ':add_show_duration' => $p_data['add_show_duration'],
+                ':show_id' => $p_data['add_show_id'],
+                ':timestamp' => $timestamp), "execute");
     }
 
     private function updateStartDateTime($p_data, $p_endDate)
