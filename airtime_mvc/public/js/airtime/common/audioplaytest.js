09d587e26235489a5485df140bf514750915ca72
Martin Konecny <martin.konecny@gmail.com>
2012-09-13 12:28:10 -0400

    CC-4393: Please ignore Flac items for Playlist Preview and disable to preview it as a single track
    -better file type detection

--- airtime_mvc/public/js/airtime/common/audioplaytest.js
+++ airtime_mvc/public/js/airtime/common/audioplaytest.js
@@ -5,12 +5,8 @@ function isAudioSupported(mime){
     var bMime = null;
     if (mime.indexOf("ogg") != -1 || mime.indexOf("vorbis") != -1) {
        bMime = 'audio/ogg; codecs="vorbis"'; 
-    } else if (mime.indexOf("mp3") != -1) {
-        bMime = "audio/mp3";
-    } else if (mime.indexOf("mp4") != -1) {
-        bMime = "audio/mp4";
-    } else if (mime.indexOf("flac") != -1) {
-        bMime = "audio/x-flac";
+    } else {
+        bMime = mime;
     }
 
     return !!bMime && !!audio.canPlayType && audio.canPlayType(bMime) != "";
