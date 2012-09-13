52882ef9384bb69d06c626526f757ef1b3905f91
Rudi Grinberg <rudi.grinberg@sourcefabric.org>
2012-09-13 10:35:13 -0400

    Added mp4 removed aac.

--- python_apps/media-monitor2/media/monitor/pure.py
+++ python_apps/media-monitor2/media/monitor/pure.py
@@ -20,8 +20,8 @@ from configobj import ConfigObj
 from media.monitor.exceptions import FailedToSetLocale, FailedToCreateDir
 
 #supported_extensions =  [u"mp3", u"ogg", u"oga"]
-supported_extensions = [u"mp3", u"ogg", u"oga", u"flac", u"aac", u"wav",
-                        u'm4a']
+supported_extensions = [u"mp3", u"ogg", u"oga", u"flac", u"wav",
+                        u'm4a', u'mp4']
 
 unicode_unknown = u'unknown'
 
