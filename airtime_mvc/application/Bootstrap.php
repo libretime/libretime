c84a608747f774e52cfefbd18978abb80a54a1c9
Martin Konecny <martin.konecny@gmail.com>
2012-09-14 16:51:39 -0400

    CC-4438: Station Time is always UTC time no matter what the time zone setting is
    -fixed

--- airtime_mvc/application/Bootstrap.php
+++ airtime_mvc/application/Bootstrap.php
@@ -13,6 +13,12 @@ require_once "Database.php";
 require_once __DIR__.'/controllers/plugins/RabbitMqPlugin.php';
 
 
+
+date_default_timezone_set('UTC');
+require_once (APPLICATION_PATH."/logging/Logging.php");
+Logging::setLogPath('/var/log/airtime/zendphp.log');
+
+Logging::info(Application_Model_Preference::GetTimezone());
 date_default_timezone_set(Application_Model_Preference::GetTimezone());
 
 global $CC_CONFIG;
