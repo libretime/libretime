7cd8541696372bb15e83d64f1c46686d72b7fa80
Rudi Grinberg <rudi.grinberg@sourcefabric.org>
2012-10-26 16:41:29 -0400

    cc-4635: Fix

--- python_apps/media-monitor2/media/metadata/definitions.py
+++ python_apps/media-monitor2/media/metadata/definitions.py
@@ -124,16 +124,12 @@ def load_definitions():
             default_title = re.sub(r'__\d+\.',u'.', default_title)
 
             # format is: track_number-title-123kbps.mp3
-            m = re.match(".+-(?P<title>.+)-\d+kbps", default_title)
+            m = re.match(".+-(?P<title>.+)-\d+kbps$", default_title)
             if m:
                 if m.group('title') == unicode_unknown: new_title = ''
                 else: new_title = m.group('title')
+            else: new_title = re.sub(r'-\d+kbps$', u'', new_title)
 
-            if re.match(".+-%s-.+$" % unicode_unknown, default_title):
-                default_title = u''
-
-            new_title = default_title
-            new_title = re.sub(r'-\d+kbps$', u'', new_title)
         return new_title
 
     with md.metadata('MDATA_KEY_TITLE') as t:
