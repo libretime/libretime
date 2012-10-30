a33a1e6e2abe57ba2d0e8879cdd61fc847ceca7e
Rudi Grinberg <rudi.grinberg@gmail.com>
2012-10-29 23:34:26 -0400

    Added more testing for ApiRequest using mocks

--- python_apps/api_clients/tests/test_apirequest.py
+++ python_apps/api_clients/tests/test_apirequest.py
@@ -1,7 +1,21 @@
 import unittest
+from mock import MagicMock, patch
 from .. api_client import ApcUrl, ApiRequest
 
 class TestApiRequest(unittest.TestCase):
     def test_init(self):
-        u = ApiRequest("request_name", ApcUrl("/test/ing"))
+        u = ApiRequest('request_name', ApcUrl('/test/ing'))
         self.assertEquals(u.name, "request_name")
+
+    def test_call(self):
+        ret = 'ok'
+        read = MagicMock()
+        read.read = MagicMock(return_value=ret)
+        u = '/testing'
+        with patch('urllib2.urlopen') as mock_method:
+            mock_method.return_value = read
+            request = ApiRequest('mm', ApcUrl(u))()
+            self.assertEquals(request, ret)
+            mock_method.assert_called_once_with(u)
+
+if __name__ == '__main__': unittest.main()
