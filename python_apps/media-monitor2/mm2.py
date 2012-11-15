57b13607c5e5309c9249ebb640dd9ee3167f4b8c
Rudi Grinberg <rudi.grinberg@gmail.com>
2012-11-15 14:22:42 -0500

    removed crap hacks

--- python_apps/media-monitor2/mm2.py
+++ python_apps/media-monitor2/mm2.py
@@ -4,23 +4,14 @@ import os
 import logging
 import logging.config
 
-from media.monitor.manager          import Manager
 from media.monitor.log              import get_logger, setup_logging
-from media.monitor.config           import MMConfig
-from media.monitor.toucher          import ToucherThread
 from media.monitor.exceptions       import FailedToObtainLocale, \
-                                           FailedToSetLocale, \
-                                           NoConfigFile
-from media.monitor.airtime          import AirtimeNotifier, \
-                                           AirtimeMessageReceiver
-from media.monitor.watchersyncer    import WatchSyncer
-from media.monitor.eventdrainer     import EventDrainer
+                                           FailedToSetLocale
 from std_err_override               import LogWriter
 from media.saas.launcher import MM2
 from media.saas.airtimeinstance import AirtimeInstance
 
 import media.monitor.pure          as mmp
-from api_clients import api_client as apc
 
 def setup_logger(log_config, logpath):
     logging.config.fileConfig(log_config)
@@ -58,9 +49,6 @@ def main(global_config, api_client_config, log_config):
     ai = AirtimeInstance('hosted_install', '/', cfg)
     log = setup_logger( log_config, ai.mm_config['logpath'] )
     setup_global(log)
-    apc.AirtimeApiClient.create_right_config(log=log,
-            config_path=api_client_config)
-    apc.AirtimeApiClient(api_client_config)
     mm = MM2(ai)
     mm.start()
 
