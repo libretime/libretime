3a1945a818e1af428e42216502799e16cf66f0a9
Martin Konecny <martin@mkonecny-DX4860.(none)>
2012-11-22 11:40:44 -0500

    CC-4702Webstream: When webstream is playing, it isn't marked as 'media_item_played' in cc_schedule -fixed

--- python_apps/pypo/pyponotify.py
+++ python_apps/pypo/pyponotify.py
@@ -114,6 +114,7 @@ class Notify:
             self.notify_source_status(options.source_name, options.source_status)
         elif options.webstream:
             self.notify_webstream_data(options.webstream, options.media_id)
+            self.notify_media_start_playing(options.media_id)
         elif options.media_id:
             self.notify_media_start_playing(options.media_id)
         elif options.liquidsoap_started:
