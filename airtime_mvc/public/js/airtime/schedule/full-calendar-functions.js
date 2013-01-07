82da4dcfe843214b82733845e277b4757987face
denise <denise@denise-DX4860sourcefabric.org>
2013-01-07 12:41:28 -0500

    CC-4599: Calendar: Add an icon to indicate show is not fully booked
    -done but a new icon is needed

--- airtime_mvc/public/js/airtime/schedule/full-calendar-functions.js
+++ airtime_mvc/public/js/airtime/schedule/full-calendar-functions.js
@@ -258,12 +258,20 @@ function eventRender(event, element, view) {
             $(element)
                 .find(".fc-event-time")
                 .before('<span id="'+event.id+'" title="'+$.i18n._("Show is empty")+'" class="small-icon show-empty"></span>');
+        } else if (event.show_partial_filled === true) {
+            $(element)
+                .find(".fc-event-time")
+                .before('<span id="'+event.id+'" title="'+$.i18n._("Show is partially filled")+'" class="small-icon show-partial-filled"></span>');
         }
     } else if (view.name === 'month') {
         if (event.show_empty === 1 && event.record === 0 && event.rebroadcast === 0) {
             $(element)
                 .find(".fc-event-title")
                 .after('<span id="'+event.id+'" title="'+$.i18n._("Show is empty")+'" class="small-icon show-empty"></span>');
+        } else if (event.show_partial_filled === true) {
+            $(element)
+                .find(".fc-event-title")
+                .after('<span id="'+event.id+'" title="'+$.i18n._("Show is partially filled")+'" class="small-icon show-partial-filled"></span>');
         }
     }
 
