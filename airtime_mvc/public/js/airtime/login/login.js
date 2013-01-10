4673f11d765f2648c1cb7b04467fa5e55e4ffab6
denise <denise@denise-DX4860sourcefabric.org>
2013-01-10 15:06:45 -0500

    Default language to english when cookie is not set

--- airtime_mvc/public/js/airtime/login/login.js
+++ airtime_mvc/public/js/airtime/login/login.js
@@ -1,6 +1,6 @@
 $(window).load(function(){
     $("#username").focus();
-    $("#locale").val($.cookie("airtime_locale"));
+    $("#locale").val($.cookie("airtime_locale")!== null?$.cookie("airtime_locale"):'en_CA');
 });
 
 $(document).ready(function() {
