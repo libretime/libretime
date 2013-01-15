4d3f5e1ba4a9233531e327423fb4a0e251d2be9b
denise <denise@denise-DX4860sourcefabric.org>
2013-01-15 16:27:50 -0500

    Reverting last commit

--- install_minimal/include/airtime-initialize.sh
+++ install_minimal/include/airtime-initialize.sh
@@ -11,10 +11,7 @@ dist=`lsb_release -is`
 echo "Generating locales"
 for i in `ls /usr/share/airtime/locale | grep ".._.."`; do
     if [ "$dist" = "Debian" ]; then
-        grep -q "$i" /etc/locale.gen
-        if [ $? -ne 0 ]; then
-            echo "$i.UTF-8 UTF-8" >> /etc/locale.gen
-        fi
+        echo "$i.UTF-8 UTF-8" >> /etc/locale.gen
     else
         locale-gen "$i.utf8"
     fi
