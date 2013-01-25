9d6f2ebdf46ca46df8734d14a0ceab53ae357931
Martin Konecny <martin.konecny@gmail.com>
2013-01-25 12:32:37 -0500

    remove unecessary code

--- airtime_mvc/application/controllers/plugins/RabbitMqPlugin.php
+++ airtime_mvc/application/controllers/plugins/RabbitMqPlugin.php
@@ -7,13 +7,9 @@ class RabbitMqPlugin extends Zend_Controller_Plugin_Abstract
         if (Application_Model_RabbitMq::$doPush) {
             $md = array('schedule' => Application_Model_Schedule::getSchedule());
             Application_Model_RabbitMq::SendMessageToPypo("update_schedule", $md);
-            if (!isset($_SERVER['AIRTIME_SRV'])) {
-                Application_Model_RabbitMq::SendMessageToShowRecorder("update_recorder_schedule");
-            }
         }
 
         if (memory_get_peak_usage() > 30*pow(2, 20)) {
-
             Logging::debug("Peak memory usage: "
                 .(memory_get_peak_usage()/1000000)
                 ." MB while accessing URI ".$_SERVER['REQUEST_URI']);
