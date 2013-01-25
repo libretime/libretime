7bea2df8bd7f230c03fb90eff4945ee78af4d0bc
denise <denise@denise-DX4860sourcefabric.org>
2013-01-25 11:38:15 -0500

    Live chat was in audio preview and listen popup windows

--- airtime_mvc/application/Bootstrap.php
+++ airtime_mvc/application/Bootstrap.php
@@ -122,13 +122,13 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         
         $view->headScript()->appendScript("var userType = '$userType';");
 
-        if (strpos($_SERVER['REQUEST_URI'], $baseUrl.'/Dashboard/stream-player') === false
-            && strpos($_SERVER['REQUEST_URI'], $baseUrl.'/audiopreview/audio-preview') === false
-            && strpos($_SERVER['REQUEST_URI'], $baseUrl.'/audiopreview/playlist-preview') === false
-            && strpos($_SERVER['REQUEST_URI'], $baseUrl.'/audiopreview/block-preview') === false) {
+        if (strpos($_SERVER['REQUEST_URI'], $baseUrl.'Dashboard/stream-player') === false
+            && strpos($_SERVER['REQUEST_URI'], $baseUrl.'audiopreview/audio-preview') === false
+            && strpos($_SERVER['REQUEST_URI'], $baseUrl.'audiopreview/playlist-preview') === false
+            && strpos($_SERVER['REQUEST_URI'], $baseUrl.'audiopreview/block-preview') === false) {
             $client_id = Application_Model_Preference::GetClientId();
             $view->headScript()->appendScript("var livechat_client_id = '$client_id';");
-            $view->headScript()->appendFile($baseUrl . '/js/airtime/common/livechat.js?'.$CC_CONFIG['airtime_version'], 'text/javascript');
+            $view->headScript()->appendFile($baseUrl . 'js/airtime/common/livechat.js?'.$CC_CONFIG['airtime_version'], 'text/javascript');
         }
 
         if (isset($CC_CONFIG['demo']) && $CC_CONFIG['demo'] == 1) {
