2c1fa283dad3d05c7a4f763d994514011aba9a71
denise <denise@denise-DX4860sourcefabric.org>
2013-02-11 14:17:18 -0500

    CC-4941: Dynamic Smart Block ignores the silan check
    -fixed for playlists that contain dynamic blocks

--- airtime_mvc/application/models/Scheduler.php
+++ airtime_mvc/application/models/Scheduler.php
@@ -203,9 +203,12 @@ class Application_Model_Scheduler
                             $file = CcFilesQuery::create()->findPk($fileId);
                             if (isset($file) && $file->visible()) {
                                 $data["id"] = $file->getDbId();
-                                $data["cliplength"] = $file->getDbLength();
-                                $data["cuein"] = "00:00:00";
-                                $data["cueout"] = $file->getDbLength();
+                                $data["cuein"] = $file->getDbCuein();
+                                $data["cueout"] = $file->getDbCueout();
+
+                                $cuein = Application_Common_DateHelper::calculateLengthInSeconds($data["cuein"]);
+                                $cueout = Application_Common_DateHelper::calculateLengthInSeconds($data["cueout"]);
+                                $data["cliplength"] = Application_Common_DateHelper::secondsToPlaylistTime($cueout - $cuein);
                                 $defaultFade = Application_Model_Preference::GetDefaultFade();
                                 if (isset($defaultFade)) {
                                     //fade is in format SS.uuuuuu
