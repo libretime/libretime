ebbb9f1b9c75a48eca7811aab9647a03ff010c3c
Daniel James <daniel@64studio.com>
2013-04-02 17:06:16 +0100

    Added Austrian German localization, thanks Hoerich!

--- airtime_mvc/build/sql/defaultdata.sql
+++ airtime_mvc/build/sql/defaultdata.sql
@@ -328,6 +328,7 @@ INSERT INTO cc_pref("subjid", "keystr", "valstr") VALUES(1, 'user_locale', 'en_C
 INSERT INTO cc_locale (locale_code, locale_lang) VALUES ('en_CA', 'English');
 INSERT INTO cc_locale (locale_code, locale_lang) VALUES ('cs_CZ', 'Český');
 INSERT INTO cc_locale (locale_code, locale_lang) VALUES ('de_DE', 'Deutsch');
+INSERT INTO cc_locale (locale_code, locale_lang) VALUES ('de_AT', 'Österreichisches Deutsch');
 INSERT INTO cc_locale (locale_code, locale_lang) VALUES ('es_ES', 'Español');
 INSERT INTO cc_locale (locale_code, locale_lang) VALUES ('fr_FR', 'Français');
 INSERT INTO cc_locale (locale_code, locale_lang) VALUES ('it_IT', 'Italiano');
