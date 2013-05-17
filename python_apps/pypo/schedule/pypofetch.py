46364bec443289f9c1301f8c935e3cb4f64cdb48
Martin Konecny <martin.konecny@gmail.com>
2013-05-17 14:54:23 -0400

    fix reference to non-existent var

--- python_apps/pypo/schedule/pypofetch.py
+++ python_apps/pypo/schedule/pypofetch.py
@@ -108,7 +108,7 @@ class PypoFetch(Thread):
                 self.regenerate_liquidsoap_conf(m['setting'])
             elif command == 'update_stream_format':
                 self.logger.info("Updating stream format...")
-                self.telnetliquidsoap.\
+                self.pypo_liquidsoap.\
                         get_telnet_dispatcher().\
                         update_liquidsoap_stream_format(m['stream_format'])
             elif command == 'update_station_name':
