01d48e7e79ae7d32c708f14f6e1bfcb35741adf4
Naomi <naomiaro@gmail.com>
2013-10-31 17:34:17 -0400

    CC-5450 : Refactor Media Management
    forgot namespace import

--- airtime_mvc/application/models/Block.php
+++ airtime_mvc/application/models/Block.php
@@ -5,6 +5,7 @@ require_once 'formatters/LengthFormatter.php';
 use Airtime\CcBlock;
 use Airtime\CcBlockPeer;
 use Airtime\CcBlockQuery;
+use Airtime\CcBlockcriteriaQuery;
 use Airtime\CcFilesPeer;
 use Airtime\CcFilesQuery;
 use Airtime\CcPlaylistcontentsQuery;
