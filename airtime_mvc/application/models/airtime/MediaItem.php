b0901abbf7fd51a312d974808a9e1e21c8d308d0
Naomi <naomiaro@gmail.com>
2013-11-01 18:38:18 -0400

    CC-5450 : Refactor Media Management
    using a preInsert hook on MediaItem to insert Owner Id to the database.

--- airtime_mvc/application/models/airtime/MediaItem.php
+++ airtime_mvc/application/models/airtime/MediaItem.php
@@ -3,7 +3,10 @@
 namespace Airtime;
 
 use Airtime\om\BaseMediaItem;
-
+use \Application_Service_UserService;
+use \Exception;
+use \Logging;
+use \PropelPDO;
 
 /**
  * Skeleton subclass for representing a row from the 'media_item' table.
@@ -18,4 +21,17 @@ use Airtime\om\BaseMediaItem;
  */
 class MediaItem extends BaseMediaItem
 {
+	public function preInsert(PropelPDO $con = null)
+	{
+		try {
+			$service = new Application_Service_UserService();
+			$this->setCcSubjs($service->getCurrentUser());
+		}
+		catch(Exception $e) {
+			Logging::warn("Unable to get current user for the inserted media item");
+		}
+
+		//have to return true for the insert to work.
+		return true;
+	}
 }
--- airtime_mvc/application/services/UserService.php
+++ airtime_mvc/application/services/UserService.php
@@ -8,9 +8,23 @@ class Application_Service_UserService
 
     public function __construct()
     {
-        $userInfo = Zend_Auth::getInstance()->getStorage()->read();
-        if (!is_null($userInfo->id)) {
-            $this->currentUser = CcSubjsQuery::create()->findPK($userInfo->id);
+    	//called from a daemon process
+        if (!class_exists("Zend_Auth", false) || !Zend_Auth::getInstance()->hasIdentity()) {
+            $id = null;
+        } 
+        else {
+            $auth = Zend_Auth::getInstance();
+            $id = $auth->getIdentity()->id;
+        }
+        
+        if (!is_null($id)) {	
+        	try {
+        		$this->currentUser = CcSubjsQuery::create()->findPK($id);
+        	}
+        	catch(Exception $e) {
+        		//user must have been deleted sometime after logging in.
+        		$this->currentUser = null;
+        	}
         }
     }
 
@@ -26,5 +40,4 @@ class Application_Service_UserService
 
         return $this->currentUser;
     }
-
 }
