03015a6b69ed35bec812a9087fc83b473e441f0f
Albert Santoni <albert.santoni@sourcefabric.org>
2014-01-29 17:19:24 -0500

    Nerf the APC cache on SaaS
    * We have multiple webservers on SaaS so we need a distributed data
      cache like memcached.
    * Fixes CC-5671: Live streaming: UI won't update connection status unless reload apache2

--- airtime_mvc/application/models/Cache.php
+++ airtime_mvc/application/models/Cache.php
@@ -2,31 +2,37 @@
 
 class Cache
 {
-	
-	private function createCacheKey($key, $isUserValue, $userId = null) {
-		
-		$CC_CONFIG = Config::getConfig();
-		$a = $CC_CONFIG["apiKey"][0];
-		
-		if ($isUserValue) {
-			$cacheKey = "{$key}{$userId}{$a}";
-		}
-		else {
-			$cacheKey = "{$key}{$a}";
-		}
+    
+    private function createCacheKey($key, $isUserValue, $userId = null) {
+        
+        $CC_CONFIG = Config::getConfig();
+        $a = $CC_CONFIG["apiKey"][0];
+        
+        if ($isUserValue) {
+                $cacheKey = "{$key}{$userId}{$a}";
+        }
+        else {
+                $cacheKey = "{$key}{$a}";
+        }
 
-		return $cacheKey;
-	}
-	
-	public function store($key, $value, $isUserValue, $userId = null) {
-		
-		$cacheKey = self::createCacheKey($key, $userId);
-		return apc_store($cacheKey, $value);
-	}
-	
-	public function fetch($key, $isUserValue, $userId = null) {
-		
-		$cacheKey = self::createCacheKey($key, $isUserValue, $userId);
-		return apc_fetch($cacheKey);
-	}
+        return $cacheKey;
+    }
+    
+    public function store($key, $value, $isUserValue, $userId = null) {
+            
+        $cacheKey = self::createCacheKey($key, $userId);
+        //XXX: Disabling APC on SaaS because it turns out we have multiple webservers
+        //     running, which means we have to use a distributed data cache like memcached.
+        //return apc_store($cacheKey, $value);
+        return false;
+    }
+    
+    public function fetch($key, $isUserValue, $userId = null) {
+            
+        $cacheKey = self::createCacheKey($key, $isUserValue, $userId);
+        //XXX: Disabling APC on SaaS because it turns out we have multiple webservers
+        //     running, which means we have to use a distributed data cache like memcached.
+        //return apc_fetch($cacheKey);
+        return false;
+    }
 }
