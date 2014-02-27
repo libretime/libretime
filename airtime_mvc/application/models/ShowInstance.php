e6896a3256d477109199c9bc757508d56081c333
Naomi <naomiaro@gmail.com>
2014-02-27 18:30:15 -0500

    show instance clean up

--- airtime_mvc/application/models/ShowInstance.php
+++ airtime_mvc/application/models/ShowInstance.php
@@ -510,6 +510,7 @@ SQL;
 
     }
 
+    //TODO this sucks.
     public static function getIsFull($p_start, $p_end)
     {
         $sql = <<<SQL
@@ -532,27 +533,7 @@ SQL;
         return $isFilled;
     }
 
-    public function showEmpty()
-    {
-        $sql = <<<SQL
-SELECT s.starts
-FROM cc_schedule AS s
-WHERE s.instance_id = :instance_id
-  AND s.playout_status >= 0
-  AND ((s.stream_id IS NOT NULL)
-       OR (s.file_id IS NOT NULL)) LIMIT 1
-SQL;
-        # TODO : use prepareAndExecute properly
-        $res = Application_Common_Database::prepareAndExecute($sql,
-            array( ':instance_id' => $this->_instanceId ), 'all' );
-        # TODO : A bit retarded. fix this later
-        foreach ($res as $r) {
-            return false;
-        }
-        return true;
-
-    }
-
+    //TODO get rid of this
     public function getShowListContent()
     {
         $con = Propel::getConnection();
