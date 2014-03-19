d6f9f3ecc95d331533331f34f85f9ef7c86a5340
Naomi <naomiaro@gmail.com>
2014-03-19 16:43:48 -0400

    CC-5450 : Refactor Media Management (Classes/DB) in Airtime
    working on playlist rules - owner tracks and repeat tracks.

--- airtime_mvc/application/models/airtime/Playlist.php
+++ airtime_mvc/application/models/airtime/Playlist.php
@@ -32,53 +32,6 @@ abstract class Playlist extends BasePlaylist implements \Interface_Playlistable
 	const RULE_ORDER_COLUMN = "column";
 	const RULE_ORDER_DIRECTION = "direction";
 	
-	protected function addLimitRule(&$query) {
-		
-		$ruleSet = $this->getRules();
-		
-		$limitValue = $ruleSet["limit"]["value"];
-		$limitUnit = $ruleSet["limit"]["unit"];
-		
-		if ($limitUnit === "items") {
-			$query->limit($limitValue);
-		}
-		else {
-			 $duration = "1:00:00";
-			 
-			 switch($limitUnit) {
-			 	
-			 	case "minutes":
-			 		if (isset($limitValue)) {
-			 			$secs = $limitValue * 60;
-			 			
-			 			$hour = floor($secs / 3600);
-			 			
-			 			$leftover = $secs - $hour*3600;
-			 			$min = floor($leftover / 60);
-			 			
-			 			$leftover = $secs - $min*60;
-			 			$sec = floor($leftover);
-			 			
-			 			$duration = "{$hour}:{$min}:{$sec}";
-			 		}
-			 		break;
-			 	case "hours":
-			 		if (isset($limitValue)) {
-			 			
-			 			$mins = $limitValue * 60;
-			 			
-			 			$hour = floor($mins / 60);
-			 			$min = floor($mins % 60);
-			 			
-			 			$duration = "{$hour}:{$min}";
-			 		}
-			 		break;
-			 }
-			 
-			 $query->where("windowedfilter.agglength < '$duration'");
-		}
-	}
-	
 	protected function getCriteriaRules(&$query) {
 		
 		//$pattern is like "%VALUE%", or just "VALUE" if % is not needed.
@@ -199,4 +152,181 @@ abstract class Playlist extends BasePlaylist implements \Interface_Playlistable
     
     	return $this;
     } // setRules()
+    
+    //takes seconds, returns in interval format.
+    private function formatLimitValue($secs) {
+    	 
+    	$hour = floor($secs / 3600);
+    	 
+    	$leftover = $secs - $hour*3600;
+    	$min = floor($leftover / 60);
+    	 
+    	$leftover = $leftover - $min*60;
+    	$sec = floor($leftover);
+    	 
+    	$duration = "{$hour}:{$min}:{$sec}";
+    	 
+    	return $duration;
+    }
+    
+    public function generateContent(PropelPDO $con) {
+    
+    	try {
+    
+    		$this->clearContent($con);
+    
+    		$ruleSet = $this->getRules();
+    		$criteria = isset($ruleSet["criteria"]) ? $ruleSet["criteria"] : array();
+    		Logging::info($criteria);
+    
+    		$query = AudioFileQuery::create();
+    
+    		//filter to this user's tracks only.
+    		if ($ruleSet[self::RULE_USERS_TRACKS_ONLY]) {
+    			$userService = new \Application_Service_UserService();
+    			$user = $userService->getCurrentUser();
+    			$query->filterByCcSubjs($user);
+    		}
+    
+    		$m = $query->getModelName();
+    		$query->withColumn("({$m}.Cueout - {$m}.Cuein)", "cuelength");
+    
+    		$criteriaRules = self::getCriteriaRules($query);
+    
+    		$conditionAnd = array();
+    		$conNum = 0;
+    		foreach ($criteria as $andBlock) {
+    			$conditionOr = array();
+    			 
+    			foreach ($andBlock as $orBlock) {
+    				$rule = $criteriaRules[$orBlock["modifier"]];
+    
+    				$column = $orBlock["criteria"];
+    				$input1 = $orBlock["input1"];
+    				$input2 = isset($orBlock["input2"]) ? $orBlock["input2"] : null;
+    
+    				$condition = $rule($column, $input1, $input2);
+    
+    				$conditionOr[] = $condition;
+    			}
+    			 
+    			$query->combine($conditionOr, 'or', $conNum);
+    			$conditionAnd[] = $conNum;
+    			$conNum++;
+    		}
+    
+    		$query->where($conditionAnd, 'and');
+    
+    		//order by a chosen column or by random.
+    		$order = $ruleSet["order"];
+    		if ($order["column"] != "") {
+    			$query->orderBy($order["column"], $order["direction"]);
+    		}
+    		else {
+    			$query->addAscendingOrderByColumn('random()');
+    		}
+    
+    		//filter length using the subquery to take advantage of window columns
+    		//added for length selection purposes.
+    		//using window function on subquery so that the ORDER BY statement can be run
+    		//(need this incase it's order by random())
+    		$windowedQuery = AudioFileQuery::create()
+    		->addSelectQuery($query, "mainfilter")
+    		->withColumn("SUM(mainfilter.cuelength) OVER (rows between unbounded preceding and current row)", "agglength");
+    
+    		//lag it to pick the item that just goes over the limit to not have empty space.
+    		$laggedQuery = AudioFileQuery::create()
+    		->addSelectQuery($windowedQuery, "windowedfilter")
+    		->withColumn("windowedfilter.agglength", "subtotal")
+    		->withColumn("LAG(windowedfilter.agglength) OVER (rows between unbounded preceding and current row)", "laglength");
+    
+    		//limit the query based on length or #items
+    		$limitedQuery = AudioFileQuery::create()
+    		->addSelectQuery($laggedQuery, "laggedfilter")
+    		->withColumn("laggedfilter.subtotal", "subtotal");
+    
+    		$limitValue = $ruleSet["limit"]["value"];
+    		$limitUnit = $ruleSet["limit"]["unit"];
+    
+    		if ($limitUnit === "items") {
+    			$limitedQuery->limit($limitValue);
+    		}
+    		else {
+    			$duration = "1:00:00";
+    				
+    			switch($limitUnit) {
+    
+    				case "minutes":
+    					if (isset($limitValue)) {
+    
+    						$secs = $limitValue * 60;
+    						$duration = $this->formatLimitValue($secs);
+    					}
+    					break;
+    				case "hours":
+    					if (isset($limitValue)) {
+    
+    						$secs = $limitValue * 3600;
+    						$duration = $this->formatLimitValue($secs);
+    					}
+    					break;
+    			}
+    
+    			$limitedQuery
+    			->where("laggedfilter.laglength < '$duration'")
+    			->_or()
+    			//the IS NULL item is the first in the list.
+    			->where("laggedfilter.laglength IS NULL");
+    		}
+    
+    		$files = $limitedQuery->find();
+    
+    		$ids = array();
+    		foreach ($files as $file) {
+    			$ids[] = $file->getId();
+    		}
+    
+    		//check if we've hit our time with the selected files, or if we must repeat tracks.
+    		if ($ruleSet[self::RULE_REPEAT_TRACKS]) {
+    			 
+    			switch($limitUnit) {
+    					
+    				case "items":
+    					while ($limitValue > count($ids)) {
+    						$index = mt_rand(0, count($files)-1);
+    						$file = $files[$index];
+    						$ids[] = $file->getId();
+    					}
+    						
+    					break;
+    				case "minutes":
+    				case "hours":
+    					$playlistLength = $files[count($files)-1]->getSubtotal();
+    					$playlistLengthSecs = \Application_Common_DateHelper::playlistTimeToSeconds($playlistLength);
+    					$durationSecs = \Application_Common_DateHelper::playlistTimeToSeconds($duration);
+    						
+    					while ($durationSecs > $playlistLengthSecs) {
+    						$index = mt_rand(0, count($files)-1);
+    						$file = $files[$index];
+    						$ids[] = $file->getId();
+    
+    						$filelength = $file->getCueLength();
+    						$filelengthSecs = \Application_Common_DateHelper::playlistTimeToSeconds($filelength);
+    						$playlistLengthSecs += $filelengthSecs;
+    					}
+    					break;
+    			}
+    		}
+    
+    		return $ids;
+    
+    		$con->commit();
+    	}
+    	catch (Exception $e) {
+    		$con->rollBack();
+    		Logging::error($e->getFile().$e->getLine());
+    		Logging::error($e->getMessage());
+    		throw $e;
+    	}
+    }
 }
