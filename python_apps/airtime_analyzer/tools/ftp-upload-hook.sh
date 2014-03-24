3bffb02bc058d0946185f1ae8eb469d7decb1995
drigato <denise.rigato@sourcefabric.org>
2014-03-24 18:22:34 -0400

    CC-5708: proftpd hook for new File API

---
+++ python_apps/airtime_analyzer/tools/ftp-upload-hook.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+php /home/denise/airtime/python_apps/airtime_analyzer/tools/message_sender.php '{"tmp_file_path":"/home/denise/uploads/01 - Larger Than Life.ogg", "import_directory":"/srv/airtime/stor/imported/1","original_filename":"01 - Larger Than Life.ogg","callback_url": "http://localhost/rest/media/1", "api_key":"3TQY7JASUNVZ8IGOZQXJ"}'
