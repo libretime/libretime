681fe4630b82eff8af3353ebac59ad7b66e83387
drigato <denise.rigato@sourcefabric.org>
2014-03-25 18:09:47 -0400

    CC-5708: proftpd hook for new File API
    url and API key are hard coded right now.
    Next task is to somehow decipher these with whatever ftp parameters we can pass in to the scipt.

--- python_apps/airtime_analyzer/tools/ftp-upload-hook.sh
+++ python_apps/airtime_analyzer/tools/ftp-upload-hook.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-php /home/denise/airtime/python_apps/airtime_analyzer/tools/message_sender.php '{"tmp_file_path":"/home/denise/uploads/01 - Larger Than Life.ogg", "import_directory":"/srv/airtime/stor/imported/1","original_filename":"01 - Larger Than Life.ogg","callback_url": "http://localhost/rest/media/1", "api_key":"3TQY7JASUNVZ8IGOZQXJ"}'
+path=$1
+filename="${path##*/}"
+curl http://localhost/rest/media -u 3188BDIMPJROQP89Z0OX: -X POST -F "file=@$path" -F "name=$filename"
