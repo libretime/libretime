700b28ae38adcdc01ffb8c176a613d405960ae87
drigato <denise.rigato@sourcefabric.org>
2014-03-27 11:09:18 -0400

    CC-5708: proftpd hook for new File API
    removed testing lines

--- python_apps/airtime_analyzer/tools/ftp-upload-hook.sh
+++ python_apps/airtime_analyzer/tools/ftp-upload-hook.sh
@@ -13,16 +13,14 @@ vhost_file=/mnt/airtimepro/system/vhost.map
 #instance_path will look like 1/1384, for example
 instance_path=$(echo $file_path | grep -Po "(?<=($base_instance_path)).*?(?=/srv)")
 
-#url to the airtime interface. bananas.airtime.pro, for example
+#post request url - http://bananas.airtime.pro/rest/media, for example
 url=http://
-url+=$(grep -E $instance_path ~/vhost.map | awk '{print $1;}')
+url+=$(grep -E $instance_path $vhost_file | awk '{print $1;}')
 url+=/rest/media
-#url=$(grep -E $instance_path $vhost_file | awk '{print $1;}')
 
 #path to specific instance's airtime.conf
 instance_conf_path=$base_instance_path$instance_path$airtime_conf_path
 
-api_key=$(sudo awk -F "=" '/api_key/ {print $2}' /etc/airtime/airtime.conf)
-#api_key=$(sudo awk -F "=" '/api_key/ {print $2}' $instance_conf_path)
+api_key=$(sudo awk -F "=" '/api_key/ {print $2}' $instance_conf_path)
 
 #curl $url -u $api_key":" -X POST -F "file=@$file_path" -F "name=$filename"
