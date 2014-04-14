63e2eda64b082cc7af854f9e10a5cb4cf0c0b7e6
drigato <denise.rigato@sourcefabric.org>
2014-04-14 12:09:15 -0400

    CC-5786: Quota Enforcement in the File Upload API
    Tweaked this so it will work on self-hosted instances

--- airtime_mvc/application/controllers/PluploadController.php
+++ airtime_mvc/application/controllers/PluploadController.php
@@ -27,7 +27,7 @@ class PluploadController extends Zend_Controller_Action
         $this->view->headLink()->appendStylesheet($baseUrl.'css/addmedia.css?'.$CC_CONFIG['airtime_version']);
 
         $this->view->quotaLimitReached = false;
-        if (Application_Model_Preference::getDiskUsage() > Application_Model_Preference::getDiskQuota()) {
+        if (Application_Model_Systemstatus::isDiskOverQuota()) {
             $this->view->quotaLimitReached = true;
         }
     }
