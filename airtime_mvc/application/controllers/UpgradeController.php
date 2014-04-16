456ac40fe6eddccd5f55ca60cd8ce0c1bad76aaa
drigato <denise.rigato@sourcefabric.org>
2014-04-16 17:27:15 -0400

    Upgrade action testing

--- airtime_mvc/application/controllers/UpgradeController.php
+++ airtime_mvc/application/controllers/UpgradeController.php
@@ -13,9 +13,9 @@ class UpgradeController extends Zend_Controller_Action
             return;
         }
         
-        if (!$this->verifyAirtimeVersion()) {
+        /*if (!$this->verifyAirtimeVersion()) {
             return;
-        }
+        }*/
         
         //Begin upgrade
         $filename = isset($_SERVER['AIRTIME_CONF']) ? $_SERVER['AIRTIME_CONF'] : "/etc/airtime/airtime.conf";
@@ -29,16 +29,10 @@ class UpgradeController extends Zend_Controller_Action
         
         passthru("export PGPASSWORD=$password && psql -h $host -U $username -q -f $dir/upgrade_sql/airtime_$airtime_upgrade_version/upgrade.sql $database 2>&1 | grep -v \"will create implicit index\"");
         
-        $musicDir = CcMusicDirsQuery::create()
-            ->filterByType('stor')
-            ->filterByExists(true)
-            ->findOne();
-        $storPath = $musicDir->getDirectory();
+        $storDir = isset($_SERVER['AIRTIME_BASE']) ? $_SERVER['AIRTIME_BASE']."srv/airtime/stor" : "/srv/airtime/stor";
+        $diskUsage = shell_exec("du -s $storDir | awk '{print $1}'");
         
-        $freeSpace = disk_free_space($storPath);
-        $totalSpace = disk_total_space($storPath);
-        
-        Application_Model_Preference::setDiskUsage($totalSpace - $freeSpace);
+        Application_Model_Preference::setDiskUsage($diskUsage);
 
         $this->getResponse()
             ->setHttpResponseCode(200)
