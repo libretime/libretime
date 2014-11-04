3024daba0ebd70626f8fd2e4bdd035eb72855180
Duncan Sommerville <duncan.sommerville@gmail.com>
2014-11-04 11:06:17 -0500

    Changed tabs to spaces

--- airtime_mvc/application/Bootstrap.php
+++ airtime_mvc/application/Bootstrap.php
@@ -17,7 +17,7 @@ require_once "Timezone.php";
 require_once "Auth.php";
 require_once __DIR__.'/forms/helpers/ValidationTypes.php';
 require_once __DIR__.'/controllers/plugins/RabbitMqPlugin.php';
- 
+
 
 require_once (APPLICATION_PATH."/logging/Logging.php");
 Logging::setLogPath('/var/log/airtime/zendphp.log');
@@ -105,18 +105,18 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $view->headScript()->appendScript("$.i18n.setDictionary(general_dict)");
         $view->headScript()->appendScript("var baseUrl='$baseUrl'");
         
-		//These timezones are needed to adjust javascript Date objects on the client to make sense to the user's set timezone
-		//or the server's set timezone.
+        //These timezones are needed to adjust javascript Date objects on the client to make sense to the user's set timezone
+        //or the server's set timezone.
         $serverTimeZone = new DateTimeZone(Application_Model_Preference::GetDefaultTimezone());
         $now = new DateTime("now", $serverTimeZone);
         $offset = $now->format("Z") * -1;
         $view->headScript()->appendScript("var serverTimezoneOffset = {$offset}; //in seconds");
         
         if (class_exists("Zend_Auth", false) && Zend_Auth::getInstance()->hasIdentity()) {
-        	$userTimeZone = new DateTimeZone(Application_Model_Preference::GetUserTimezone());
-        	$now = new DateTime("now", $userTimeZone);
-        	$offset = $now->format("Z") * -1;
-        	$view->headScript()->appendScript("var userTimezoneOffset = {$offset}; //in seconds");
+            $userTimeZone = new DateTimeZone(Application_Model_Preference::GetUserTimezone());
+            $now = new DateTime("now", $userTimeZone);
+            $offset = $now->format("Z") * -1;
+            $view->headScript()->appendScript("var userTimezoneOffset = {$offset}; //in seconds");
         }
         
         //scripts for now playing bar
@@ -167,7 +167,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     protected function _initZFDebug()
     {
 
-        Zend_Controller_Front::getInstance()->throwExceptions(true);
+        Zend_Controller_Front::getInstance()->throwExceptions(false);
 
         /*
         if (APPLICATION_ENV == "development") {
