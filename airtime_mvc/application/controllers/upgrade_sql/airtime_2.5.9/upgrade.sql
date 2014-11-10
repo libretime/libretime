13f6b693d64021121875319c101c2225caeebbca
drigato <denise.rigato@sourcefabric.org>
2014-11-10 10:37:01 -0500

    Fixed upgrade.sql script

--- airtime_mvc/application/controllers/upgrade_sql/airtime_2.5.9/upgrade.sql
+++ airtime_mvc/application/controllers/upgrade_sql/airtime_2.5.9/upgrade.sql
@@ -1,7 +1,7 @@
 CREATE TABLE cloud_file
 (
-    id serial NOT NULL,
-    resource_id text NOT NULL default nextval('cloud_file_id_seq'::regclass),
+    id serial NOT NULL default nextval('cloud_file_id_seq'::regclass),
+    resource_id text NOT NULL,
     cc_file_id integer,
     CONSTRAINT cloud_file_pkey PRIMARY KEY (id),
     CONSTRAINT "cloud_file_FK_1" FOREIGN KEY (cc_file_id)
