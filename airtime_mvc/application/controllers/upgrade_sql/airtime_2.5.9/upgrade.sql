87337888074aa259fd1c9ba613a06364804a3df4
drigato <denise.rigato@sourcefabric.org>
2014-11-10 10:55:15 -0500

    Added NOT NULL to cloud_file foreign key field

--- airtime_mvc/application/controllers/upgrade_sql/airtime_2.5.9/upgrade.sql
+++ airtime_mvc/application/controllers/upgrade_sql/airtime_2.5.9/upgrade.sql
@@ -2,7 +2,7 @@ CREATE TABLE cloud_file
 (
     id serial NOT NULL,
     resource_id text NOT NULL,
-    cc_file_id integer,
+    cc_file_id integer NOT NULL,
     CONSTRAINT cloud_file_pkey PRIMARY KEY (id),
     CONSTRAINT "cloud_file_FK_1" FOREIGN KEY (cc_file_id)
         REFERENCES cc_files (id) MATCH SIMPLE
