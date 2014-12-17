97805ad271d935b1e056b7abb4b48e0379657d18
Duncan Sommerville <duncan.sommerville@gmail.com>
2014-12-17 16:22:40 -0500

    Removed many unnecessary files and started changing pypo to use setuptools

--- install_minimal/upgrades/airtime-2.2.1/data/upgrade.sql
+++
@@ -1,9 +0,0 @@
-DELETE FROM cc_pref WHERE keystr = 'system_version';
-INSERT INTO cc_pref (keystr, valstr) VALUES ('system_version', '2.2.1');
-
-ALTER TABLE cc_block
-	DROP CONSTRAINT cc_block_createdby_fkey;
-
-ALTER TABLE cc_block
-	ADD CONSTRAINT cc_block_createdby_fkey FOREIGN KEY (creator_id) REFERENCES cc_subjs(id) ON DELETE CASCADE;
-
