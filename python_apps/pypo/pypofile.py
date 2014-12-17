28a06136aa7df66d77ad1fba6ab037f4aa878af5
drigato <denise.rigato@sourcefabric.org>
2014-12-17 18:30:06 -0500

    Better error logging when pypo downloads files

--- python_apps/pypo/pypofile.py
+++ python_apps/pypo/pypofile.py
@@ -73,6 +73,7 @@ class PypoFile(Thread):
                     response = requests.get(url, auth=requests.auth.HTTPBasicAuth(username, ''), stream=True, verify=False)
                     
                     if not response.ok:
+                        self.logger.error(response)
                         raise Exception("%s - Error occurred downloading file" % response.status_code)
                     
                     for chunk in response.iter_content(1024):
