609a46dfa3d994bb31dfcf1ebff055f3a566b99c
Albert Santoni <albert.santoni@sourcefabric.org>
2015-01-14 15:12:20 -0500

    Comment Airtime_Decorator_SuperAdmin_Only

--- airtime_mvc/application/forms/helpers/CustomDecorators.php
+++ airtime_mvc/application/forms/helpers/CustomDecorators.php
@@ -1,5 +1,6 @@
 <?php
 
+/** Hide a Zend_Form_Element unless you're logged in as a SuperAdmin. */
 class Airtime_Decorator_SuperAdmin_Only extends Zend_Form_Decorator_Abstract
 {
     public function render($content)
