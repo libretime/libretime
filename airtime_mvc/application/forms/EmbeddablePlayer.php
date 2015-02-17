4fd7b4c622315ef94e3c16b03eaae271c0acddb3
drigato <denise.rigato@sourcefabric.org>
2015-02-17 11:48:43 -0500

    SAAS-585: Create Embeddable Player form
    Added stream urls - hardcoded for now

--- airtime_mvc/application/forms/EmbeddablePlayer.php
+++ airtime_mvc/application/forms/EmbeddablePlayer.php
@@ -8,11 +8,18 @@ class Application_Form_EmbeddablePlayer extends Zend_Form_SubForm
             array('ViewScript', array('viewScript' => 'form/embeddableplayer.html'))
         ));
 
-
         $displayTrackMetadata = new Zend_Form_Element_Checkbox('display_track_metadata');
         $displayTrackMetadata->setValue(true);
         $displayTrackMetadata->setLabel(_('Display track metadata?'));
         $this->addElement($displayTrackMetadata);
+
+        $streamURL = new Zend_Form_Element_Radio('stream_url');
+        $streamURL->setMultiOptions(array(
+            'AAC' => 'http://127.0.0.1:8000/airtime_a',
+            'MP3' => 'http://127.0.0.1:8000/airtime_b'
+        ));
+        $streamURL->setLabel(_('Select stream:'));
+        $this->addElement($streamURL);
 
         $submit = new Zend_Form_Element_Submit('submit');
         $this->addElement($submit);
