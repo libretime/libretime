99d16444d26633d154cb0ed75e6ca20d85e084b3
Albert Santoni <albert.santoni@sourcefabric.org>
2015-03-25 10:51:51 -0400

    Squash unlink warning in CcFiles.php

--- airtime_mvc/application/models/airtime/CcFiles.php
+++ airtime_mvc/application/models/airtime/CcFiles.php
@@ -95,9 +95,10 @@ class CcFiles extends BaseCcFiles {
 
         try {
             self::createAndImport($fileArray, $tempFilePath, $originalFilename);
-        } catch (Exception $e)
-        {
-            @unlink($tempFilePath);
+        } catch (Exception $e) {
+            if (file_exists($tempFilePath)) {
+                unlink($tempFilePath);
+            }
             throw $e;
         }
     }
