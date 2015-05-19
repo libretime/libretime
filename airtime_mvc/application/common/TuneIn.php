2da4e264e56b9a57df5a6c998ecface1735d7edf
drigato <denise.rigato@sourcefabric.org>
2015-05-19 15:42:07 -0400

    SAAS-772: Send metadata to Tunein
    Update metadata when station goes offline

--- airtime_mvc/application/common/TuneIn.php
+++ airtime_mvc/application/common/TuneIn.php
@@ -4,15 +4,11 @@ class Application_Common_TuneIn
 {
     public static function sendMetadataToTunein($title, $artist)
     {
-        $tuneInStationID = Application_Model_Preference::getTuneinStationId();
-        $tuneInPartnerID = Application_Model_Preference::getTuneinPartnerId();
-        $tuneInPartnerKey = Application_Model_Preference::getTuneinPartnerKey();
-
-        $qry_str = "?partnerId=".$tuneInPartnerID."&partnerKey=".$tuneInPartnerKey."&id=".$tuneInStationID
-            ."&title=".$title."&artist=".$artist."&commercial=false";
+        $credQryStr = self::getCredentialsQueryString();
+        $metadataQryStr = "&title=".$title."&artist=".$artist."&commercial=false";
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, TUNEIN_API_URL . $qry_str);
+        curl_setopt($ch, CURLOPT_URL, TUNEIN_API_URL . $credQryStr . $metadataQryStr);
         curl_setopt($ch, CURLOPT_FAILONERROR, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_TIMEOUT, 30);
@@ -25,6 +21,28 @@ class Application_Common_TuneIn
 
     }
 
-    public static function updateOfflineMetadata()
+    private static function getCredentialsQueryString() {
+        $tuneInStationID = Application_Model_Preference::getTuneinStationId();
+        $tuneInPartnerID = Application_Model_Preference::getTuneinPartnerId();
+        $tuneInPartnerKey = Application_Model_Preference::getTuneinPartnerKey();
+
+        return "?partnerId=".$tuneInPartnerID."&partnerKey=".$tuneInPartnerKey."&id=".$tuneInStationID;
+    }
+
+    public static function updateOfflineMetadata() {
+        $credQryStr = self::getCredentialsQueryString();
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, TUNEIN_API_URL . $credQryStr . "&commercial=true");
+        curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+
+        curl_exec($ch);
+        if (curl_error($ch)) {
+            Logging::error("Failed to reach TuneIn: ". curl_errno($ch)." - ". curl_error($ch) . " - " . curl_getinfo($ch, CURLINFO_EFFECTIVE_URL));
+        }
+        curl_close($ch);
+    }
 
 }
