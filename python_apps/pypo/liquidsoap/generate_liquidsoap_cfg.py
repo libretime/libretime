196e08fc90a6ad099ed2912b42fb5f68b4ed8adc
Duncan Sommerville <duncan.sommerville@gmail.com>
2015-05-21 12:49:26 -0400

    Fix bug where users can't connect to Master source due to hardcoded path in liquidsoap

--- python_apps/pypo/liquidsoap/generate_liquidsoap_cfg.py
+++ python_apps/pypo/liquidsoap/generate_liquidsoap_cfg.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import sys
 import time
 import traceback
@@ -23,7 +24,9 @@ def generate_liquidsoap_config(ss):
         str_buffer = "%s = %s\n" % (key, val)
         fh.write(str_buffer.encode('utf-8'))
 
+    auth_path = os.path.dirname(os.path.realpath(__file__))
     fh.write('log_file = "/var/log/airtime/pypo-liquidsoap/<script>.log"\n')
+    fh.write('auth_path = "%s/liquidsoap_auth.py"\n' % auth_path)
     fh.close()
     
 def run():
