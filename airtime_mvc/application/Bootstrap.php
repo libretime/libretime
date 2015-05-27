a74a16193619d5de2e69d1e4fe585a48fafa1337
drigato <denise.rigato@sourcefabric.org>
2015-05-27 10:18:50 -0400

    SAAS-812: Remove live chat from radio page

--- airtime_mvc/application/Bootstrap.php
+++ airtime_mvc/application/Bootstrap.php
@@ -194,12 +194,12 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         }
 
         $view->headScript()->appendScript("var userType = '$userType';");
-
         if (array_key_exists('REQUEST_URI', $_SERVER)) { //Doesn't exist for unit tests
             if (strpos($_SERVER['REQUEST_URI'], $baseUrl . 'Dashboard/stream-player') === false
                 && strpos($_SERVER['REQUEST_URI'], $baseUrl . 'audiopreview/audio-preview') === false
                 && strpos($_SERVER['REQUEST_URI'], $baseUrl . 'audiopreview/playlist-preview') === false
                 && strpos($_SERVER['REQUEST_URI'], $baseUrl . 'audiopreview/block-preview') === false
+                && $_SERVER['REQUEST_URI'] != "/"
             ) {
                 $plan_level = strval(Application_Model_Preference::GetPlanLevel());
                 // Since the Hobbyist plan doesn't come with Live Chat support, don't enable it
