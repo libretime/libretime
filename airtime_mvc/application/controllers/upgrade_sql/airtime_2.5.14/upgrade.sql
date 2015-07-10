07238260ed04cd6b2a28684357caa9d935290194
Duncan Sommerville <duncan.sommerville@gmail.com>
2015-07-10 10:27:28 -0400

    Remove incorrect sql in 2.5.14 upgrade

--- airtime_mvc/application/controllers/upgrade_sql/airtime_2.5.14/upgrade.sql
+++ airtime_mvc/application/controllers/upgrade_sql/airtime_2.5.14/upgrade.sql
@@ -1,4 +1,3 @@
-ALTER TABLE cc_pref ALTER COLUMN subjid SET NULL;
 ALTER TABLE cc_pref ALTER COLUMN subjid SET DEFAULT NULL;
 CREATE UNIQUE INDEX cc_pref_key_idx ON cc_pref (keystr) WHERE subjid IS NULL;
 ANALYZE cc_pref;
