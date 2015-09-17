8a7a97f48ecba2052cfcd2e8fe763b01cd6669c3
Albert Santoni <albert.santoni@sourcefabric.org>
2015-09-17 15:06:51 -0400

    Updated the podcast table widget columns

--- airtime_mvc/public/js/airtime/library/library.js
+++ airtime_mvc/public/js/airtime/library/library.js
@@ -1239,11 +1239,13 @@ var AIRTIME = (function(AIRTIME) {
     mod.initPodcastDatatable = function()
     {
         var aoColumns = [
-            /* Title */           { "sTitle" : $.i18n._("Title")              , "mDataProp" : "track_title"  , "sClass"      : "library_title"       , "sWidth"      : "170px"                 },
-            /* Creator */         { "sTitle" : $.i18n._("Creator")            , "mDataProp" : "artist_name"  , "sClass"      : "library_creator"     , "sWidth"      : "160px"                 },
-            /* Upload Time */     { "sTitle" : $.i18n._("Uploaded")           , "mDataProp" : "utime"        , "bVisible"    : false                 , "sClass"      : "library_upload_time"   , "sWidth" : "155px"        },
-            /* Website */         { "sTitle" : $.i18n._("Website")            , "mDataProp" : "info_url"     , "bVisible"    : false                 , "sClass"      : "library_url"           , "sWidth" : "150px"        },
-            /* Year */            { "sTitle" : $.i18n._("Year")               , "mDataProp" : "year"         , "bVisible"    : false                 , "sClass"      : "library_year"          , "sWidth" : "60px"         },
+            /* Title */           { "sTitle" : $.i18n._("Title")              , "mDataProp" : "title"  , "sClass"      : "library_title"       , "sWidth"      : "170px"                 },
+            /* Creator */         { "sTitle" : $.i18n._("Creator")            , "mDataProp" : "creator"  , "sClass"      : "library_creator"     , "sWidth"      : "160px"                 },
+            /* Upload Time      { "sTitle" : $.i18n._("Uploaded")           , "mDataProp" : "utime"        , "bVisible"    : false                 , "sClass"      : "library_upload_time"   , "sWidth" : "155px"        }, */
+            /* Website */         { "sTitle" : $.i18n._("Description")            , "mDataProp" : "description"     , "bVisible"    : false          , "sWidth" : "150px"        },
+            /* Year */            { "sTitle" : $.i18n._("Owner")               , "mDataProp" : "owner"         , "bVisible"    : false          , "sWidth" : "60px"         },
+            /* URL */            { "sTitle" : $.i18n._("Feed URL")               , "mDataProp" : "url"         , "bVisible"    : false          , "sWidth" : "60px"         },
+
         ];
         var ajaxSourceURL = baseUrl+"rest/podcast";
 
