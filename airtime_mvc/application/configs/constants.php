ad2e26a1c3e8de8de3a538c58013ae1a4d9de499
Daniel James <daniel@64studio.com>
2015-09-21 17:44:00 +0100

    IN-1956 Update Transifex project link

--- airtime_mvc/application/configs/constants.php
+++ airtime_mvc/application/configs/constants.php
@@ -17,7 +17,7 @@ define('WHOS_USING_URL'             , 'http://sourcefabric.org/en/airtime/whosus
 define('TERMS_AND_CONDITIONS_URL'   , 'http://www.sourcefabric.org/en/about/policy/');
 define('PRIVACY_POLICY_URL'         , 'http://www.sourcefabric.org/en/about/policy/');
 define('USER_MANUAL_URL'            , 'http://sourcefabric.booktype.pro/airtime-pro-for-broadcasters');
-define('AIRTIME_TRANSIFEX_URL'      , 'https://www.transifex.com/projects/p/airtime/');
+define('AIRTIME_TRANSIFEX_URL'      , 'https://www.transifex.com/sourcefabric/airtime/');
 define('WHMCS_PASSWORD_RESET_URL'   , 'https://account.sourcefabric.com/pwreset.php');
 
 define('LICENSE_VERSION'    , 'GNU AGPL v.3');
