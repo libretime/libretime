79db208cd4782d94d7b3a143edd686f7ec97b491
Duncan Sommerville <duncan.sommerville@gmail.com>
2015-09-22 14:18:48 -0400

    Refactor SoundCloud/Third party controllers to fit new service architecture

--- airtime_mvc/application/controllers/SoundcloudController.php
+++ airtime_mvc/application/controllers/SoundcloudController.php
@@ -24,6 +24,45 @@ class SoundcloudController extends ThirdPartyController implements OAuth2Control
     }
 
     /**
+     * Upload the file with the given id to a third-party service
+     *
+     * @return void
+     *
+     * @throws Zend_Controller_Response_Exception thrown if upload fails for any reason
+     */
+    public function uploadAction() {
+        $request = $this->getRequest();
+        $id = $request->getParam('id');
+        $this->_service->upload($id);
+    }
+
+    /**
+     * Download the file with the given id from a third-party service
+     *
+     * @return void
+     *
+     * @throws Zend_Controller_Response_Exception thrown if download fails for any reason
+     */
+    public function downloadAction() {
+        $request = $this->getRequest();
+        $id = $request->getParam('id');
+        $this->_service->download($id);
+    }
+
+    /**
+     * Delete the file with the given id from a third-party service
+     *
+     * @return void
+     *
+     * @throws Zend_Controller_Response_Exception thrown if deletion fails for any reason
+     */
+    public function deleteAction() {
+        $request = $this->getRequest();
+        $id = $request->getParam('id');
+        $this->_service->delete($id);
+    }
+
+    /**
      * Send user to SoundCloud to authorize before being redirected
      *
      * @return void
--- airtime_mvc/application/controllers/ThirdPartyController.php
+++ airtime_mvc/application/controllers/ThirdPartyController.php
@@ -24,50 +24,10 @@ abstract class ThirdPartyController extends Zend_Controller_Action {
      * Disable controller rendering and initialize
      */
     public function init() {
-        $CC_CONFIG = Config::getConfig();
-        $this->_baseUrl = 'http://' . $CC_CONFIG['baseUrl'] . ":" . $CC_CONFIG['basePort'] . "/";
+        $this->_baseUrl = Application_Common_HTTPHelper::getStationUrl();
 
         $this->view->layout()->disableLayout();  // Don't inject the standard Now Playing header.
         $this->_helper->viewRenderer->setNoRender(true);  // Don't use (phtml) templates
-    }
-
-    /**
-     * Upload the file with the given id to a third-party service
-     *
-     * @return void
-     *
-     * @throws Zend_Controller_Response_Exception thrown if upload fails for any reason
-     */
-    public function uploadAction() {
-        $request = $this->getRequest();
-        $id = $request->getParam('id');
-        $this->_service->upload($id);
-    }
-
-    /**
-     * Download the file with the given id from a third-party service
-     *
-     * @return void
-     *
-     * @throws Zend_Controller_Response_Exception thrown if download fails for any reason
-     */
-    public function downloadAction() {
-        $request = $this->getRequest();
-        $id = $request->getParam('id');
-        $this->_service->download($id);
-    }
-
-    /**
-     * Delete the file with the given id from a third-party service
-     *
-     * @return void
-     *
-     * @throws Zend_Controller_Response_Exception thrown if deletion fails for any reason
-     */
-    public function deleteAction() {
-        $request = $this->getRequest();
-        $id = $request->getParam('id');
-        $this->_service->delete($id);
     }
 
 }
