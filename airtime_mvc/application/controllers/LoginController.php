91c584ba164a06cd21075944986bb51cc3f975bc
Albert Santoni <albert.santoni@sourcefabric.org>
2015-09-25 11:37:36 -0400

    Test alternate session reopening code to alleviate double Set-Cookie header

--- airtime_mvc/application/controllers/LoginController.php
+++ airtime_mvc/application/controllers/LoginController.php
@@ -33,8 +33,15 @@ class LoginController extends Zend_Controller_Action
         Application_Model_Locale::configureLocalization($request->getcookie('airtime_locale', $stationLocale));
 
         if (Zend_Session::isStarted()) {
+
+            ini_set('session.use_only_cookies', false);
+            ini_set('session.use_cookies', false);
+            ini_set('session.use_trans_sid', false);
+            ini_set('session.cache_limiter', null);
+            session_start(); // second session_start
+
             //Open the session for writing, because we close it for writing by default in Bootstrap.php as an optimization.
-            session_start();
+            //session_start();
 
             $auth = Zend_Auth::getInstance();
             $auth->getStorage();
