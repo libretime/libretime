51ec5a0df527246b9842afee698cf72133f1c330
drigato <denise.rigato@sourcefabric.org>
2015-11-03 11:29:50 -0500

    SAAS-1073: Schema upgrade for podcasts
    Delete station podcast id from cc_pref in downgrade sql

--- airtime_mvc/application/controllers/downgrade_sql/airtime_2.5.16/downgrade.sql
+++ airtime_mvc/application/controllers/downgrade_sql/airtime_2.5.16/downgrade.sql
@@ -1,5 +1,7 @@
 ALTER TABLE cc_files DROP COLUMN description;
 
+DELETE FROM cc_pref WHERE keystr = 'station_podcast_id';
+
 DROP TABLE IF EXISTS "podcast" CASCADE;
 
 DROP TABLE IF EXISTS "imported_podcast" CASCADE;
