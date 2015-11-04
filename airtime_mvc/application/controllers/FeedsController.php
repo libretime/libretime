90ac2cc681c51f5270364208abdbb041285d0c3c
Duncan Sommerville <duncan.sommerville@gmail.com>
2015-11-04 15:57:51 -0500

    Fix bug where public station podcast feed would not display when logged out

--- airtime_mvc/application/controllers/FeedsController.php
+++ airtime_mvc/application/controllers/FeedsController.php
@@ -7,9 +7,9 @@ class FeedsController extends Zend_Controller_Action
         $this->view->layout()->disableLayout();
         $this->_helper->viewRenderer->setNoRender(true);
 
-        if (!RestAuth::verifyAuth(true, true, $this)
-            && (Application_Model_Preference::getStationPodcastPrivacy()
-            && $this->getRequest()->getParam("sharing_token") != Application_Model_Preference::getStationPodcastDownloadKey())) {
+        if ((Application_Model_Preference::getStationPodcastPrivacy()
+                && $this->getRequest()->getParam("sharing_token") != Application_Model_Preference::getStationPodcastDownloadKey())
+                && !RestAuth::verifyAuth(true, true, $this)) {
             $this->getResponse()
                 ->setHttpResponseCode(401);
             return;
