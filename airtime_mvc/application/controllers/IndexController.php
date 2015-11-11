faeaf5450bdb86c1f7fec8c1c6b2301e2d4ed4c6
drigato <denise.rigato@sourcefabric.org>
2015-11-11 15:35:07 -0500

    SAAS-1203: Sort published tracks on radio page by publication date DESC

--- airtime_mvc/application/controllers/IndexController.php
+++ airtime_mvc/application/controllers/IndexController.php
@@ -53,7 +53,7 @@ class IndexController extends Zend_Controller_Action
         //station feed episodes
         $stationPodcastId = Application_Model_Preference::getStationPodcastId();
         $podcastEpisodesService = new Application_Service_PodcastEpisodeService();
-        $episodes = $podcastEpisodesService->getPodcastEpisodes($stationPodcastId, 0, 0);
+        $episodes = $podcastEpisodesService->getPodcastEpisodes($stationPodcastId, 0, 0, PodcastEpisodesPeer::PUBLICATION_DATE, "DESC");
         foreach ($episodes as $e => $v) {
             $episodes[$e]["CcFiles"]["track_title"] = htmlspecialchars($v["CcFiles"]["track_title"], ENT_QUOTES);
             $episodes[$e]["CcFiles"]["artist_name"] = htmlspecialchars($v["CcFiles"]["artist_name"], ENT_QUOTES);
