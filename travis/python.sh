69a6eeb7bb0af2e422c7bf9db3f3d59627eb25d5
Lucas Bickel <hairmare@rabe.ch>
2017-03-04 16:40:23 +0100

    Build docs from conditional python script

--- travis/python.sh
+++ travis/python.sh
@@ -8,3 +8,7 @@ pushd python_apps/airtime_analyzer
 nosetests -a '!rgain'
 echo "replaygain tests where skipped due to not having a reliable replaygain install on travis."
 popd
+
+echo "Building docs..."
+mkdocs build --clean -q > /dev/null
+echo -n "done"
