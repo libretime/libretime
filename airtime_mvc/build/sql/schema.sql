7f00182913001bb0d039dc5c0d0766d0695654aa
Lucas Bickel <hairmare@rabe.ch>
2017-03-18 11:37:45 +0100

    Allow 0 file_id in third_party_trackrefs
    The previous constraint of NOT NULL made it impossible to create a placeholder entry for later downloading. This uses a 0 default instead of the constraint and downloading as well as the green checkbox work again.

--- airtime_mvc/build/sql/schema.sql
+++ airtime_mvc/build/sql/schema.sql
@@ -685,7 +685,7 @@ CREATE TABLE "third_party_track_references"
     "id" serial NOT NULL,
     "service" VARCHAR(256) NOT NULL,
     "foreign_id" VARCHAR(256),
-    "file_id" INTEGER NOT NULL,
+    "file_id" INTEGER DEFAULT 0 NOT NULL,
     "upload_time" TIMESTAMP,
     "status" VARCHAR(256),
     PRIMARY KEY ("id"),
