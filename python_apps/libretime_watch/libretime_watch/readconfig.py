958aaf9bb04810f12e2224129ffb5a7ab5411c43
Hans-Joachim <rni@chef.net>
2017-04-26 07:49:19 +0200

    added upstart script

--- python_apps/libretime_watch/libretime_watch/readconfig.py
+++ python_apps/libretime_watch/libretime_watch/readconfig.py
@@ -3,26 +3,24 @@
 import ConfigParser
 import logging
 import os
-import libretime_watch
+import libretime_watch as libretime_watch
 
 CONFIGFILE="/etc/airtime/airtime.conf"
-
-config = {}
 
 def read_config():
   """Read airtime configfile"""
   try: 
     Config = ConfigParser.ConfigParser()
     Config.read(CONFIGFILE)
-    config["db_host"]=Config.get('database','host')
-    config["db_name"]=Config.get('database','dbname')
-    config["db_user"]=Config.get('database','dbuser')
-    config["db_pass"]=Config.get('database','dbpass')
-    config["rm_host"]=Config.get('rabbitmq','host')
-    config["rm_vhost"]=Config.get('rabbitmq','vhost')
-    config["rm_port"]=Config.get('rabbitmq','port')
-    config["rm_user"]=Config.get('rabbitmq','user')
-    config["rm_pass"]=Config.get('rabbitmq','password')
+    libretime_watch.config["db_host"]=Config.get('database','host')
+    libretime_watch.config["db_name"]=Config.get('database','dbname')
+    libretime_watch.config["db_user"]=Config.get('database','dbuser')
+    libretime_watch.config["db_pass"]=Config.get('database','dbpass')
+    libretime_watch.config["rm_host"]=Config.get('rabbitmq','host')
+    libretime_watch.config["rm_vhost"]=Config.get('rabbitmq','vhost')
+    libretime_watch.config["rm_port"]=Config.get('rabbitmq','port')
+    libretime_watch.config["rm_user"]=Config.get('rabbitmq','user')
+    libretime_watch.config["rm_pass"]=Config.get('rabbitmq','password')
   except:
     logging.error ("can't open the configfile")  
-  return config
+  return libretime_watch.config
