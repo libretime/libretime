919c77d8f1b081f79a6f66c10ddf8bc6be8672f5
Robb Ebright <ebright.48@osu.edu>
2017-08-09 13:24:23 -0400

    adding block model data

---
+++ airtime_mvc/tests/application/testdata/BlockModelData.php
@@ -0,0 +1,18 @@
+<?php
+Class BlockModelData
+{
+    public static function getCriteriaSingleNewestLabelNada() {
+
+        return array(
+            Array("name" => "sp_type", "value" => 0),
+            Array("name" => "sp_type", "value" => 0),
+            Array("name" => "sp_repeat_tracks", "value" => 0),
+            Array("name" => "sp_sort_options", "value" => "newest"),
+            Array("name" => "sp_limit_value", "value" => 1),
+            Array("name" => "sp_limit_options", "value" => "items"),
+            Array("name" => "sp_criteria_field_0_0", "value" => "label"),
+            Array("name" => "sp_criteria_modifier_0_0", "value" => "contains"),
+            Array("name" => "sp_criteria_value_0_0", "value" => "nada"),
+            );
+    }
+}
