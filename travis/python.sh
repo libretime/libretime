1ba907c5a179ce918bf7b19db974588f55de7243
Lucas Bickel <hairmare@rabe.ch>
2017-09-17 11:35:36 +0200

    Install deps for test

--- travis/python.sh
+++ travis/python.sh
@@ -4,7 +4,8 @@ set -xe
 
 [[ "$PYTHON" == false ]] && exit 0
 
-pushd python_apps/airtime_analyzer 
+pushd python_apps/airtime_analyzer
+pip install -e .
 nosetests -a '!rgain'
 echo "replaygain tests where skipped due to not having a reliable replaygain install on travis."
 popd
