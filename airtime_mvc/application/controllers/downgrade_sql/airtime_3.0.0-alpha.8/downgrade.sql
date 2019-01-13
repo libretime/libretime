a9ca4432f901eea082f2b300e4d0b32c9ed09857
Robbt <robbt@azone.org>
2019-01-13 00:02:07 -0500

    added upgrade and downgrade scripts

---
+++ airtime_mvc/application/controllers/downgrade_sql/airtime_3.0.0-alpha.8/downgrade.sql
@@ -0,0 +1,2 @@
+ALTER TABLE podcast_episodes DROP COLUMN IF EXISTS episode_title;
+ALTER TABLE podcast_episodes DROP COLUMN IF EXISTS episode_description;
---
+++ airtime_mvc/application/controllers/upgrade_sql/airtime_3.0.0-alpha.8/upgrade.sql
@@ -0,0 +1,2 @@
+ALTER TABLE podcast_episodes ADD COLUMN episode_title VARCHAR(4096) NOT NULL;
+ALTER TABLE podcast_episodes ADD COLUMN episode_description VARCHAR(4096) NOT NULL;
