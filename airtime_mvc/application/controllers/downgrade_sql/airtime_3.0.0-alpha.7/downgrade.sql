600a681999be8013314b0fcb695f58efc0b056a9
Robbt <robbt@azone.org>
2019-01-22 08:03:01 -0500

    moving sql back to alpha.7 for release

--- airtime_mvc/application/controllers/downgrade_sql/airtime_3.0.0-alpha.7/downgrade.sql
+++ airtime_mvc/application/controllers/downgrade_sql/airtime_3.0.0-alpha.7/downgrade.sql
@@ -1,1 +1,3 @@
 ALTER TABLE cc_block ALTER COLUMN type SET DEFAULT 'static';
+ALTER TABLE podcast_episodes DROP COLUMN IF EXISTS episode_title;
+ALTER TABLE podcast_episodes DROP COLUMN IF EXISTS episode_description;
