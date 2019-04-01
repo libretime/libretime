6f9ce404a8afca2409f44aabcfdf7f748c9c14b6
Robbt <robbt@azone.org>
2019-04-01 11:12:15 -0400

    Remove duplicate sql from existing lines and update Upgrades.php

--- airtime_mvc/application/controllers/downgrade_sql/airtime_3.0.0-alpha.7/downgrade.sql
+++ airtime_mvc/application/controllers/downgrade_sql/airtime_3.0.0-alpha.7/downgrade.sql
@@ -1,5 +1,1 @@
 ALTER TABLE cc_block ALTER COLUMN type SET DEFAULT 'static';
-ALTER TABLE podcast_episodes DROP COLUMN IF EXISTS episode_title;
-ALTER TABLE podcast_episodes DROP COLUMN IF EXISTS episode_description;
-ALTER TABLE cc_blockcriteria DROP COLUMN IF EXISTS criteriagroup;
-
--- airtime_mvc/application/controllers/upgrade_sql/airtime_3.0.0-alpha.7/upgrade.sql
+++ airtime_mvc/application/controllers/upgrade_sql/airtime_3.0.0-alpha.7/upgrade.sql
@@ -1,5 +1,1 @@
 ALTER TABLE cc_block ALTER COLUMN type SET DEFAULT 'dynamic';
-ALTER TABLE podcast_episodes ADD COLUMN episode_title VARCHAR(4096);
-ALTER TABLE podcast_episodes ADD COLUMN episode_description VARCHAR(4096);
-ALTER TABLE cc_blockcriteria ADD COLUMN criteriagroup integer;
-
