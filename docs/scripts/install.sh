a978ebdb9ebac2ea03331487b846ef8d9b092424
Kyle Robbertze <kyle@paddatrapper.com>
2020-01-22 15:18:13 +0200

    Follow pip best practices
    Let apt manage system pip3 version

--- docs/scripts/install.sh
+++ docs/scripts/install.sh
@@ -4,7 +4,5 @@ echo "Updating Apt."
 apt-get update > /dev/null
 echo "Ensuring Pip is installed."
 DEBIAN_FRONTEND=noninteractive apt-get install -y -qq python3-pip > /dev/null
-echo "Updating Pip."
-pip3 install pip -q -q --upgrade > /dev/null
 echo "Ensuring Mkdocs is installed."
 pip3 install mkdocs
