4174d20bb5607fe994ca93665e6c6258b5951e37
Kyle Robbertze <kyle@paddatrapper.com>
2020-01-22 12:10:37 +0200

    test api_clients and pypo

--- travis/python.sh
+++ travis/python.sh
@@ -4,8 +4,18 @@ set -xe
 
 [[ "$PYTHON" == false ]] && exit 0
 
-pushd python_apps/airtime_analyzer
 pyenv local 3.7
+pushd python_apps/airtime_analyzer
+pip3 install -e .
+nosetests
+popd
+
+pushd python_apps/api_clients
+pip3 install -e .
+nosetests
+popd
+
+pushd python_apps/pypo
 pip3 install -e .
 nosetests
 popd
