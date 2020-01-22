25101c79b192e72d313f1126ea37047977734f90
Kyle Robbertze <kyle@paddatrapper.com>
2020-01-22 12:14:40 +0200

    Fix tests

--- python_apps/api_clients/tests/test_requestprovider.py
+++ python_apps/api_clients/tests/test_requestprovider.py
@@ -2,13 +2,19 @@ import unittest
 import json
 from mock import patch, MagicMock
 from configobj import ConfigObj
-from .. api_client import RequestProvider
+from api_clients.api_client import RequestProvider, api_config
 
 class TestRequestProvider(unittest.TestCase):
     def setUp(self):
-        self.cfg = ConfigObj('api_client.cfg')
+        self.cfg = api_config
+        self.cfg['general'] = {}
+        self.cfg['general']['base_dir'] = '/test'
+        self.cfg['general']['base_port'] = 80
+        self.cfg['general']['base_url'] = 'localhost'
+        self.cfg['general']['api_key'] = 'TEST_KEY'
+        self.cfg['api_base'] = 'api'
     def test_test(self):
-        self.assertTrue('api_key' in self.cfg)
+        self.assertTrue('general' in self.cfg)
     def test_init(self):
         rp = RequestProvider(self.cfg)
         self.assertTrue( len( rp.available_requests() ) > 0 )
@@ -16,17 +22,6 @@ class TestRequestProvider(unittest.TestCase):
         rp = RequestProvider(self.cfg)
         methods = ['upload_recorded', 'update_media_url', 'list_all_db_files']
         for meth in methods:
-            self.assertTrue( meth in rp )
-
-    def test_notify_webstream_data(self):
-        ret = json.dumps( {'testing' : '123' } )
-        rp = RequestProvider(self.cfg)
-        read = MagicMock()
-        read.read = MagicMock(return_value=ret)
-        with patch('urllib2.urlopen') as mock_method:
-            mock_method.return_value = read
-            response = rp.notify_webstream_data(media_id=123)
-            mock_method.called_once_with(media_id=123)
-            self.assertEqual(json.loads(ret), response)
+            self.assertTrue( meth in rp.requests )
 
 if __name__ == '__main__': unittest.main()
--- python_apps/pypo/pypo/pypoliquidsoap.py
+++ python_apps/pypo/pypo/pypoliquidsoap.py
@@ -125,7 +125,7 @@ class PypoLiquidsoap():
             scheduled_now_webstream = \
                     [x for x in scheduled_now if x["type"] == eventtypes.STREAM_OUTPUT_START]
 
-            schedule_ids = {x["row_id"] for x in scheduled_now_files]}
+            schedule_ids = [x["row_id"] for x in scheduled_now_files]
 
             row_id_map = {}
             liq_queue_ids = set()
