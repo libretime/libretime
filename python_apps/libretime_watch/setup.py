fe785b148bd0f059bf3ac53cccd5a4ab61326d40
Keoni Mahelona <keoni@kmahelona.net>
2020-03-11 15:29:49 +1300

    Proper version

--- python_apps/libretime_watch/setup.py
+++ python_apps/libretime_watch/setup.py
@@ -41,7 +41,7 @@ setup(name='libretime_watch',
         ]
       },
       install_requires=[
-          'mutagen~=1.4.3',
+          'mutagen~=1.43.0',
           'pika',
           'psycopg2-binary', # database
           'jason',
