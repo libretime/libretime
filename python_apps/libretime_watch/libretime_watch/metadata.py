d1e993a9bfdb992ad79eb1b9c7d1748a8f44b8f7
Keoni Mahelona <keoni@kmahelona.net>
2020-03-13 13:49:10 +1300

    Check if valid bpm from tag if not then calculate

--- python_apps/libretime_watch/libretime_watch/metadata.py
+++ python_apps/libretime_watch/libretime_watch/metadata.py
@@ -230,6 +230,8 @@ def analyse_file (filename, database):
     # Get BPM
     try:
         bpm = float(audio['bpm'][0])
+        if not bpm:
+            raise KeyError
     except KeyError as e:
         try:
             # Attempt to calculate BPM
@@ -247,7 +249,8 @@ def analyse_file (filename, database):
         database["bpm"] = int(bpm)
         logging.info("BPM: {0}".format(bpm))
     else:
-        del database["bpm"]
+        if "bpm" in database:
+            del database["bpm"]
 
     database["bit_rate"] = f.info.bitrate
     database["sample_rate"] = f.info.sample_rate
