1c5ead9468501d47b2da4199b6446ae028532b65
farggus <61951994+farggus@users.noreply.github.com>
2020-03-24 02:53:39 +0200

    Updated for translate

--- airtime_mvc/application/configs/navigation.php
+++ airtime_mvc/application/configs/navigation.php
@@ -185,7 +185,7 @@ $pages[] = array(
             'target'    => "_blank"
         ),
         array(
-            'label'      => _(sprintf("Help Translate %s", PRODUCT_NAME)),
+            'label'      => _('Help Translate Libretime'),
             'uri'        => AIRTIME_TRANSIFEX_URL,
             'target'     => "_blank"
         ),
