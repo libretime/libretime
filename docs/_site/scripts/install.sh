3a54f8548e9ea3d2aab8801d2e4482a2670ebc43
Zachary Klosko <kloskoz@vcu.edu>
2020-05-22 20:49:41 -0400

    Removing cache uploaded by mistake

--- docs/_site/scripts/install.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-echo "Updating Apt."
-apt-get update > /dev/null
-echo "Ensuring Pip is installed."
-DEBIAN_FRONTEND=noninteractive apt-get install -y -qq python3-pip > /dev/null
-echo "Ensuring Mkdocs is installed."
-pip3 install mkdocs
