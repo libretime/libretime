73a1c77517b3fc29d2e8f7b69aa423560a973c8f
Lucas Bickel <hairmare@purplehaze.ch>
2020-12-01 19:51:38 +0100

    feat(ui): yarn add i18n

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
+  }
 }
