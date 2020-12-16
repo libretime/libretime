09dc9f53f34adf0901f939a6949685e2576efe98
Zachary Klosko <kloskoz@vcu.edu>
2020-12-16 14:53:22 -0500

    Trying @pashagolub's suggestion

--- .github/scripts/init-postgres.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-psql -c 'CREATE DATABASE libretime;' -U postgres
-psql -c "CREATE USER libretime WITH PASSWORD 'libretime';" -U postgres
-psql -c 'GRANT CONNECT ON DATABASE libretime TO libretime;' -U postgres
-psql -c 'ALTER USER libretime CREATEDB;' -U postgres
