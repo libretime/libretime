42328b927797e09babe53989df954b06c58e0343
Lucas Bickel <hairmare@rabe.ch>
2020-12-24 23:18:15 +0100

    feat(pypo): support liquidsoap 1.4

--- python_apps/pypo/liquidsoap/__main__.py
+++ python_apps/pypo/liquidsoap/__main__.py
@@ -22,12 +22,9 @@ def run():
         logging.basicConfig(level=getattr(logging, 'DEBUG', None))
 
     generate_liquidsoap_cfg.run()
-    ''' check liquidsoap version if less than 1.3 use legacy liquidsoap script '''
-    liquidsoap_version = subprocess.check_output("liquidsoap --version", shell=True, universal_newlines=True)
-    if "1.1.1" not in liquidsoap_version:
-        script_path = os.path.join(os.path.dirname(__file__), 'ls_script.liq')
-    else:
-        script_path = os.path.join(os.path.dirname(__file__), 'ls_script_legacy.liq')
+    ''' check liquidsoap version so we can run a scripts matching the liquidsoap minor version '''
+    liquidsoap_version = subprocess.check_output("liquidsoap --version", shell=True, universal_newlines=True)[0:3]
+    script_path = os.path.join(os.path.dirname(__file__), liquidsoap_version, 'ls_script.py')
     if args.debug:
         os.execl('/usr/bin/liquidsoap', 'airtime-liquidsoap', script_path, '--verbose', '-f', '--debug')
     else:
