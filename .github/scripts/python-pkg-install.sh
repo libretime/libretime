b6d83d9b9b437cd19d2bfc251b4d8b80b3474849
Kyle Robbertze <kyle@paddatrapper.com>
2021-05-21 16:49:40 +0200

    install for all python apps

--- .github/scripts/python-pkg-install.sh
+++ .github/scripts/python-pkg-install.sh
@@ -2,8 +2,7 @@
 echo "::group::Install Python apps"
 pip3 install nose mock
 
-pip3 install -e python_apps/airtime_analyzer/.
-pip3 install -e python_apps/airtime-celery/.
-pip3 install -e python_apps/api_clients/.
-pip3 install -e python_apps/pypo/.
+for app in `ls python_apps`; do
+  pip3 install -e python_apps/$app
+done
 echo "::endgroup::"
