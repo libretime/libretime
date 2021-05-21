6193d80e2ad2d4b22172a2ba7ccec0823716850f
Kyle Robbertze <kyle@paddatrapper.com>
2021-05-21 16:22:50 +0200

    group install steps

--- .github/scripts/python-pkg-install.sh
+++ .github/scripts/python-pkg-install.sh
@@ -1,7 +1,9 @@
 #/bin/bash
+echo "::group::Install Python apps"
 pip3 install nose mock
 
 pip3 install -e python_apps/airtime_analyzer/.
 pip3 install -e python_apps/airtime-celery/.
 pip3 install -e python_apps/api_clients/.
 pip3 install -e python_apps/pypo/.
+echo "::endgroup::"
