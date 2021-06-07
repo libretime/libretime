ed3d527c09d17111365215d956e05c976ab6afb9
jo <ljonas@riseup.net>
2021-06-07 23:58:16 +0200

    Update CI to run pytest

--- .github/scripts/python-pkg-install.sh
+++ .github/scripts/python-pkg-install.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 echo "::group::Install Python apps"
-pip3 install nose mock
-
 for app in $(ls python_apps); do
   if [[ -f "python_apps/$app/requirements-dev.txt" ]]; then
     pip3 install -r "python_apps/$app/requirements-dev.txt"
   fi
-  pip3 install -e python_apps/$app
+  pip3 install -e "python_apps/$app"
 done
 echo "::endgroup::"
