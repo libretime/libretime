f944eca0a75cb05484ba03a58dac1a269ae28a18
jo <ljonas@riseup.net>
2021-06-08 00:08:32 +0200

    Migrate api_clients tests to pytest

--- python_apps/api_clients/tests/apirequest_test.py
+++ python_apps/api_clients/tests/apirequest_test.py
@@ -1,46 +1,33 @@
-import json
-import unittest
-
 from api_clients.utils import ApcUrl, ApiRequest
 from mock import MagicMock, patch
 
 
-class ResponseInfo:
-    @property
-    def headers(self):
-        return {"content-type": "application/json"}
-
-    def json(self):
-        return {"ok", "ok"}
+def test_api_request_init():
+    u = ApiRequest("request_name", ApcUrl("/test/ing"))
+    assert u.name == "request_name"
 
 
-class TestApiRequest(unittest.TestCase):
-    def test_init(self):
-        u = ApiRequest("request_name", ApcUrl("/test/ing"))
-        self.assertEqual(u.name, "request_name")
+def test_api_request_call_json():
+    return_value = {"ok": "ok"}
 
-    def test_call_json(self):
-        ret = {"ok": "ok"}
-        read = MagicMock()
-        read.headers = {"content-type": "application/json"}
-        read.json = MagicMock(return_value=ret)
-        u = "http://localhost/testing"
-        with patch("requests.get") as mock_method:
-            mock_method.return_value = read
-            request = ApiRequest("mm", ApcUrl(u))()
-            self.assertEqual(request, ret)
+    read = MagicMock()
+    read.headers = {"content-type": "application/json"}
+    read.json = MagicMock(return_value=return_value)
 
-    def test_call_html(self):
-        ret = "<html><head></head><body></body></html>"
-        read = MagicMock()
-        read.headers = {"content-type": "application/html"}
-        read.text = MagicMock(return_value=ret)
-        u = "http://localhost/testing"
-        with patch("requests.get") as mock_method:
-            mock_method.return_value = read
-            request = ApiRequest("mm", ApcUrl(u))()
-            self.assertEqual(request.text(), ret)
+    with patch("requests.get") as mock_method:
+        mock_method.return_value = read
+        request = ApiRequest("mm", ApcUrl("http://localhost/testing"))()
+        assert request == return_value
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_api_request_call_html():
+    return_value = "<html><head></head><body></body></html>"
+
+    read = MagicMock()
+    read.headers = {"content-type": "application/html"}
+    read.text = MagicMock(return_value=return_value)
+
+    with patch("requests.get") as mock_method:
+        mock_method.return_value = read
+        request = ApiRequest("mm", ApcUrl("http://localhost/testing"))()
+        assert request.text() == return_value
--- python_apps/api_clients/tests/requestprovider_test.py
+++ python_apps/api_clients/tests/requestprovider_test.py
@@ -1,39 +1,33 @@
-import json
-import unittest
-
+import pytest
 from api_clients.utils import RequestProvider
 from api_clients.version1 import api_config
-from configobj import ConfigObj
-from mock import MagicMock, patch
 
 
-class TestRequestProvider(unittest.TestCase):
-    def setUp(self):
-        self.cfg = api_config
-        self.cfg["general"] = {}
-        self.cfg["general"]["base_dir"] = "/test"
-        self.cfg["general"]["base_port"] = 80
-        self.cfg["general"]["base_url"] = "localhost"
-        self.cfg["general"]["api_key"] = "TEST_KEY"
-        self.cfg["api_base"] = "api"
-
-    def test_test(self):
-        self.assertTrue("general" in self.cfg)
-
-    def test_init(self):
-        rp = RequestProvider(self.cfg, {})
-        self.assertEqual(len(rp.available_requests()), 0)
-
-    def test_contains(self):
-        methods = {
-            "upload_recorded": "/1/",
-            "update_media_url": "/2/",
-            "list_all_db_files": "/3/",
-        }
-        rp = RequestProvider(self.cfg, methods)
-        for meth in methods:
-            self.assertTrue(meth in rp.requests)
+@pytest.fixture()
+def config():
+    return {
+        **api_config,
+        "general": {
+            "base_dir": "/test",
+            "base_port": 80,
+            "base_url": "localhost",
+            "api_key": "TEST_KEY",
+        },
+        "api_base": "api",
+    }
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_request_provider_init(config):
+    request_provider = RequestProvider(config, {})
+    assert len(request_provider.available_requests()) == 0
+
+
+def test_request_provider_contains(config):
+    endpoints = {
+        "upload_recorded": "/1/",
+        "update_media_url": "/2/",
+        "list_all_db_files": "/3/",
+    }
+    request_provider = RequestProvider(config, endpoints)
+    for endpoint in endpoints:
+        assert endpoint in request_provider.requests
