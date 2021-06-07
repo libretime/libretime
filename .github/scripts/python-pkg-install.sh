8d9d1e43c5d204bd83dc21c34f6fc6ac354df4d5
jo <ljonas@riseup.net>
2021-06-08 00:12:14 +0200

    Update CI to run api_clients pytest

--- .github/scripts/python-pkg-install.sh
+++ .github/scripts/python-pkg-install.sh
@@ -4,6 +4,9 @@ echo "::group::Install Python apps"
 pip3 install nose mock
 
 for app in $(ls python_apps); do
+  if [[ -f "python_apps/$app/requirements-dev.txt" ]]; then
+    pip3 install -r "python_apps/$app/requirements-dev.txt"
+  fi
   pip3 install -e python_apps/$app
 done
 echo "::endgroup::"
