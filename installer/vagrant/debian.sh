8ce437828e1753edfb50d7f22378755e39b393f1
jo <ljonas@riseup.net>
2021-08-16 23:49:15 +0200

    Remove install apt-get dangerous options
    Apt should fail fast (and report) instead of
    forcing a broken installation.
    
    The version check is also not required anymore as
    most distribution now provide an apt version > 1.1.

--- installer/vagrant/debian.sh
+++ installer/vagrant/debian.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DEBIAN_FRONTEND=noninteractive apt-get -y -m --force-yes install alsa-utils
+DEBIAN_FRONTEND=noninteractive apt-get -y install alsa-utils
 usermod -a -G audio vagrant
 usermod -a -G audio www-data
