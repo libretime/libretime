ceb40ee216381e9cb515e3907808b7a2e854ba62
jo <ljonas@riseup.net>
2021-09-10 13:46:47 +0200

    Use shared python makefile and start linting
    Always update setuptools/wheel/pip
    
    Never fail while linting
    
    We use github annotations to impove the code incrementally.

---
+++ tools/python.mk
@@ -0,0 +1,29 @@
+.ONESHELL:
+
+SHELL := bash
+CPU_CORES := $(shell nproc)
+
+# PIP_INSTALL := --editable .[dev]
+# PYLINT_ARG :=
+# PYTEST_ARG :=
+
+VENV := venv
+$(VENV):
+	python3 -m venv $(VENV)
+	source $(VENV)/bin/activate
+	pip install --upgrade pip setuptools wheel
+	pip install $(PIP_INSTALL)
+
+.PHONY: .pylint
+.pylint: $(VENV)
+	source $(VENV)/bin/activate
+	pylint --output-format=colorized $(PYLINT_ARG) || true
+
+.PHONY: .pytest
+.pytest: $(VENV)
+	source venv/bin/activate
+	pytest -n $(CPU_CORES) --color=yes -v $(PYTEST_ARG)
+
+.PHONY: .clean
+.clean:
+	rm -Rf $(VENV)
