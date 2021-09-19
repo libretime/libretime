f84e40dc06943f8ce7e32b2eb0ab97e58a9c1bde
jo <ljonas@riseup.net>
2021-09-19 22:38:49 +0200

    Small refactoring

--- api/libretimeapi/tests/models/test_schedule.py
+++ api/libretimeapi/tests/models/test_schedule.py
@@ -16,47 +16,29 @@ class TestSchedule(SimpleTestCase):
         cue_in = timedelta(seconds=1)
         cue_out = length - timedelta(seconds=4)
 
+        def create_schedule(starts):
+            return Schedule(
+                starts=starts,
+                ends=starts + length,
+                cue_in=cue_in,
+                cue_out=cue_out,
+                instance=show_instance,
+            )
+
         # No overlapping schedule datetimes, normal usecase:
         s1_starts = datetime(year=2021, month=10, day=2, hour=1, minute=30)
-        s1_ends = s1_starts + length
-
-        s1 = Schedule(
-            starts=s1_starts,
-            ends=s1_ends,
-            cue_in=cue_in,
-            cue_out=cue_out,
-            instance=show_instance,
-        )
-
+        s1 = create_schedule(s1_starts)
         self.assertEqual(s1.get_cueout(), cue_out)
-        self.assertEqual(s1.get_ends(), s1_ends)
+        self.assertEqual(s1.get_ends(), s1_starts + length)
 
         # Mixed overlapping schedule datetimes (only ends is overlapping):
         s2_starts = datetime(year=2021, month=10, day=2, hour=1, minute=55)
-        s2_ends = s2_starts + length
-
-        s2 = Schedule(
-            starts=s2_starts,
-            ends=s2_ends,
-            cue_in=cue_in,
-            cue_out=cue_out,
-            instance=show_instance,
-        )
-
+        s2 = create_schedule(s2_starts)
         self.assertEqual(s2.get_cueout(), timedelta(minutes=5))
         self.assertEqual(s2.get_ends(), show_instance.ends)
 
         # Fully overlapping schedule datetimes (starts and ends are overlapping):
         s3_starts = datetime(year=2021, month=10, day=2, hour=2, minute=1)
-        s3_ends = s3_starts + length
-
-        s3 = Schedule(
-            starts=s3_starts,
-            ends=s3_ends,
-            cue_in=cue_in,
-            cue_out=cue_out,
-            instance=show_instance,
-        )
-
+        s3 = create_schedule(s3_starts)
         self.assertEqual(s3.get_cueout(), cue_out)
         self.assertEqual(s3.get_ends(), show_instance.ends)
