1a765d3d66e681049b2388a5f3e0ca9b2fe57fa5
jo <ljonas@riseup.net>
2021-10-11 16:50:57 +0200

    Reenable escape_implicit_backslashes

--- legacy/.php-cs-fixer.php
+++ legacy/.php-cs-fixer.php
@@ -9,7 +9,6 @@ $config = new PhpCsFixer\Config();
 return $config->setRules([
     '@PhpCsFixer' => true,
     'concat_space' => ['spacing' => 'one'],
-    'escape_implicit_backslashes' => false,
     'ordered_class_elements' => false,
     'yoda_style' => false,
 ])
