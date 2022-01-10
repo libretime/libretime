2643d8d3a09a63a9798efa3fd79ad9a31abf4b72
jo <ljonas@riseup.net>
2022-01-10 06:33:51 +0100

    feat(worker): make celery compatible with v5

--- worker/libretime_worker/config.py
+++ worker/libretime_worker/config.py
@@ -12,21 +12,19 @@ LIBRETIME_CONFIG_FILEPATH = getenv("LIBRETIME_CONFIG_FILEPATH")
 
 config = Config(filepath=LIBRETIME_CONFIG_FILEPATH)
 
-# Celery amqp settings
-BROKER_URL = config.rabbitmq.url
-CELERY_RESULT_BACKEND = "amqp"  # Use RabbitMQ as the celery backend
-CELERY_RESULT_PERSISTENT = True  # Persist through a broker restart
-CELERY_TASK_RESULT_EXPIRES = 900  # Expire task results after 15 minutes
-CELERY_RESULT_EXCHANGE = "celeryresults"  # Default exchange - needed due to php-celery
-CELERY_QUEUES = (
+# Celery settings
+# See https://docs.celeryproject.org/en/stable/userguide/configuration.html
+broker_url = config.rabbitmq.url
+worker_concurrency = 1
+event_queue_expires = 900
+
+result_backend = "rpc://"
+result_persistent = True
+result_expires = 900
+result_exchange = "celeryresults"  # needed due to php-celery
+
+task_time_limit = 1800
+task_queues = (
     Queue("podcast", exchange=Exchange("podcast"), routing_key="podcast"),
     Queue(exchange=Exchange("celeryresults"), auto_delete=True),
 )
-CELERY_EVENT_QUEUE_EXPIRES = 900  # RabbitMQ x-expire after 15 minutes
-
-# Celery task settings
-CELERY_TASK_SERIALIZER = "json"
-CELERY_RESULT_SERIALIZER = "json"
-CELERY_ACCEPT_CONTENT = ["json"]
-CELERY_TIMEZONE = "Europe/Berlin"
-CELERY_ENABLE_UTC = True
