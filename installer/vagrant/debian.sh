cf7c14ab4046c191055f95110668324b4e122d25
jo <ljonas@riseup.net>
2022-01-13 02:32:16 +0100

    bypass apt SRV redirections for deb.debian.org

--- installer/vagrant/debian.sh
+++ installer/vagrant/debian.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+sed --in-place 's/deb\.debian\.org/cdn-fastly.deb.debian.org/g' /etc/apt/sources.list
+
 DEBIAN_FRONTEND=noninteractive apt-get update --allow-releaseinfo-change
 DEBIAN_FRONTEND=noninteractive apt-get -y install alsa-utils auto-apt-proxy
+
 usermod -a -G audio vagrant
 usermod -a -G audio www-data
