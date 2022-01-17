4b8ff88abb06dd6006a2260e79cc61d217b8f570
jo <ljonas@riseup.net>
2022-01-17 20:35:21 +0100

    refactor(analyzer): rename analyzer files

--- analyzer/tests/analyzer_test.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from libretime_analyzer.analyzer import Analyzer
-
-
-def test_analyze():
-    with pytest.raises(NotImplementedError):
-        abstract_analyzer = Analyzer()
-        abstract_analyzer.analyze(u"foo", dict())
