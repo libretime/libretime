6ffd4009dc6c27abc85f30395cc8c8a829a74225
jo <ljonas@riseup.net>
2022-01-17 05:19:39 +0100

    fix: make vagrant source.list update idempotent

--- installer/vagrant/debian.sh
+++ installer/vagrant/debian.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sed --in-place 's/deb\.debian\.org/cdn-fastly.deb.debian.org/g' /etc/apt/sources.list
+sed --in-place 's|http://deb\.debian\.org|http://cdn-fastly.deb.debian.org|g' /etc/apt/sources.list
 
 DEBIAN_FRONTEND=noninteractive apt-get update --allow-releaseinfo-change
 DEBIAN_FRONTEND=noninteractive apt-get -y install alsa-utils auto-apt-proxy
