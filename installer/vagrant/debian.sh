40bc1791f52bfdc4eca78d1a30fb6185af81a9d8
jo <ljonas@riseup.net>
2022-02-03 12:18:32 +0100

    chore: improve vagrant debian prepare script

--- installer/vagrant/debian.sh
+++ installer/vagrant/debian.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+# Setup apt-cacher-ng proxy
 sed --in-place 's|http://deb\.debian\.org|http://cdn-fastly.deb.debian.org|g' /etc/apt/sources.list
-
 DEBIAN_FRONTEND=noninteractive apt-get update --allow-releaseinfo-change
-DEBIAN_FRONTEND=noninteractive apt-get -y install alsa-utils auto-apt-proxy
+DEBIAN_FRONTEND=noninteractive apt-get -y -qq install auto-apt-proxy
 
+# Install alsa utils
+DEBIAN_FRONTEND=noninteractive apt-get -y -qq install alsa-utils
 usermod -a -G audio vagrant
 usermod -a -G audio www-data
