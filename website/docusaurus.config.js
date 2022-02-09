55fd3456da27a753fa36cfa445bcfbcb5b0521f1
jo <ljonas@riseup.net>
2022-02-09 08:01:07 +0100

    docs: replace jekyll with docusaurus
    - jsnon5 not handled by pre-commit
    
    Co-authored-by: Zachary Klosko <zklosko@users.noreply.github.com>

---
+++ website/docusaurus.config.js
@@ -0,0 +1,108 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Libretime",
+  tagline: "Broadcast without limits",
+  url: "https://libretime.org",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "throw",
+  favicon: "img/favicon.ico",
+  organizationName: "libretime",
+  projectName: "libretime",
+  trailingSlash: true,
+
+  plugins: [
+    [
+      require.resolve("@cmfcmf/docusaurus-search-local"),
+      {
+        indexBlog: false,
+        indexPages: false,
+      },
+    ],
+  ],
+
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          path: "../docs",
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/libretime/libretime",
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Libretime",
+        logo: {
+          alt: "Libretime tower",
+          src: "img/logo.svg",
+        },
+        items: [
+          { to: "/contribute", label: "Contribute", position: "left" },
+          { type: "localeDropdown", position: "right" },
+          {
+            href: "https://github.com/libretime/libretime",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Discourse",
+                href: "https://discourse.libretime.org",
+              },
+              {
+                label: "Mattermost",
+                href: "https://chat.libretime.org",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/libretime/libretime",
+              },
+            ],
+          },
+        ],
+        copyright: `Code licensed under AGPLv3; docs licensed under GPLv2.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ["apacheconf", "ini"],
+      },
+    }),
+};
+
+module.exports = config;
---
+++ website/src/pages/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./index.module.css";
+
+import HomepageFeatures from "../components/HomepageFeatures";
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className={clsx(
+              "button button--secondary button--lg",
+              styles.buttonPadding
+            )}
+            to="/docs/intro"
+          >
+            Get started 🚀
+          </Link>
+          <Link
+            className={clsx(
+              "button button--secondary button--lg",
+              styles.buttonPadding
+            )}
+            to="/release-notes"
+          >
+            Release Notes
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout title={siteConfig.title} description={siteConfig.tagline}>
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
