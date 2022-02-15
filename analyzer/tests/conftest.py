7c354f52f48e681c93b15f829ba4f799c70cc47b
jo <ljonas@riseup.net>
2022-02-15 10:33:17 +0100

    test(analyzer): set test logging level to trace

--- analyzer/tests/conftest.py
+++ analyzer/tests/conftest.py
@@ -1,10 +1,12 @@
-import os
 import shutil
 from tempfile import TemporaryDirectory
 
 import pytest
+from libretime_shared.logging import TRACE, setup_logger
 
 from .fixtures import fixtures_path
+
+setup_logger(TRACE)
 
 AUDIO_FILENAME = "s1-stereo-tagged.mp3"
 AUDIO_FILE = fixtures_path / AUDIO_FILENAME
