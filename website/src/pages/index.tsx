2b62fe8e69fd47aa54c110cb36d20646824a6302
jo <ljonas@riseup.net>
2022-02-18 21:36:30 +0100

    update import prefix for vars file

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -3,9 +3,9 @@ import clsx from "clsx";
 import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import styles from "./index.module.css";
+import vars from "@site/vars";
 
-import vars from "../../vars";
+import styles from "./index.module.css";
 import HomepageFeatures from "../components/HomepageFeatures";
 
 function HomepageHeader() {
