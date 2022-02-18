dd7d3a152063a5d89c78d3160551dea95238bef1
jo <ljonas@riseup.net>
2022-02-18 12:29:39 +0100

    tmp: update deploy url

--- website/vars.js
+++ website/vars.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "LibreTime",
   description: "Radio Broadcast & Automation Platform",
   version: "3.0.0-alpha.10",
-  website: "https://jooola.github.io/libretime/",
+  website: "https://jooola.github.io",
 
   repository: {
     href: "https://github.com/libretime/libretime",
