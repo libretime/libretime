0d169608878744e1b410891d74e516505120b519
jo <ljonas@riseup.net>
2022-04-29 14:43:45 +0200

    feat: remove php web installer

--- legacy/public/install/clearstatcache.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/* The purpose of this file is get PHP to clear its cache regarding the
- * filesystem layout. See this ticket http://dev.sourcefabric.org/browse/CC-3320 */
-
-clearstatcache(true);
