ae4ef81ac6343017e8f516eb335766f710739f7c
jo <ljonas@riseup.net>
2022-06-19 15:58:45 +0200

    style(legacy): format CcFiles.php

--- legacy/application/models/airtime/CcFiles.php
+++ legacy/application/models/airtime/CcFiles.php
@@ -484,10 +484,10 @@ class CcFiles extends BaseCcFiles
         // reject overly long 2 byte sequences, as well as characters above U+10000 and replace with ?
         $string = preg_replace(
             '/[\x00-\x08\x10\x0B\x0C\x0E-\x19\x7F]' .
-            '|[\x00-\x7F][\x80-\xBF]+' .
-            '|([\xC0\xC1]|[\xF0-\xFF])[\x80-\xBF]*' .
-            '|[\xC2-\xDF]((?![\x80-\xBF])|[\x80-\xBF]{2,})' .
-            '|[\xE0-\xEF](([\x80-\xBF](?![\x80-\xBF]))|(?![\x80-\xBF]{2})|[\x80-\xBF]{3,})/S',
+                '|[\x00-\x7F][\x80-\xBF]+' .
+                '|([\xC0\xC1]|[\xF0-\xFF])[\x80-\xBF]*' .
+                '|[\xC2-\xDF]((?![\x80-\xBF])|[\x80-\xBF]{2,})' .
+                '|[\xE0-\xEF](([\x80-\xBF](?![\x80-\xBF]))|(?![\x80-\xBF]{2})|[\x80-\xBF]{3,})/S',
             '?',
             $string
         );
