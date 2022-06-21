0326c51f589a839dccdac4859a15cb229e684141
jo <ljonas@riseup.net>
2022-06-21 14:42:04 +0200

    chore(api): update Preference model

--- api/libretime_api/core/models/preference.py
+++ api/libretime_api/core/models/preference.py
@@ -2,16 +2,30 @@ from django.db import models
 
 
 class Preference(models.Model):
-    subjid = models.ForeignKey(
-        "User", models.DO_NOTHING, db_column="subjid", blank=True, null=True
+    user = models.ForeignKey(
+        "User",
+        on_delete=models.CASCADE,
+        db_column="subjid",
+        blank=True,
+        null=True,
     )
-    keystr = models.CharField(unique=True, max_length=255, blank=True, null=True)
-    valstr = models.TextField(blank=True, null=True)
+    key = models.CharField(
+        db_column="keystr",
+        max_length=255,
+        unique=True,
+        blank=True,
+        null=True,
+    )
+    value = models.TextField(
+        db_column="valstr",
+        blank=True,
+        null=True,
+    )
 
     class Meta:
         managed = False
         db_table = "cc_pref"
-        unique_together = (("subjid", "keystr"),)
+        unique_together = (("user", "key"),)
 
 
 class StreamSetting(models.Model):
