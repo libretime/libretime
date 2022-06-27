654314cb1b063d638f7164f1ebfe3e3e75905939
jo <ljonas@riseup.net>
2022-06-27 17:09:17 +0200

    chore(api): rename country model fields

--- api/libretime_api/core/models/country.py
+++ api/libretime_api/core/models/country.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Country(models.Model):
-    isocode = models.CharField(primary_key=True, max_length=3)
+    iso_code = models.CharField(primary_key=True, max_length=3, db_column="isocode")
     name = models.CharField(max_length=255)
 
     class Meta:
