7bcb2f74d2fa95948341ffc9bd88c21d314ea415
jo <ljonas@riseup.net>
2022-07-25 01:39:58 +0200

    chore(analyzer): install missing requests types

--- analyzer/setup.py
+++ analyzer/setup.py
@@ -38,6 +38,7 @@ setup(
     extras_require={
         "dev": [
             "distro",
+            "types-requests",
             f"libretime-shared @ file://localhost{here.parent / 'shared'}",
         ],
     },
