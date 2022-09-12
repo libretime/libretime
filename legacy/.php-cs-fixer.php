59f900b16cd4e1b3a2c41814dc989a4fb22997e9
jo <ljonas@riseup.net>
2022-09-12 14:09:01 +0200

    chore(legacy): set format rules based on version

--- legacy/.php-cs-fixer.php
+++ legacy/.php-cs-fixer.php
@@ -6,12 +6,24 @@ $finder = PhpCsFixer\Finder::create()
     ->exclude('application/models/airtime/map')
     ->exclude('application/models/airtime/om');
 
+
+$extra_rules = [
+    'phpdoc_order' => false,
+];
+
+if (version_compare(PhpCsFixer\Console\Application::VERSION, '3.11.0') >= 0) {
+    $extra_rules['phpdoc_order'] = ['order' => ['param', 'return', 'throws']];
+}
+
 $config = new PhpCsFixer\Config();
-return $config->setRules([
-    '@PhpCsFixer' => true,
-    'concat_space' => ['spacing' => 'one'],
-    'multiline_whitespace_before_semicolons' => ['strategy' => 'no_multi_line'],
-    'ordered_class_elements' => false,
-    'yoda_style' => false,
-])
+return $config->setRules(array_merge(
+    [
+        '@PhpCsFixer' => true,
+        'concat_space' => ['spacing' => 'one'],
+        'multiline_whitespace_before_semicolons' => ['strategy' => 'no_multi_line'],
+        'ordered_class_elements' => false,
+        'yoda_style' => false,
+    ],
+    $extra_rules
+))
     ->setFinder($finder);
