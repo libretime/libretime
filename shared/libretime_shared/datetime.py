d29d837d01a2770f1d4a1595f648495d5e7b20ec
jo <ljonas@riseup.net>
2022-10-10 18:39:40 +0200

    feat: drop Python 3.6 support

--- shared/libretime_shared/datetime.py
+++ shared/libretime_shared/datetime.py
@@ -1,4 +1,4 @@
-from datetime import datetime, time
+from datetime import time
 
 
 def time_in_seconds(value: time) -> float:
@@ -12,16 +12,3 @@ def time_in_seconds(value: time) -> float:
 
 def time_in_milliseconds(value: time) -> float:
     return time_in_seconds(value) * 1000
-
-
-def time_fromisoformat(value: str) -> time:
-    """
-    This is required for Python 3.6 support. datetime.time.fromisoformat was
-    only added in Python 3.7. Until LibreTime drops Python 3.6 support, this
-    wrapper uses the old way of doing it.
-    """
-    try:
-        obj = datetime.strptime(value, "%H:%M:%S.%f")
-    except ValueError:
-        obj = datetime.strptime(value, "%H:%M:%S")
-    return obj.time()
