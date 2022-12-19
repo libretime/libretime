86ceac76d8e1c3da7eff186daa2b1bd76551bc26
jo <ljonas@riseup.net>
2022-12-19 14:26:34 +0100

    chore: release 3.0.1

--- api-client/setup.py
+++ api-client/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="libretime-api-client",
-    version="3.0.0",
+    version="3.0.1",
     description="LibreTime API Client",
     author="LibreTime Contributors",
     url="https://github.com/libretime/libretime",
