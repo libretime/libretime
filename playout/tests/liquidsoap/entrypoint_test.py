d3ac058a19707e646d7a35a19e3256dee7bf2d43
jo <ljonas@riseup.net>
2023-02-25 18:52:22 +0100

    test(playout): more entrypoint config test cases

--- playout/tests/liquidsoap/entrypoint_test.py
+++ playout/tests/liquidsoap/entrypoint_test.py
@@ -7,12 +7,18 @@ from libretime_playout.config import Config
 from libretime_playout.liquidsoap.entrypoint import generate_entrypoint
 from libretime_playout.liquidsoap.models import Info, StreamPreferences
 
+from .fixtures import TEST_STREAM_CONFIGS
+
 
 @pytest.mark.parametrize(
     "version",
     [pytest.param((1, 4, 4), id="1.4")],
 )
-def test_generate_entrypoint(tmp_path: Path, config: Config, version, snapshot):
+@pytest.mark.parametrize(
+    "stream_config",
+    TEST_STREAM_CONFIGS,
+)
+def test_generate_entrypoint(tmp_path: Path, stream_config: Config, version, snapshot):
     entrypoint_filepath = tmp_path / "radio.liq"
 
     with mock.patch(
@@ -22,7 +28,7 @@ def test_generate_entrypoint(tmp_path: Path, config: Config, version, snapshot):
         generate_entrypoint(
             entrypoint_filepath,
             log_filepath=Path("/var/log/radio.log"),
-            config=config,
+            config=stream_config,
             preferences=StreamPreferences(
                 input_fade_transition=0.0,
                 message_format=0,
