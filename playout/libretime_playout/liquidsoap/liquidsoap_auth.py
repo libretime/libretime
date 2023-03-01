4a14b6b01fa4d10b53aa2d75c7fd8bd9ce9dc645
jo <ljonas@riseup.net>
2023-03-01 16:35:08 +0100

    refactor(playout): update liquidsoap input auth handler

--- playout/libretime_playout/liquidsoap/liquidsoap_auth.py
+++ playout/libretime_playout/liquidsoap/liquidsoap_auth.py
@@ -1,24 +1,36 @@
-import sys
+from argparse import ArgumentParser
+from typing import Literal
 
 from libretime_api_client.v1 import ApiClient as LegacyClient
 
-legacy_client = LegacyClient()
 
-dj_type = sys.argv[1]
-username = sys.argv[2]
-password = sys.argv[3]
+def main(input_name: Literal["main", "show"], username: str, password: str) -> int:
+    legacy_client = LegacyClient()
 
-source_type = ""
-if dj_type == "--master":
-    source_type = "master"
-elif dj_type == "--dj":
-    source_type = "dj"
+    input_name_map = {"main": "master", "show": "dj"}
+    response: dict = legacy_client.check_live_stream_auth(
+        username,
+        password,
+        input_name_map[input_name],
+    )
 
-response = legacy_client.check_live_stream_auth(username, password, source_type)
+    if response.get("msg", False) is True:
+        return 0
+    return 1
 
-if "msg" in response and response["msg"] == True:
-    print(response["msg"])
-    sys.exit(0)
-else:
-    print(False)
-    sys.exit(1)
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+
+    parser.add_argument("input_name", choices=["main", "show"])
+    parser.add_argument("username")
+    parser.add_argument("password")
+    args = parser.parse_args()
+
+    raise SystemExit(
+        main(
+            input_name=args.input_name,
+            username=args.username,
+            password=args.password,
+        )
+    )
