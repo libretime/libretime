6c493bac5bbb23ad4c3a5bf07bc2810ff8fd4b5d
jo <ljonas@riseup.net>
2023-03-01 20:58:28 +0100

    chore: move mime type utils

--- playout/libretime_playout/utils.py
+++ playout/libretime_playout/utils.py
@@ -1,4 +1,11 @@
+import logging
+import mimetypes
 from datetime import datetime
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+here = Path(__file__).parent
 
 
 def seconds_between(base: datetime, target: datetime) -> float:
@@ -8,3 +15,14 @@ def seconds_between(base: datetime, target: datetime) -> float:
     Return 0 if target is older than base.
     """
     return max(0, (target - base).total_seconds())
+
+
+mimetypes.init([str(here / "mime.types")])
+
+
+def mime_guess_extension(mime: str) -> str:
+    extension = mimetypes.guess_extension(mime, strict=False)
+    if extension is None:
+        logger.warning("could not determine file extension from mime: %s", mime)
+        return ""
+    return extension
