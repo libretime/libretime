c826a4dea876acabceef56fddc326a300456ab97
Marvin <marvin@weiler.rocks>
2023-03-06 14:35:55 +0100

    Copied the login form from legacy

--- api/libretime_api/authentication/urls.py
+++ api/libretime_api/authentication/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login", views.login_view),
-    path("logout", views.logout_view),
+    path("login", views.login_view, name="authentication.login"),
+    path("logout", views.logout_view, name="authentication.logout"),
 ]
