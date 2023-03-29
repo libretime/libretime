028eafb1b6c34640df4202e4dc46f57915238eff
jo <ljonas@riseup.net>
2023-03-29 16:45:13 +0200

    test(playout): liquidsoap wait for version

--- playout/tests/liquidsoap/client/client_test.py
+++ playout/tests/liquidsoap/client/client_test.py
@@ -1,4 +1,6 @@
-from libretime_playout.liquidsoap.client import LiquidsoapClient
+import pytest
+
+from libretime_playout.liquidsoap.client import LiquidsoapClient, LiquidsoapClientError
 
 
 def test_liq_client():
@@ -7,3 +9,16 @@ def test_liq_client():
         port=1234,
         timeout=15,
     )
+
+
+def test_liq_client_wait_for_version(liq_client: LiquidsoapClient):
+    assert liq_client.wait_for_version()
+
+
+def test_liq_client_wait_for_version_invalid_host():
+    liq_client = LiquidsoapClient(
+        host="invalid",
+        port=1234,
+    )
+    with pytest.raises(LiquidsoapClientError):
+        liq_client.wait_for_version(timeout=1)
