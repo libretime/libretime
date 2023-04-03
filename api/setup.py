b474eb3f6461f136a548a2e9c39efa50e72673b7
jo <ljonas@riseup.net>
2023-04-03 12:51:00 +0200

    fix(api): require django >=4.2.0,<4.3

--- api/setup.py
+++ api/setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         "django-cors-headers>=3.14.0,<3.15",
         "django-filter>=2.4.0,<23.2",
-        "django>=4.1.4,<4.3",
+        "django>=4.2.0,<4.3",
         "djangorestframework>=3.12.1,<3.15",
         "drf-spectacular>=0.22.1,<0.27",
         "requests>=2.25.1,<2.29",
