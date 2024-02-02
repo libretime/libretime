170d09545e4fcfeeb95f9fc5c355329764501854
Thomas Göttgens <tgoettgens@gmail.com>
2024-02-02 19:04:12 +0100

    feat(legacy): disable public radio page and redirect to login (#2903)
    ### Description
    
    Many people don't need the public page and use libretime purely for
    playout management. This adds the ability to have libretime publicly
    available but only present the login page to the user.
    
    **I have updated the documentation to reflect these changes**:
    
    no, but i will add documentation if this PR is accepted.
    
    ### Testing Notes
    
    **What I did:**
    
    Toggle the new ceckbox on the general settings, log out and back in and
    check behaviour.
    
    note: this may have conflicts with the trim overbooked PR since the
    toggle sits in the same place. If both are accepted this needs to be
    formatted nicely :-)
    
    ---------
    
    Co-authored-by: Thomas Göttgens <tgoettgens@mail.com>
    Co-authored-by: Jonas L. <jooola@users.noreply.github.com>
    Co-authored-by: Kyle Robbertze <paddatrapper@users.noreply.github.com>
    Co-authored-by: jo <ljonas@riseup.net>

--- legacy/application/controllers/PreferenceController.php
+++ legacy/application/controllers/PreferenceController.php
@@ -50,6 +50,7 @@ class PreferenceController extends Zend_Controller_Action
                 Application_Model_Preference::SetDefaultLocale($values['locale']);
                 Application_Model_Preference::SetWeekStartDay($values['weekStartDay']);
                 Application_Model_Preference::setRadioPageDisplayLoginButton($values['radioPageLoginButton']);
+                Application_Model_Preference::setRadioPageDisabled($values['radioPageDisabled']);
                 Application_Model_Preference::SetFeaturePreviewMode($values['featurePreviewMode']);
 
                 $logoUploadElement = $form->getSubForm('preferences_general')->getElement('stationLogo');
