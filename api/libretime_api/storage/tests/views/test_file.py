7b8cecdadd27415419bacc5e54da94e261e0a686
Thomas Göttgens <tgoettgens@gmail.com>
2024-02-20 15:04:34 +0100

    feat(api): implement file deletion - new fork

--- api/libretime_api/storage/tests/views/test_file.py
+++ api/libretime_api/storage/tests/views/test_file.py
@@ -1,3 +1,5 @@
+import os
+
 from django.conf import settings
 from model_bakery import baker
 from rest_framework.test import APITestCase
@@ -33,3 +35,18 @@ class TestFileViewSet(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION=f"Api-Key {self.token}")
         response = self.client.get(path)
         self.assertEqual(response.status_code, 200)
+
+    def test_destroy(self):
+        path = "/api/v2/files/{id}"
+        file = baker.make(
+            "storage.File",
+            mime="audio/mp3",
+            filepath=AUDIO_FILENAME,
+        )
+        path = path.format(id=str(file.pk))
+        self.client.credentials(HTTP_AUTHORIZATION=f"Api-Key {self.token}")
+        response = self.client.delete(path)
+        self.assertEqual(response.status_code, 204)
+        self.assertFalse(
+            os.path.isfile(os.path.join(settings.CONFIG.storage.path, file.filepath))
+        )
