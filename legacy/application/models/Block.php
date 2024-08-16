83dd504025279fb795a0561eca826c92d58f26b3
dakriy <git@persignum.com>
2024-08-16 01:15:34 -0700

    refactor(legacy): In my git stash merge I missed some modifiers... so add them back here.

--- legacy/application/models/Block.php
+++ legacy/application/models/Block.php
@@ -1568,20 +1568,20 @@ SQL;
                         $spCriteriaValue = "{$spCriteriaValue}%";
                     } elseif ($spCriteriaModifier == CriteriaModifier::ENDS_WITH) {
                         $spCriteriaValue = "%{$spCriteriaValue}";
-                    } elseif ($spCriteriaModifier == 'contains' || $spCriteriaModifier == 'does not contain') {
+                    } elseif ($spCriteriaModifier == CriteriaModifier::CONTAINS || $spCriteriaModifier == CriteriaModifier::DOES_NOT_CONTAIN) {
                         $spCriteriaValue = "%{$spCriteriaValue}%";
-                    } elseif ($spCriteriaModifier == 'is in the range') {
+                    } elseif ($spCriteriaModifier == CriteriaModifier::IS_IN_THE_RANGE) {
                         $spCriteriaValue = "{$spCriteria} >= '{$spCriteriaValue}' AND {$spCriteria} <= '{$spCriteriaExtra}'";
-                    } elseif ($spCriteriaModifier == 'before') {
+                    } elseif ($spCriteriaModifier == CriteriaModifier::BEFORE) {
                         // need to pull in the current time and subtract the value or figure out how to make it relative
                         $relativedate = new DateTime($spCriteriaValue);
                         $dt = $relativedate->format(DateTime::ISO8601);
                         $spCriteriaValue = "COALESCE({$spCriteria}, DATE '-infinity') <= '{$dt}'";
-                    } elseif ($spCriteriaModifier == 'after') {
+                    } elseif ($spCriteriaModifier == CriteriaModifier::AFTER) {
                         $relativedate = new DateTime($spCriteriaValue);
                         $dt = $relativedate->format(DateTime::ISO8601);
                         $spCriteriaValue = "COALESCE({$spCriteria}, DATE '-infinity') >= '{$dt}'";
-                    } elseif ($spCriteriaModifier == 'between') {
+                    } elseif ($spCriteriaModifier == CriteriaModifier::BETWEEN) {
                         $fromrelativedate = new DateTime($spCriteriaValue);
                         $fdt = $fromrelativedate->format(DateTime::ISO8601);
 
