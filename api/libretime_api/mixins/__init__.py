df06fd33ff747e04614694559a988ddd577696c9
Thomas Göttgens <tgoettgens@gmail.com>
2024-11-12 22:13:09 +0000

    Enable writes to schedule table

---
+++ api/libretime_api/mixins/__init__.py
@@ -0,0 +1,1 @@
+from .readwriteserializer import ReadWriteSerializerMixin
---
+++ api/libretime_api/mixins/readwriteserializer.py
@@ -0,0 +1,32 @@
+class ReadWriteSerializerMixin:
+    """
+    Overrides get_serializer_class to choose the read serializer
+    for GET requests and the write serializer for POST requests.
+
+    Set read_serializer_class and write_serializer_class attributes on a
+    viewset.
+    """
+
+    read_serializer_class = None
+    write_serializer_class = None
+
+    def get_serializer_class(self):
+        if self.action in ["create"]:
+            return self.get_write_serializer_class()
+        return self.get_read_serializer_class()
+
+    def get_read_serializer_class(self):
+        assert self.read_serializer_class is not None, (
+            "'%s' should either include a `read_serializer_class` attribute,"
+            "or override the `get_read_serializer_class()` method."
+            % self.__class__.__name__
+        )
+        return self.read_serializer_class
+
+    def get_write_serializer_class(self):
+        assert self.write_serializer_class is not None, (
+            "'%s' should either include a `write_serializer_class` attribute,"
+            "or override the `get_write_serializer_class()` method."
+            % self.__class__.__name__
+        )
+        return self.write_serializer_class
