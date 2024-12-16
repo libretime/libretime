4cfc13ec401ea782093f1f0c2724a2d1e2dfaeb0
HighTechHarmony <smcgrath@nfatech.com>
2024-12-15 21:26:12 -0500

    fix(legacy): migrations from airtime 2.5.1

--- api/libretime_api/legacy/migrations/0023_3_0_0_alpha_9_1.py
+++ api/libretime_api/legacy/migrations/0023_3_0_0_alpha_9_1.py
@@ -4,10 +4,12 @@ from django.db import migrations
 
 from ._migrations import legacy_migration_factory
 
-"""
-This migration is currently an empty placeholder for 3.0.0-alpha.9.1.  Please do not remove it.  There are
-currently no actions, but it needs to remain intact because of the incremental nature of the migrations.
-"""
+# This migration is currently a placeholder for 3.0.0-alpha.9.1.
+# Please do not remove it.  There are currently no actions, but it
+# needs to remain intact so it does not fail when called from the
+# migrations script. Any future migrations that may apply to
+# 3.0.0-alpha.9.1 will be added to this file.
+
 UP = """
 
 """
