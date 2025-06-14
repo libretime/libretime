545e91ec1e00c281c9dc1d518d445f2f421b8424
jo <ljonas@riseup.net>
2025-06-14 15:14:13 +0200

    refactor(playout): revert prefer datetime.now(timezone.utc) over datetime.utcnow() (#3167)
    This reverts commit 8bd2db16617bb90d9a3b00ec48a8836a6bafa4f1.

--- playout/libretime_playout/player/liquidsoap.py
+++ playout/libretime_playout/player/liquidsoap.py
@@ -1,6 +1,6 @@
 import logging
 import time
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 from typing import Dict, List, Optional, Set
 
 from ..liquidsoap.client import LiquidsoapClient
@@ -317,7 +317,7 @@ class Liquidsoap:
     def modify_cue_point(self, file_event: FileEvent) -> None:
         assert file_event.type == EventKind.FILE
 
-        lateness = seconds_between(file_event.start, datetime.now(timezone.utc))
+        lateness = seconds_between(file_event.start, datetime.utcnow())
 
         if lateness > 0:
             logger.debug("media item was supposed to start %ss ago", lateness)
