e920ed4717bd134d50767b8912ed7ad8c8f54439
dakriy <git@persignum.com>
2025-06-17 15:07:18 -0700

    feat: now macro should use show start time to determine what tracks go into a show for dynamically autoloading smart blocks

--- legacy/application/models/Block.php
+++ legacy/application/models/Block.php
@@ -1295,9 +1295,9 @@ SQL;
         }
     }
 
-    public function getListOfFilesUnderLimit($show = null)
+    public function getListOfFilesUnderLimit($show = null, $resolveDateTime = null)
     {
-        $info = $this->getListofFilesMeetCriteria($show);
+        $info = $this->getListofFilesMeetCriteria($show, $resolveDateTime);
         $files = $info['files'];
         $limit = $info['limit'];
         $repeat = $info['repeat_tracks'] == 1;
@@ -1480,13 +1480,14 @@ SQL;
         return $storedCrit;
     }
 
-    private function resolveDate($value, $timeZone)
+    private function resolveDate($value, ?DateTime $dateTime, string $timeZone)
     {
         if (!is_string($value)) {
             return $value;
         }
 
-        $dt = new DateTime('now', new DateTimeZone($timeZone));
+        $dt = $dateTime ?: new DateTime('now');
+        $dt->setTimezone(new DateTimeZone($timeZone));
 
         return preg_replace_callback(
             '/now{(.*?)}/',
@@ -1496,7 +1497,7 @@ SQL;
     }
 
     // this function return list of propel object
-    public function getListofFilesMeetCriteria($showLimit = null)
+    public function getListofFilesMeetCriteria($showLimit = null, $resolveDateTime = null)
     {
         $storedCrit = $this->getCriteria();
 
@@ -1563,7 +1564,7 @@ SQL;
                         $spCriteriaExtra = $criteria['extra'];
                     }
 
-                    $spCriteriaValue = $this->resolveDate($spCriteriaValue, $timeZone);
+                    $spCriteriaValue = $this->resolveDate($spCriteriaValue, $resolveDateTime, $timeZone);
 
                     if ($spCriteriaModifier == CriteriaModifier::STARTS_WITH) {
                         $spCriteriaValue = "{$spCriteriaValue}%";
