10e5feaddebd17042304a1a300f9cc52e21d8290
jo <ljonas@riseup.net>
2025-08-10 16:06:04 +0200

    wip: remove old worker and fix tests

--- worker/tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ.setdefault("LIBRETIME_DEBUG", "true")
-os.environ.setdefault("LIBRETIME_GENERAL_PUBLIC_URL", "http://localhost")
-os.environ.setdefault("LIBRETIME_GENERAL_API_KEY", "testing")
-os.environ.setdefault("LIBRETIME_GENERAL_SECRET_KEY", "testing")
