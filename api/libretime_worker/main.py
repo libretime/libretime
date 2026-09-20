7191bd482ce6e2021e18348d7ebd13488841d639
jo <ljonas@riseup.net>
2026-09-20 22:02:32 +0200

    fix: do not exec into celery

--- api/libretime_worker/main.py
+++ api/libretime_worker/main.py
@@ -1,11 +1,11 @@
-import os
 from pathlib import Path
-from shutil import which
 from typing import Optional
 
 import click
 from libretime_shared.cli import cli_logging_options
 from libretime_shared.config import DEFAULT_ENV_PREFIX
+
+from libretime_api.worker import app
 
 
 @click.command(context_settings={"auto_envvar_prefix": DEFAULT_ENV_PREFIX})
@@ -15,8 +15,6 @@ def cli(log_level: str, log_filepath: Optional[Path]):
     Run celery.
     """
     args = [
-        which("celery"),
-        "libretime-worker",
         "--app=libretime_api.worker",
         "worker",
         "--beat",
@@ -25,4 +23,4 @@ def cli(log_level: str, log_filepath: Optional[Path]):
     if log_filepath is not None:
         args.append(f"--logfile={log_filepath}")
 
-    os.execl(*args)
+    app.worker_main(args)
